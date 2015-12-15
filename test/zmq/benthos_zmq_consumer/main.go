08b842c106c7461b62393340e32cde02c13b5ed7
jeffail <ash@jeffs.eu>
2015-12-15 00:54:58 +0000

    Add byte rate measure to zmq4 test utils

--- test/zmq/benthos_zmq_consumer/main.go
+++ test/zmq/benthos_zmq_consumer/main.go
@@ -49,6 +49,11 @@ func bytesToIndex(b []byte) (index int32) {
 	return index
 }
 
+type msgStat struct {
+	latency int64 // Time taken for a message to leave the producer and be received by the consumer.
+	nBytes  int64 // Number of bytes carried in the message.
+}
+
 func main() {
 	var address string
 	flag.StringVar(&address, "addr", "tcp://localhost:1235", "Address of the benthos server")
@@ -75,7 +80,7 @@ func main() {
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 
-	readChan := make(chan int64)
+	readChan := make(chan msgStat)
 
 	var running, index, dataLost int32 = 1, 0, 0
 
@@ -83,26 +88,31 @@ func main() {
 		timer := time.NewTicker(time.Second)
 		defer timer.Stop()
 
-		var total, tally, avg, secTotal, secTally, secAvg int64
+		var total, tally, avg, secTotal, secTally, secNBytes, secAvg int64
 
 		for atomic.LoadInt32(&running) == 1 {
 			select {
-			case t := <-readChan:
-				total = total + t
+			case stat := <-readChan:
+				// Total stats
+				total = total + stat.latency
 				tally = tally + 1
 				avg = total / tally
-				secTotal = secTotal + t
+
+				// Per second stats
+				secTotal = secTotal + stat.latency
 				secTally = secTally + 1
+				secNBytes = secNBytes + stat.nBytes
 				secAvg = secTotal / secTally
 			case <-timer.C:
 				fmt.Printf("+\n")
-				fmt.Printf("| TICK\n")
-				fmt.Printf("| Average : %v\n", time.Duration(secAvg))
-				fmt.Printf("| Count   : %v\n", secTally)
+				fmt.Printf("| THIS SECOND\n")
+				fmt.Printf("| Avg Latency : %v\n", time.Duration(secAvg))
+				fmt.Printf("| Byte Rate   : %.6f MB/s\n", float64(secNBytes)/1048576)
+				fmt.Printf("| Msg Count   : %v\n", secTally)
 				fmt.Printf("|\n")
 				fmt.Printf("| TOTAL\n")
-				fmt.Printf("| Average : %v\n", time.Duration(avg))
-				fmt.Printf("| Count   : %v\n", tally)
+				fmt.Printf("| Avg Latency : %v\n", time.Duration(avg))
+				fmt.Printf("| Msg Count   : %v\n", tally)
 				if lost := atomic.LoadInt32(&dataLost); lost != 0 {
 					fmt.Printf("|\n")
 					fmt.Printf("| DATA LOST: %v\n", lost)
@@ -110,6 +120,7 @@ func main() {
 				fmt.Printf("+\n\n")
 				secTotal = 0
 				secTally = 0
+				secNBytes = 0
 				secAvg = 0
 			}
 		}
@@ -132,7 +143,14 @@ func main() {
 					index = i
 					t := time.Now()
 					if err = t.UnmarshalBinary(msgParts[0]); err == nil {
-						readChan <- int64(time.Since(t))
+						var totalBytes int64
+						for _, part := range msgParts {
+							totalBytes = totalBytes + int64(len(part))
+						}
+						readChan <- msgStat{
+							latency: int64(time.Since(t)),
+							nBytes:  totalBytes,
+						}
 					} else {
 						panic(err)
 					}
