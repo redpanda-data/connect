5cf359c3d98d47f5b98463b42210abc7703cd86f
jeffail <ash@jeffs.eu>
2018-01-05 15:18:32 +0000

    Add noop processor

---
+++ lib/processor/noop.go
@@ -0,0 +1,58 @@
+// Copyright (c) 2017 Ashley Jeffs
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package processor
+
+import (
+	"github.com/Jeffail/benthos/lib/types"
+	"github.com/Jeffail/benthos/lib/util/service/log"
+	"github.com/Jeffail/benthos/lib/util/service/metrics"
+)
+
+//------------------------------------------------------------------------------
+
+func init() {
+	constructors["noop"] = typeSpec{
+		constructor: NewNoop,
+		description: `
+Noop is a no-op processor that does nothing, the message passes through
+unchanged.`,
+	}
+}
+
+//------------------------------------------------------------------------------
+
+// Noop is a no-op processor that does nothing.
+type Noop struct {
+}
+
+// NewNoop returns a Noop processor.
+func NewNoop(conf Config, log log.Modular, stats metrics.Type) (Type, error) {
+	return &Noop{}, nil
+}
+
+//------------------------------------------------------------------------------
+
+// ProcessMessage does nothing and returns the message unchanged.
+func (c *Noop) ProcessMessage(msg *types.Message) (*types.Message, types.Response, bool) {
+	return msg, nil, true
+}
+
+//------------------------------------------------------------------------------
