1d9a0138920f38d0327929ce57b57b70c3e5655d
Paul Dixon <paul.dixon@rocketx.com>
2018-08-27 18:42:59 +0100

    wip: start to implement a pgp decrypt processor

---
+++ lib/processor/decrypt.go
@@ -0,0 +1,201 @@
+// Copyright (c) 2018 Ashley Jeffs
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package processor
+
+import (
+	"bytes"
+	"fmt"
+	"golang.org/x/crypto/openpgp"
+	"golang.org/x/crypto/openpgp/armor"
+	"golang.org/x/crypto/openpgp/packet"
+	"io/ioutil"
+	"os"
+
+	"github.com/Jeffail/benthos/lib/log"
+	"github.com/Jeffail/benthos/lib/metrics"
+	"github.com/Jeffail/benthos/lib/response"
+	"github.com/Jeffail/benthos/lib/types"
+)
+
+//------------------------------------------------------------------------------
+
+func init() {
+	Constructors[TypeDecrypt] = TypeSpec{
+		constructor: NewDecrypt,
+		description: `
+Decrypts parts of a message according to the selected scheme. Supported available
+schemes are: pgp.`,
+	}
+}
+
+//------------------------------------------------------------------------------
+
+// DecryptConfig contains configuration fields for the Decrypt processor.
+type DecryptConfig struct {
+	Scheme     string `json:"scheme" yaml:"scheme"`
+	Key        string `json:"key" yaml:"key"`
+	Parts      []int  `json:"parts" yaml:"parts"`
+}
+
+// NewDecryptConfig returns a DecryptConfig with default values.
+func NewDecryptConfig() DecryptConfig {
+	return DecryptConfig{
+		Scheme:     "pgp",
+		Key:        "",
+		Parts:      []int{},
+	}
+}
+
+//------------------------------------------------------------------------------
+
+type decryptFunc func(key string, bytes []byte) ([]byte, error)
+
+func pgpDecode(key string, b []byte) ([]byte, error) {
+	// read the key from the filesystem
+	keyData, err := os.Open(key)
+	defer key.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	// decode armor
+	keyBlock, err := armor.Decode(keyData)
+	if err != nil {
+		return nil, err
+	}
+  // check key type
+	if keyBlock.Type != openpgp.PrivateKeyType {
+		return nil, errors.New("invalid key type")
+	}
+
+	// add key to key list
+	keyReader := packet.NewReader(keyBlock.Body)
+	keyEntity, err := openpgp.ReadEntity(keyReader)
+	entityList := &openpgp.EntityList{keyEntity}
+
+	m := bytes.NewReader(b)
+	messageBlock, err := armor.Decode(m)
+	if err != nil {
+		return nil, err
+	}
+	message, err := openpgp.ReadMessage(messageBlock.Body, entityList, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	messageBuf := bytes.NewBuffer(nil)
+	messageBuf.ReadFrom(message.UnverifiedBody)
+
+	return messageBuf, nil
+}
+
+func strToDecryptor(str string) (decryptFunc, error) {
+	switch str {
+	case "pgp":
+		return pgpDecode, nil
+	}
+	return nil, fmt.Errorf("decrypt scheme not recognised: %v", str)
+}
+
+//------------------------------------------------------------------------------
+
+// decryptFunc is a processor that can selectively decrypt parts of a message
+// following a chosen scheme.
+type decryptFunc struct {
+	conf decryptFuncConfig
+	fn   decryptFunc
+
+	log   log.Modular
+	stats metrics.Type
+
+	mCount     metrics.StatCounter
+	mSucc      metrics.StatCounter
+	mErr       metrics.StatCounter
+	mSkipped   metrics.StatCounter
+	mSent      metrics.StatCounter
+	mSentParts metrics.StatCounter
+}
+
+// NewDecrypt returns a Decrypt processor.
+func NewDecrypt(
+	conf Config, mgr types.Manager, log log.Modular, stats metrics.Type,
+) (Type, error) {
+	cor, err := strToDecryptor(conf.Decrypt.Scheme)
+	if err != nil {
+		return nil, err
+	}
+	return &Decrypt{
+		conf:  conf.Decrypt,
+		fn:    cor,
+		log:   log.NewModule(".processor.decrypt"),
+		stats: stats,
+
+		mCount:     stats.GetCounter("processor.decrypt.count"),
+		mSucc:      stats.GetCounter("processor.decrypt.success"),
+		mErr:       stats.GetCounter("processor.decrypt.error"),
+		mSkipped:   stats.GetCounter("processor.decrypt.skipped"),
+		mSent:      stats.GetCounter("processor.decrypt.sent"),
+		mSentParts: stats.GetCounter("processor.decrypt.parts.sent"),
+	}, nil
+}
+
+//------------------------------------------------------------------------------
+
+// ProcessMessage applies the processor to a message, either creating >0
+// resulting messages or a response to be sent back to the message source.
+func (c *Decode) ProcessMessage(msg types.Message) ([]types.Message, types.Response) {
+	c.mCount.Incr(1)
+
+	newMsg := msg.Copy()
+
+	targetParts := c.conf.Parts
+	if len(targetParts) == 0 {
+		targetParts = make([]int, newMsg.Len())
+		for i := range targetParts {
+			targetParts[i] = i
+		}
+	}
+
+	for _, index := range targetParts {
+		part := msg.Get(index).Get()
+		newPart, err := c.fn(c.conf.Key, part)
+		if err == nil {
+			c.mSucc.Incr(1)
+			newMsg.Get(index).Set(newPart)
+		} else {
+			c.log.Errorf("Failed to decrypt message part: %v\n", err)
+			c.mErr.Incr(1)
+		}
+	}
+
+	if newMsg.Len() == 0 {
+		c.mSkipped.Incr(1)
+		return nil, response.NewAck()
+	}
+
+	c.mSent.Incr(1)
+	c.mSentParts.Incr(int64(newMsg.Len()))
+	msgs := [1]types.Message{newMsg}
+	return msgs[:], nil
+}
+
+//------------------------------------------------------------------------------
