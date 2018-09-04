ce9d4e204fc1a8355905bf93c0118851ab502118
Adam Boscarino <aboscarino@digitalocean.com>
2018-09-04 10:20:54 -0400

    lib/output/writer/hdfs_test: use dockertest

--- lib/output/writer/hdfs_test.go
+++ lib/output/writer/hdfs_test.go
@@ -30,39 +30,78 @@ import (
 	"github.com/Jeffail/benthos/lib/message"
 	"github.com/Jeffail/benthos/lib/metrics"
 	"github.com/colinmarc/hdfs"
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
 )
-
-var (
-	hdfsNameNode, hdfsUser string
-)
-
-func init() {
-	hdfsNameNode = os.Getenv("HDFS_NAMENODE")
-	hdfsUser = os.Getenv("HDFS_USER")
-}
 
 func TestHDFSIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping integration test in short mode")
 	}
 
-	if hdfsNameNode == "" || hdfsUser == "" {
-		t.Skip("HDFS config not specified.")
-	}
-
-	hosts := []string{hdfsNameNode}
-	user := hdfsUser
-
-	client, err := hdfs.NewClient(hdfs.ClientOptions{
-		Addresses: hosts,
-		User:      user,
-	})
+	pool, err := dockertest.NewPool("")
 	if err != nil {
-		t.Fatalf("hdfs.NewClient(%s) failed: %v", hdfsNameNode, err)
+		t.Skipf("Could not connect to docker: %s", err)
+	}
+	pool.MaxWait = time.Second * 30
+
+	options := &dockertest.RunOptions{
+		Repository:   "cybermaggedon/hadoop",
+		Tag:          "2.8.2",
+		Hostname:     "localhost",
+		ExposedPorts: []string{"9000", "50075", "50070", "50010"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"9000/tcp":  {{HostIP: "", HostPort: "9000"}},
+			"50070/tcp": {{HostIP: "", HostPort: "50070"}},
+			"50075/tcp": {{HostIP: "", HostPort: "50075"}},
+			"50010/tcp": {{HostIP: "", HostPort: "50010"}},
+		},
 	}
 
-	t.Run("TestHDFSConnect", func(t *testing.T) {
-		testHDFSConnect(hosts, user, client, t)
+	resource, err := pool.RunWithOptions(options)
+	if err != nil {
+		t.Fatalf("Could not start resource: %s", err)
+	}
+
+	var client *hdfs.Client
+
+	hosts := []string{"localhost:9000"}
+	user := "root"
+
+	if err = pool.Retry(func() error {
+		testFile := "/cluster_ready" + time.Now().Format("20060102150405")
+		client, err = hdfs.NewClient(hdfs.ClientOptions{
+			Addresses: hosts,
+			User:      user,
+		})
+		if err != nil {
+			return err
+		}
+		fw, err := client.Create(testFile)
+		if err != nil {
+			return err
+		}
+		_, err = fw.Write([]byte("cluster is ready"))
+		if err != nil {
+			return err
+		}
+		err = fw.Close()
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		t.Fatalf("Could not connect to docker resource: %s", err)
+	}
+
+	defer func() {
+		if err = pool.Purge(resource); err != nil {
+			t.Logf("Failed to clean up docker resource: %v", err)
+		}
+	}()
+
+	t.Run("TestHDFSConnect", func(th *testing.T) {
+		testHDFSConnect(hosts, user, client, th)
 	})
 }
 
@@ -70,7 +109,7 @@ func testHDFSConnect(hosts []string, user string, client *hdfs.Client, t *testin
 	conf := NewHDFSConfig()
 	conf.User = user
 	conf.Hosts = hosts
-	conf.Directory = "/tmp/benthos_test"
+	conf.Directory = "/"
 	conf.Path = "${!count:files}-benthos_test.txt"
 
 	h := NewHDFS(conf, log.New(os.Stdout, log.Config{LogLevel: "NONE"}), metrics.DudType{})
@@ -101,7 +140,7 @@ func testHDFSConnect(hosts []string, user string, client *hdfs.Client, t *testin
 		}
 	}
 	for i := 0; i < N; i++ {
-		filePath := fmt.Sprintf("/tmp/benthos_test/%v-benthos_test.txt", i+1)
+		filePath := fmt.Sprintf("/%v-benthos_test.txt", i+1)
 		data, err := client.ReadFile(filePath)
 		if err != nil {
 			t.Fatalf("Failed to get file '%v': %v", filePath, err)
