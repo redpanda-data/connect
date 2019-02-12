959377fe8c215ff3790327cef1a7c2fd95c5b288
Dan Rubenstein <drubenstein13@gmail.com>
2019-02-12 15:47:27 -0500

    Don't just blatantly copy docs

--- lib/metrics/whitelist.go
+++ lib/metrics/whitelist.go
@@ -19,21 +19,9 @@ func init() {
 	constructors[TypeWhiteList] = typeSpec{
 		constructor: NewWhitelist,
 		description: `
-Sends messages to an AMQP (0.91) exchange. AMQP is a messaging protocol used by
-various message brokers, including RabbitMQ. The metadata from each message are
-delivered as headers.
+Whitelist metrics
 
-It's possible for this output type to create the target exchange by setting
-` + "`exchange_declare.enabled` to `true`" + `, if the exchange already exists
-then the declaration passively verifies that the settings match.
-
-Exchange type options are: direct|fanout|topic|x-custom
-
-TLS is automatic when connecting to an ` + "`amqps`" + ` URL, but custom
-settings can be enabled in the ` + "`tls`" + ` section.
-
-The field 'key' can be dynamically set using function interpolations described
-[here](../config_interpolation.md#functions).`,
+Extensive docs to come.`,
 	}
 }
 
