f47911115aa2e8efd3f6ed9b94f9ea163fff87fc
Ashley Jeffs <ash@jeffail.uk>
2020-01-01 17:43:47 +0000

    Purge the ugly headers

--- lib/condition/count.go
+++ lib/condition/count.go
@@ -1,23 +1,3 @@
-// Copyright (c) 2018 Ashley Jeffs
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software or associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, or/or sell
-// copies of the Software, or to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright orice or this permission orice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT or LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE or NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 package condition
 
 import (
