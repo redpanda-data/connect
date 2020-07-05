a91eb3842db6caeaf40a0de8b9fba761ebed65ef
Ashley Jeffs <ash@jeffail.uk>
2020-07-05 10:00:14 +0100

    Add config linting to test

--- lib/input/reader/zmq4.go
+++ lib/input/reader/zmq4.go
@@ -17,30 +17,6 @@ import (
 
 //------------------------------------------------------------------------------
 
-// ZMQ4Config contains configuration fields for the ZMQ4 input type.
-type ZMQ4Config struct {
-	URLs          []string `json:"urls" yaml:"urls"`
-	Bind          bool     `json:"bind" yaml:"bind"`
-	SocketType    string   `json:"socket_type" yaml:"socket_type"`
-	SubFilters    []string `json:"sub_filters" yaml:"sub_filters"`
-	HighWaterMark int      `json:"high_water_mark" yaml:"high_water_mark"`
-	PollTimeout   string   `json:"poll_timeout" yaml:"poll_timeout"`
-}
-
-// NewZMQ4Config creates a new ZMQ4Config with default values.
-func NewZMQ4Config() *ZMQ4Config {
-	return &ZMQ4Config{
-		URLs:          []string{"tcp://localhost:5555"},
-		Bind:          false,
-		SocketType:    "PULL",
-		SubFilters:    []string{},
-		HighWaterMark: 0,
-		PollTimeout:   "5s",
-	}
-}
-
-//------------------------------------------------------------------------------
-
 // ZMQ4 is an input type that consumes ZMQ messages.
 type ZMQ4 struct {
 	urls  []string
--- lib/output/writer/zmq4.go
+++ lib/output/writer/zmq4.go
@@ -16,28 +16,6 @@ import (
 
 //------------------------------------------------------------------------------
 
-// ZMQ4Config contains configuration fields for the ZMQ4 output type.
-type ZMQ4Config struct {
-	URLs          []string `json:"urls" yaml:"urls"`
-	Bind          bool     `json:"bind" yaml:"bind"`
-	SocketType    string   `json:"socket_type" yaml:"socket_type"`
-	HighWaterMark int      `json:"high_water_mark" yaml:"high_water_mark"`
-	PollTimeout   string   `json:"poll_timeout" yaml:"poll_timeout"`
-}
-
-// NewZMQ4Config creates a new ZMQ4Config with default values.
-func NewZMQ4Config() *ZMQ4Config {
-	return &ZMQ4Config{
-		URLs:          []string{"tcp://*:5556"},
-		Bind:          true,
-		SocketType:    "PUSH",
-		HighWaterMark: 0,
-		PollTimeout:   "5s",
-	}
-}
-
-//------------------------------------------------------------------------------
-
 // ZMQ4 is an output type that writes ZMQ4 messages.
 type ZMQ4 struct {
 	log   log.Modular
