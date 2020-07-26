2182fcddecacc97b8373368f10f5d47b5d66ee71
Ashley Jeffs <ash@jeffail.uk>
2020-07-26 18:22:49 +0100

    Fix wasm build

--- lib/output/writer/table_storage_wasm.go
+++ lib/output/writer/table_storage_wasm.go
@@ -3,21 +3,13 @@
 package writer
 
 import (
-	"context"
 	"errors"
 
 	"github.com/Jeffail/benthos/v3/lib/log"
 	"github.com/Jeffail/benthos/v3/lib/metrics"
-	"github.com/Jeffail/benthos/v3/lib/types"
 )
 
 //------------------------------------------------------------------------------
-
-type dummy interface {
-	Type
-	ConnectWithContext(ctx context.Context) error
-	WriteWithContext(ctx context.Context, msg types.Message) error
-}
 
 // NewAzureTableStorage returns an error as it is not supported in WASM builds.
 func NewAzureTableStorage(
