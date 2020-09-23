cd5ed0503e9f20031881861f1b90b5043eec33b3
Ashley Jeffs <ash@jeffail.uk>
2020-09-23 21:48:07 +0100

    Fix discord invite URL

--- website/src/pages/community.js
+++ website/src/pages/community.js
@@ -33,7 +33,7 @@ function Community() {
                   <p>Be one of the first to join the new Benthos discord server</p>
                 </div>
                 <div class="card__footer">
-                  <Link to="https://discord.gg/pXAF57" class="button button--outline button--primary button--block">Join</Link>
+                  <Link to="https://discord.gg/6VaWjzP" class="button button--outline button--primary button--block">Join</Link>
                 </div>
               </div>
             </div>
