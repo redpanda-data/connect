b328a39592c170321c9e4e7bc5244a12b0966802
Nathan West <nwest1@users.noreply.github.com>
2020-12-16 11:13:37 -0600

    fixed cluster test

--- lib/test/integration/cache/redis_cluster_test.go
+++ lib/test/integration/cache/redis_cluster_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/Jeffail/benthos/v3/lib/log"
 	"github.com/Jeffail/benthos/v3/lib/metrics"
 	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -20,65 +21,49 @@ var _ = registerIntegrationTest("redis_cluster", func(t *testing.T) {
 
 	pool, err := dockertest.NewPool("")
 	require.NoError(t, err)
-
 	pool.MaxWait = time.Second * 30
 
-	net, err := pool.CreateNetwork("redis-cluster")
-	require.NoError(t, err)
-	defer pool.RemoveNetwork(net)
-
-	redisNodes := make([]*dockertest.Resource, 6)
-	for i := 0; i < 6; i++ {
-		node, err := pool.RunWithOptions(&dockertest.RunOptions{
-			Name:       fmt.Sprintf("node%d", i),
-			Repository: "bitnami/redis-cluster",
-			Tag:        "latest",
-			Networks:   []*dockertest.Network{net},
-			Env: []string{
-				"REDIS_PASSWORD=benthos",
-				"REDIS_CLUSTER_DYNAMIC_IPS=no",
-				"REDIS_CLUSTER_ANNOUNCE_IP=localhost",
-				"REDIS_NODES=node0 node1 node2 node3 node4 node5",
-			},
-		})
-		require.NoError(t, err)
-		redisNodes[i] = node
-		node.Expire(900)
+	networks, _ := pool.Client.ListNetworks()
+	hostIP := ""
+	for _, network := range networks {
+		if network.Name == "bridge" {
+			hostIP = network.IPAM.Config[0].Gateway
+		}
+	}
+	if runtime.GOOS == "darwin" {
+		hostIP = "0.0.0.0"
 	}
 
-	creator, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Name:       "creator",
-		Repository: "bitnami/redis-cluster",
-		Tag:        "latest",
-		Networks:   []*dockertest.Network{net},
+	exposedPorts := make([]string, 12)
+	portBindings := make(map[docker.Port][]docker.PortBinding, 12)
+	for i := 0; i < 6; i++ {
+		p1 := fmt.Sprintf("%d/tcp", 7000+i)
+		p2 := fmt.Sprintf("%d/tcp", 17000+i)
+		exposedPorts[i] = p1
+		exposedPorts[i+6] = p2
+		portBindings[docker.Port(p1)] = []docker.PortBinding{{HostIP: "", HostPort: p1}}
+		portBindings[docker.Port(p2)] = []docker.PortBinding{{HostIP: "", HostPort: p2}}
+	}
+
+	cluster, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Name:         "redis-cluster",
+		Repository:   "grokzen/redis-cluster",
+		Tag:          "6.0.7",
+		ExposedPorts: exposedPorts,
+		PortBindings: portBindings,
 		Env: []string{
-			"REDISCLI_AUTH=benthos",
-			"REDIS_CLUSTER_REPLICAS=1",
-			"REDIS_CLUSTER_ANNOUNCE_IP=localhost",
-			"REDIS_NODES=node0 node1 node2 node3 node4 node5",
-			"REDIS_CLUSTER_CREATOR=yes",
+			"IP=" + hostIP,
 		},
 	})
 	require.NoError(t, err)
-	creator.Expire(900)
 
 	t.Cleanup(func() {
-		for _, v := range redisNodes {
-			assert.NoError(t, pool.Purge(v))
-		}
-		assert.NoError(t, pool.Purge(creator))
+		assert.NoError(t, pool.Purge(cluster))
 	})
 
 	clusterURL := ""
-	for _, v := range redisNodes {
-		if runtime.GOOS == "darwin" {
-			clusterURL += fmt.Sprintf("tcp://benthos:benthos@%s:%s/0,",
-				v.GetBoundIP("6379/tcp"),
-				v.GetPort("6379/tcp"),
-			)
-		} else {
-			clusterURL += fmt.Sprintf("tcp://benthos:benthos@localhost:%s/0,", v.GetPort("6379/tcp"))
-		}
+	for i := 0; i < 6; i++ {
+		clusterURL += fmt.Sprintf("redis://%s:%s/0,", hostIP, fmt.Sprintf("%d", 7000+i))
 	}
 	clusterURL = strings.TrimSuffix(clusterURL, ",")
 
@@ -101,7 +86,7 @@ resources:
     testcache:
       redis:
         url: $VAR1
-		kind: cluster
+        kind: cluster
         prefix: $ID
 `
 	suite := integrationTests(
