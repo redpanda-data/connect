ce0b3a991046562faf8c176623bf0494eb71fd7b
Marco Amador <amador.marco@gmail.com>
2020-12-21 20:22:53 +0000

    Add azure prefix

--- lib/output/azure_queue_storage.go
+++ lib/output/azure_queue_storage.go
@@ -12,7 +12,7 @@ import (
 //------------------------------------------------------------------------------
 
 func init() {
-	Constructors[TypeQueueStorage] = TypeSpec{
+	Constructors[TypeAzureQueueStorage] = TypeSpec{
 		constructor: NewAzureQueueStorage,
 		Status:      docs.StatusBeta,
 		Summary: `
@@ -25,7 +25,7 @@ calculated per message of a batch.
 
 `,
 		sanitiseConfigFunc: func(conf Config) (interface{}, error) {
-			return sanitiseWithBatch(conf.QueueStorage, conf.QueueStorage.Batching)
+			return sanitiseWithBatch(conf.AzureQueueStorage, conf.AzureQueueStorage.Batching)
 		},
 		Async:   true,
 		Batches: true,
@@ -35,7 +35,7 @@ calculated per message of a batch.
 			docs.FieldCommon("storage_connection_string", "A storage account connection string. This field is required if `storage_account` and `storage_access_key` are not set."),
 			docs.FieldCommon("queue_name", "The name of the target Queue Storage queue."),
 			docs.FieldAdvanced(
-				"ttl", "The TTL of each individual msg as a duration string. Defaults to 0, meaning no TTL is set",
+				"ttl", "The TTL of each individual message as a duration string. Defaults to 0, meaning no retention period is set",
 				"60s", "5m", "36h",
 			),
 			docs.FieldCommon("max_in_flight", "The maximum number of messages to have in flight at a given time. Increase this to improve throughput."),
@@ -52,24 +52,24 @@ calculated per message of a batch.
 
 // NewAzureQueueStorage creates a new AzureQueueStorage output type.
 func NewAzureQueueStorage(conf Config, mgr types.Manager, log log.Modular, stats metrics.Type) (Type, error) {
-	s, err := writer.NewAzureQueueStorage(conf.QueueStorage, log, stats)
+	s, err := writer.NewAzureQueueStorage(conf.AzureQueueStorage, log, stats)
 	if err != nil {
 		return nil, err
 	}
 	var w Type
-	if conf.QueueStorage.MaxInFlight == 1 {
+	if conf.AzureQueueStorage.MaxInFlight == 1 {
 		w, err = NewWriter(
-			TypeQueueStorage, s, log, stats,
+			TypeAzureQueueStorage, s, log, stats,
 		)
 	} else {
 		w, err = NewAsyncWriter(
-			TypeQueueStorage, conf.QueueStorage.MaxInFlight, s, log, stats,
+			TypeAzureQueueStorage, conf.AzureQueueStorage.MaxInFlight, s, log, stats,
 		)
 	}
 	if err != nil {
 		return w, err
 	}
-	return newBatcherFromConf(conf.QueueStorage.Batching, w, mgr, log, stats)
+	return newBatcherFromConf(conf.AzureQueueStorage.Batching, w, mgr, log, stats)
 }
 
 //------------------------------------------------------------------------------
