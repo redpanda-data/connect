2a53c5daf2dfcea2be17b8a9e9c4bede0306cc05
Anders Qvist <anders.qvist@xenit.se>
2021-08-22 17:32:16 +0200

    Meeting simongottschlag comments on PR #850.

--- lib/output/mqtt.go
+++ lib/output/mqtt.go
@@ -2,7 +2,7 @@ package output
 
 import (
 	"github.com/Jeffail/benthos/v3/internal/docs"
-	mqttconf "github.com/Jeffail/benthos/v3/internal/mqttconf"
+	"github.com/Jeffail/benthos/v3/internal/mqttconf"
 	"github.com/Jeffail/benthos/v3/lib/log"
 	"github.com/Jeffail/benthos/v3/lib/metrics"
 	"github.com/Jeffail/benthos/v3/lib/output/writer"
