1c5dcbd098bc2c27f72f1e1d531c22a197ee7f3d
James Harr <james.harr@gmail.com>
2021-09-07 08:23:54 -0500

    Added notes

--- internal/bloblang/query/methods_geoip.go
+++ internal/bloblang/query/methods_geoip.go
@@ -11,6 +11,7 @@ var _ = registerSimpleMethod(
 		"geoip_city", "",
 	).Param(ParamString("path", "Path to city mmdb file")),
 	func(args *ParsedParams) (simpleMethod, error) {
+		// TODO: Ensure path is a static. Unsure how to accomplish this
 		path, err := args.FieldString("path")
 		if err != nil {
 			return nil, err
@@ -24,6 +25,8 @@ var _ = registerSimpleMethod(
 
 		return func(v interface{}, ctx FunctionContext) (interface{}, error) {
 			var ip net.IP
+
+			// Cast to string type
 			switch t := v.(type) {
 			case []byte:
 				ip = net.ParseIP(string(t))
@@ -32,7 +35,11 @@ var _ = registerSimpleMethod(
 			default:
 				return nil, NewTypeError(v, ValueString)
 			}
-			// TODO: ip will be 'nil' if invalid. Need to handle
+
+			// TODO: ip will be 'nil' if not a valid v4/v6 address. How to handle?
+			// It currently winds up reporting this when invalid:
+			//    "failed assignment (line 2): field `this.ip`: IP passed to Lookup cannot be nil"
+			// should we add context? IE what the lookup value is?
 			geoip_city, err := db.City(ip)
 			if err != nil {
 				return nil, err
@@ -41,3 +48,11 @@ var _ = registerSimpleMethod(
 		}, nil
 	},
 )
+
+// TODO: geoip_country
+// TODO: geoip_asn
+// TODO: geoip_enterprise
+// TODO: geoip_anonymous_ip
+// TODO: geoip_connection_type
+// TODO: geoip_domain
+// TODO: geoip_isp
