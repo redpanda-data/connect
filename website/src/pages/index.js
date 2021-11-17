b21f2fcd676203fab365fc2cbe5f59851485d2ab
Ashley Jeffs <ash@jeffail.uk>
2021-11-17 19:59:29 +0000

    Add new sponsor 🎉

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -330,6 +330,7 @@ function Home() {
                 <a href="https://www.humansecurity.com" className={styles.sponsorLink}><img className={styles.humanImg} src="/img/sponsors/HUMAN_logo.png" /></a>
                 <a href="https://www.infosum.com/" className={styles.sponsorLink}><img className={styles.infosumImg} src="/img/sponsors/infosum_logo.png" /></a>
                 <a href="https://community.com/" className={styles.sponsorLink}><img className={styles.communityImg} src="/img/sponsors/community.svg" /></a>
+                <a href="https://www.optum.com/" className={styles.sponsorLink}><img className={styles.optumImg} src="/img/sponsors/optum_logo.png" /></a>
               </div>
               <div className={classnames('col col--6', styles.loveSectionPlea)}>
                 <div>
