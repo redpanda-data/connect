ed82058ea9c5d8c6a32039550101ff82a6885184
Remy Franke <franke.remy@gmail.com>
2022-01-06 07:26:55 +0100

    Updated test to prepare for json implementation. Tests are failing succesfully.

--- lib/util/text/env_vars_test.go
+++ lib/util/text/env_vars_test.go
@@ -7,21 +7,24 @@ import (
 
 func TestEnvVarDetection(t *testing.T) {
 	tests := map[string]bool{
-		"foo ${BENTHOS_TEST_FOO:bar} baz":           true,
-		"foo ${BENTHOS_TEST_FOO:} baz":              false,
-		"foo ${BENTHOS_TEST_FOO} baz":               true,
-		"foo ${BENTHOS_TEST_FOO} baz ${and_this}":   true,
-		"foo $BENTHOS_TEST_FOO} baz $but_not_this}": false,
-		"foo ${BENTHOS_TEST_FOO baz ${or_this":      false,
-		"nothing $ here boss {}":                    false,
-		"foo ${BENTHOS_TEST_FOO:barthisdoesntend":   false,
-		"foo ${{BENTHOS_TEST_FOO:bar}} baz":         true,
-		"foo ${{BENTHOS_TEST_FOO:bar} baz":          false,
-		"foo ${foo.bar} baz":                        true,
-		"foo ${foo.bar^} baz":                       false,
-		"foo ${foo.bar:} baz":                       false,
-		"foo ${foo.bar:bar} baz":                    true,
-		"foo ${foo*bar} baz":                        false,
+		"foo ${BENTHOS__TEST__FOO:bar} baz":             true,
+		"foo ${BENTHOS__TEST__FOO:} baz":                false,
+		"foo ${BENTHOS__TEST__FOO} baz":                 true,
+		"foo ${BENTHOS__TEST__FOO} baz ${and__this}":    true,
+		"foo $BENTHOS__TEST__FOO} baz $but__not__this}": false,
+		"foo ${BENTHOS__TEST__FOO baz ${or__this":       false,
+		"nothing $ here boss {}":                        false,
+		"foo ${BENTHOS__TEST__FOO:barthisdoesntend":     false,
+		"foo ${{BENTHOS__TEST__FOO:bar}} baz":           true,
+		"foo ${{BENTHOS__TEST__FOO:bar} baz":            false,
+		"foo ${foo.bar} baz":                            true,
+		"foo ${foo.bar^} baz":                           false,
+		"foo ${foo.bar:} baz":                           false,
+		"foo ${foo.bar:bar} baz":                        true,
+		"foo ${foo*bar} baz":                            false,
+		"foo ${foo[0].bar} baz":                         true,
+		"foo ${foo[0]} baz":                             true,
+		"foo ${foo[0]bar} baz":                          true,
 	}
 
 	for in, exp := range tests {
@@ -33,33 +36,38 @@ func TestEnvVarDetection(t *testing.T) {
 }
 
 func TestEnvSwapping(t *testing.T) {
-	if os.Getenv("BENTHOS_TEST_FOO") != "" {
-		os.Setenv("BENTHOS_TEST_FOO", "")
+	if os.Getenv("BENTHOS__TEST__FOO") != "" {
+		os.Setenv("BENTHOS__TEST__FOO", "")
 	}
 
-	os.Setenv("BENTHOS.TEST.FOO", "testfoo")
-	os.Setenv("BENTHOS.TEST.BAR", "test\nbar")
+	os.Setenv("BENTHOS_TEST_FOO", "testfoo")
+	os.Setenv("BENTHOS_TEST_BAR", "test\nbar")
+	os.Setenv("BENTHOS_TEST_JSON", "{\"foo\": [{\"bar\": \"baz\"}]}")
 
 	tests := map[string]string{
-		"foo ${BENTHOS_TEST_FOO:bar} baz":                    "foo bar baz",
-		"foo ${BENTHOS.TEST.FOO:bar} baz":                    "foo testfoo baz",
-		"foo ${BENTHOS.TEST.FOO} baz":                        "foo testfoo baz",
-		"foo ${BENTHOS_TEST_FOO:http://bar.com} baz":         "foo http://bar.com baz",
-		"foo ${BENTHOS_TEST_FOO:http://bar.com?wat=nuh} baz": "foo http://bar.com?wat=nuh baz",
-		"foo ${BENTHOS_TEST_FOO:http://bar.com#wat} baz":     "foo http://bar.com#wat baz",
-		"foo ${BENTHOS_TEST_FOO:tcp://*:2020} baz":           "foo tcp://*:2020 baz",
-		"foo ${BENTHOS_TEST_FOO:bar} http://bar.com baz":     "foo bar http://bar.com baz",
-		"foo ${BENTHOS_TEST_FOO} http://bar.com baz":         "foo  http://bar.com baz",
-		"foo ${BENTHOS_TEST_FOO:wat@nuh.com} baz":            "foo wat@nuh.com baz",
-		"foo ${} baz":                                                              "foo ${} baz",
-		"foo ${BENTHOS_TEST_FOO:foo,bar} baz":                                      "foo foo,bar baz",
-		"foo ${BENTHOS_TEST_FOO} baz":                                              "foo  baz",
-		"foo ${BENTHOS_TEST_FOO:${!metadata:foo}} baz":                             "foo ${!metadata:foo} baz",
-		"foo ${BENTHOS_TEST_FOO:${!metadata:foo}${!metadata:bar}} baz":             "foo ${!metadata:foo}${!metadata:bar} baz",
-		"foo ${BENTHOS_TEST_FOO:${!count:foo}-${!timestamp_unix_nano}.tar.gz} baz": "foo ${!count:foo}-${!timestamp_unix_nano}.tar.gz baz",
-		"foo ${{BENTHOS_TEST_FOO:bar}} baz":                                        "foo ${BENTHOS_TEST_FOO:bar} baz",
-		"foo ${{BENTHOS_TEST_FOO}} baz":                                            "foo ${BENTHOS_TEST_FOO} baz",
-		"foo ${BENTHOS.TEST.BAR} baz":                                              "foo test\\nbar baz",
+		"foo ${BENTHOS__TEST__FOO:bar} baz":                    "foo bar baz",
+		"foo ${BENTHOS_TEST_FOO:bar} baz":                      "foo testfoo baz",
+		"foo ${BENTHOS_TEST_FOO} baz":                          "foo testfoo baz",
+		"foo ${BENTHOS__TEST__FOO:http://bar_com} baz":         "foo http://bar_com baz",
+		"foo ${BENTHOS__TEST__FOO:http://bar_com?wat=nuh} baz": "foo http://bar_com?wat=nuh baz",
+		"foo ${BENTHOS__TEST__FOO:http://bar_com#wat} baz":     "foo http://bar_com#wat baz",
+		"foo ${BENTHOS__TEST__FOO:tcp://*:2020} baz":           "foo tcp://*:2020 baz",
+		"foo ${BENTHOS__TEST__FOO:bar} http://bar_com baz":     "foo bar http://bar_com baz",
+		"foo ${BENTHOS__TEST__FOO} http://bar_com baz":         "foo  http://bar_com baz",
+		"foo ${BENTHOS__TEST__FOO:wat@nuh_com} baz":            "foo wat@nuh_com baz",
+		"foo ${} baz":                                                                  "foo ${} baz",
+		"foo ${BENTHOS__TEST__FOO:foo,bar} baz":                                        "foo foo,bar baz",
+		"foo ${BENTHOS__TEST__FOO} baz":                                                "foo  baz",
+		"foo ${BENTHOS__TEST__FOO:${!metadata:foo}} baz":                               "foo ${!metadata:foo} baz",
+		"foo ${BENTHOS__TEST__FOO:${!metadata:foo}${!metadata:bar}} baz":               "foo ${!metadata:foo}${!metadata:bar} baz",
+		"foo ${BENTHOS__TEST__FOO:${!count:foo}-${!timestamp__unix__nano}_tar_gz} baz": "foo ${!count:foo}-${!timestamp__unix__nano}_tar_gz baz",
+		"foo ${{BENTHOS__TEST__FOO:bar}} baz":                                          "foo ${BENTHOS__TEST__FOO:bar} baz",
+		"foo ${{BENTHOS__TEST__FOO}} baz":                                              "foo ${BENTHOS__TEST__FOO} baz",
+		"foo ${BENTHOS_TEST_BAR} baz":                                                  "foo test\\nbar baz",
+		"foo ${BENTHOS_TEST_JSON} baz":                                                 "foo {\"foo\": [{\"bar\": \"baz\"}]} baz",
+		"foo ${BENTHOS_TEST_BAR.foo[0]} baz":                                           "foo [{\"bar\": \"baz\"}] baz",
+		"foo ${BENTHOS_TEST_BAR.foo[0].bar} baz":                                       "foo baz baz",
+		"foo ${BENTHOS_TEST_BAR.foo[1].bar:nope} baz":                                  "foo nope baz",
 	}
 
 	for in, exp := range tests {
