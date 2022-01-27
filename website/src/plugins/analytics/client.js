d84dfad4108b9260a116821131ffad8ca94d09ec
Ashley Jeffs <ash@jeffail.uk>
2022-01-27 09:00:57 +0000

    Check exec env

--- website/src/plugins/analytics/client.js
+++ website/src/plugins/analytics/client.js
@@ -1,16 +1,20 @@
-const host = window.location.hostname;
-
-function poke(path) {
+function poke(host, path) {
   // TODO: Allow this to be configured
   fetch(`https://poke.benthos.dev/poke?h=${encodeURIComponent(host)}&p=${encodeURIComponent(path)}`, { method: "POST" })
     .catch((error) => console.error(error))
 }
 
 module.exports = (() => {
-  poke(window.location.pathname);
+  if (typeof window !== "object") {
+    return {};
+  }
+
+  const host = window.location.hostname;
+
+  poke(host, window.location.pathname);
   return {
     onRouteUpdate({location}) {
-      poke(location.pathname);
+      poke(host, location.pathname);
     },
   };
 })();
