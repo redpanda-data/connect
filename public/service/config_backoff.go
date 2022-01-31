00484395c256bc92159d300b5052556700a76009
Ashley Jeffs <ash@jeffail.uk>
2022-01-31 09:34:17 +0000

    Add optional defaults to backoff field

--- public/service/config_backoff.go
+++ public/service/config_backoff.go
@@ -13,10 +13,25 @@ import (
 // maximum elapsed time set). In cases where this would be problematic the field
 // allowUnbounded should be set `false` in order to add linting rules that
 // ensure an upper bound is set.
-func NewBackOffField(name string, allowUnbounded bool) *ConfigField {
+//
+// The defaults struct is optional, and if provided will be used to establish
+// default values for time interval fields. Otherwise the chosen defaults result
+// in one minute of retry attempts, starting at 500ms intervals.
+func NewBackOffField(name string, allowUnbounded bool, defaults *backoff.ExponentialBackOff) *ConfigField {
+	var (
+		initDefault       = "500ms"
+		maxDefault        = "10s"
+		maxElapsedDefault = "1m"
+	)
+	if defaults != nil {
+		initDefault = defaults.InitialInterval.String()
+		maxDefault = defaults.MaxInterval.String()
+		maxElapsedDefault = defaults.MaxElapsedTime.String()
+	}
+
 	maxElapsedTime := NewDurationField("max_elapsed_time").
 		Description("The maximum overall period of time to spend on retry attempts before the request is aborted.").
-		Default("15m").Example("1m").Example("1h")
+		Default(maxElapsedDefault).Example("1m").Example("1h")
 	if allowUnbounded {
 		maxElapsedTime.field.Description += " Setting this value to a zeroed duration (such as `0s`) will result in unbounded retries."
 	}
@@ -25,12 +40,12 @@ func NewBackOffField(name string, allowUnbounded bool) *ConfigField {
 	return NewObjectField(name,
 		NewDurationField("initial_interval").
 			Description("The initial period to wait between retry attempts.").
-			Default("500ms").Example("50ms").Example("1s"),
+			Default(initDefault).Example("50ms").Example("1s"),
 		NewDurationField("max_interval").
 			Description("The maximum period to wait between retry attempts").
-			Default("60s").Example("5s").Example("1m"),
+			Default(maxDefault).Example("5s").Example("1m"),
 		maxElapsedTime,
-	)
+	).Description("Determine time intervals and cut offs for retry attempts.")
 }
 
 // FieldBackOff accesses a field from a parsed config that was defined with
