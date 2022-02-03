583bc7aab9b6657650ccf9b1bc8ba76fcd2f295b
Ashley Jeffs <ash@jeffail.uk>
2022-02-03 00:48:03 +0000

    Upgrade docusaurus

--- website/src/theme/CodeSnippet/index.js
+++ website/src/theme/CodeSnippet/index.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState, useRef} from 'react';
 import clsx from 'clsx';
 import Highlight, {defaultProps} from 'prism-react-renderer';
 import copy from 'copy-text-to-clipboard';
-import useThemeContext from '@theme/hooks/useThemeContext';
+import {useColorMode} from '@docusaurus/theme-common';
 import Translate, {translate} from '@docusaurus/Translate';
 import Link from '@docusaurus/Link';
 
@@ -31,7 +31,7 @@ function CodeSnippet(props) {
     setMounted(true);
   }, []);
 
-  const {isDarkTheme} = useThemeContext();
+  const {isDarkTheme} = useColorMode();
   const lightModeTheme = prism.theme || monokai;
   const darkModeTheme = prism.darkTheme || lightModeTheme;
   const prismTheme = isDarkTheme ? darkModeTheme : lightModeTheme;
