5671a1b0ce75a4dbd7e0e528779ef495fd5025b1
Steve McDaniel <steve@klumzy.com>
2022-02-05 21:16:35 -0500

    Added batching support in DispatchMessage

--- lib/pipeline/processor.go
+++ lib/pipeline/processor.go
@@ -4,7 +4,7 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
-
+	"fmt"
 	"github.com/Jeffail/benthos/v3/lib/log"
 	"github.com/Jeffail/benthos/v3/lib/metrics"
 	"github.com/Jeffail/benthos/v3/lib/processor"
@@ -137,19 +137,35 @@ func (p *Processor) dispatchMessages(msgs []types.Message, ogResChan chan<- type
 		}
 	}
 
+	var batch []types.Message
+    nMessages := len(msgs)
+    nBatches := 8
+    batchSize := nMessages / nBatches
+
+    if batchSize == 0 {
+        batchSize = 1
+    }
+
 	wg := sync.WaitGroup{}
-	wg.Add(len(msgs))
+	wg.Add(nBatches)
 
-	block := make(chan struct{}, 32)
+    for i := 0; i < nBatches; i++ {
+        start := i*batchSize
+        end := (i*batchSize) + batchSize - 1
 
-	for _, msg := range msgs {
-	    block <- struct{}{}
-		go func(m types.Message) {
-			sendMsg(m)
-			wg.Done()
-		}(msg)
-		<-block
-	}
+        if i == (nBatches - 1) {
+            batch = msgs[start:]
+        } else {
+            batch = msgs[start:end]
+        }
+
+        go func(batch []types.Message) {
+            for _, m := range batch {
+          		sendMsg(m)
+            }
+            wg.Done()
+        }(batch)
+    }
 
 	wg.Wait()
 
@@ -179,7 +195,7 @@ func (p *Processor) Consume(msgs <-chan types.Transaction) error {
 // pipeline.
 func (p *Processor) TransactionChan() <-chan types.Transaction {
 	return p.messagesOut
-}
+}	
 
 // CloseAsync shuts down the pipeline and stops processing messages.
 func (p *Processor) CloseAsync() {
