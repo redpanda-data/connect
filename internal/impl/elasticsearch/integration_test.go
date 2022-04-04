2a829e10a6b18fd3434378ca13a97dfdb9c9d9a4
Ashley Jeffs <ash@jeffail.uk>
2022-04-04 23:24:07 +0100

    Re-enable ES integration tests (including v8)

--- internal/impl/elasticsearch/integration_test.go
+++ internal/impl/elasticsearch/integration_test.go
@@ -37,16 +37,97 @@ var elasticIndex = `{
 	}
 }`
 
-func TestIntegrationElasticsearch(t *testing.T) {
+func TestIntegrationElasticsearchV8(t *testing.T) {
 	integration.CheckSkip(t)
-	t.Skip("This uses a ton of memory so we don't run it by default")
 	t.Parallel()
 
 	pool, err := dockertest.NewPool("")
 	require.NoError(t, err)
 
 	pool.MaxWait = time.Second * 30
-	resource, err := pool.Run("elasticsearch", "7.17.0", []string{
+	resource, err := pool.Run("elasticsearch", "8.1.2", []string{
+		"discovery.type=single-node",
+		"xpack.security.enabled=false",
+		"xpack.security.http.ssl.enabled=false",
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, pool.Purge(resource))
+	})
+
+	var client *elastic.Client
+	if err = pool.Retry(func() error {
+		opts := []elastic.ClientOptionFunc{
+			elastic.SetURL(fmt.Sprintf("http://localhost:%v", resource.GetPort("9200/tcp"))),
+			elastic.SetHttpClient(&http.Client{
+				Timeout: time.Second,
+			}),
+			elastic.SetSniff(false),
+		}
+
+		var cerr error
+		if client, cerr = elastic.NewClient(opts...); cerr == nil {
+			_, cerr = client.
+				CreateIndex("test_conn_index").
+				Timeout("20s").
+				Body(elasticIndex).
+				Do(context.Background())
+		}
+		return cerr
+	}); err != nil {
+		t.Fatalf("Could not connect to docker resource: %s", err)
+	}
+
+	_ = resource.Expire(900)
+
+	template := `
+output:
+  elasticsearch:
+    urls:
+      - http://localhost:$PORT
+    index: $ID
+    id: ${!json("id")}
+    sniff: false
+`
+	queryGetFn := func(ctx context.Context, testID, messageID string) (string, []string, error) {
+		res, err := client.Get().
+			Index(testID).
+			Id(messageID).
+			Do(ctx)
+		if err != nil {
+			return "", nil, err
+		}
+
+		if !res.Found {
+			return "", nil, fmt.Errorf("document %v not found", messageID)
+		}
+
+		resBytes, err := res.Source.MarshalJSON()
+		if err != nil {
+			return "", nil, err
+		}
+		return string(resBytes), nil, nil
+	}
+
+	suite := integration.StreamTests(
+		integration.StreamTestOutputOnlySendSequential(10, queryGetFn),
+		integration.StreamTestOutputOnlySendBatch(10, queryGetFn),
+	)
+	suite.Run(
+		t, template,
+		integration.StreamTestOptPort(resource.GetPort("9200/tcp")),
+	)
+}
+
+func TestIntegrationElasticsearchV7(t *testing.T) {
+	integration.CheckSkip(t)
+	t.Parallel()
+
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	pool.MaxWait = time.Second * 30
+	resource, err := pool.Run("elasticsearch", "7.17.2", []string{
 		"discovery.type=single-node",
 	})
 	require.NoError(t, err)
@@ -86,7 +167,6 @@ output:
       - http://localhost:$PORT
     index: $ID
     id: ${!json("id")}
-    type: doc
     sniff: false
 `
 	queryGetFn := func(ctx context.Context, testID, messageID string) (string, []string, error) {
@@ -166,7 +246,6 @@ output:
       - http://localhost:$PORT
     index: $ID
     id: ${!json("id")}
-    type: doc
     sniff: false
 `
 	suite := integration.StreamBenchs(
