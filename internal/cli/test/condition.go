970b6e9c643a765268343dd32f4424850ca1f0ef
Nenad Stojanovikj <nst@hellofresh.com>
2022-04-11 16:36:25 +0200

    Introduce checkFrom method to ensure relative directory is correctly set

--- internal/cli/test/condition.go
+++ internal/cli/test/condition.go
@@ -248,7 +248,11 @@ type FileJSONEqualsCondition string
 
 // Check this condition against a message part.
 func (c FileJSONEqualsCondition) Check(p *message.Part) error {
-	relPath := filepath.Join("", string(c))
+	return c.checkFrom("", p)
+}
+
+func (c FileJSONEqualsCondition) checkFrom(dir string, p *message.Part) error {
+	relPath := filepath.Join(dir, string(c))
 
 	fileContent, err := os.ReadFile(relPath)
 	if err != nil {
