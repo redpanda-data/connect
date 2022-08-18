4c0295e1052a4e57cf03ede973e87e84186dba2b
ekeric13 <ekeric13@gmail.com>
2022-08-17 17:00:59 -0700

    Update test name

--- internal/impl/confluent/processor_schema_registry_decode_test.go
+++ internal/impl/confluent/processor_schema_registry_decode_test.go
@@ -217,7 +217,7 @@ func TestSchemaRegistryDecodeAvro(t *testing.T) {
 			output: `{"int_time_millis":{"int.time-millis":35245000},"long_time_micros":{"long.time-micros":20192000000000},"long_timestamp_micros":{"long.timestamp-micros":62135596800000000},"pos_0_33333333":{"bytes.decimal":"!"}}`,
 		},
 		{
-			name:   "successful message with logical raw json",
+			name:   "successful message with logical types raw json",
 			input:  "\x00\x00\x00\x00\x04\x02\x90\xaf\xce!\x02\x80\x80揪\x97\t\x02\x80\x80\xde\xf2\xdf\xff\xdf\xdc\x01\x02\x02!",
 			output: `{"int_time_millis":{"int.time-millis":35245000},"long_time_micros":{"long.time-micros":20192000000000},"long_timestamp_micros":{"long.timestamp-micros":62135596800000000},"pos_0_33333333":{"bytes.decimal":"!"}}`,
 		},
