2cc72e4b9dd703e526a21265828fc4b1cf2dd633
Tanner Prestegard <tprestegard@users.noreply.github.com>
2022-08-25 14:34:35 -0500

    Add individual public packages for components (#1411)
    * Add public nats package
    
    * Add public memcached package
    
    * Add public amqp packages
    
    * Add public avro package
    
    * Add public awk package
    
    * Add public azure package
    
    * Add public cassandra package
    
    * Add public confluent package
    
    * Add public dgraph package
    
    * Add public elasticsearch package
    
    * Add public gcp package
    
    * Add public hdfs package
    
    * Add public influxdb package
    
    * Add public jaeger package
    
    * Add public maxmind package
    
    * Add public mongodb package
    
    * Add public mqtt package
    
    * Add public nanomsg package
    
    * Add public nsq package
    
    * Add public otlp package
    
    * Add public prometheus package
    
    * Add public pusher package
    
    * Add public redis package
    
    * Add public sftp package
    
    * Add public snowflake package
    
    * Add public sql package
    
    * Add public statsd package
    
    * Add public jsonpath package

---
+++ public/components/avro/package.go
@@ -0,0 +1,6 @@
+package avro
+
+import (
+	// Bring in the internal plugin definitions.
+	_ "github.com/benthosdev/benthos/v4/internal/impl/avro"
+)
---
+++ public/components/statsd/package.go
@@ -0,0 +1,6 @@
+package statsd
+
+import (
+	// Bring in the internal plugin definitions.
+	_ "github.com/benthosdev/benthos/v4/internal/impl/statsd"
+)
