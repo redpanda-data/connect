dadba954c670fb0565218ded581b99fd122e027a
mkvolkov <mk.volkov34@gmail.com>
2022-09-09 14:24:03 +0300

    newest public config API: input changed

--- internal/impl/beanstalkd/input.go
+++ internal/impl/beanstalkd/input.go
@@ -2,79 +2,75 @@ package beanstalkd
 
 import (
 	"context"
-	"strconv"
 	"sync"
 	"time"
 
 	"github.com/beanstalkd/go-beanstalk"
 
-	"github.com/benthosdev/benthos/v4/internal/bundle"
-	"github.com/benthosdev/benthos/v4/internal/component/input"
-	"github.com/benthosdev/benthos/v4/internal/component/input/processors"
-	"github.com/benthosdev/benthos/v4/internal/docs"
-	"github.com/benthosdev/benthos/v4/internal/log"
-	"github.com/benthosdev/benthos/v4/internal/message"
+	"github.com/benthosdev/benthos/v4/public/service"
 )
 
+func beanstalkdInputConfig() *service.ConfigSpec {
+	return service.NewConfigSpec().
+			Categories("Services").
+			Version("3.46.0").
+			Summary("Reads messages from Beanstalkd queue.").
+			Field(service.NewStringField("tcp_address").
+					Description("Beanstalkd address to connect to.").
+					Example("127.0.0.1:11300"))
+}
+
 func init() {
-	err := bundle.AllInputs.Add(processors.WrapConstructor(newBeanstalkdInput), docs.ComponentSpec{
-		Name:    "beanstalkd",
-		Summary: `Subscribe to a beanstalked instance.`,
-		Config: docs.FieldComponent().WithChildren(
-			docs.FieldString("tcp_address", "Beanstalkd address to connect to."),
-		).ChildDefaultAndTypesFromStruct(input.NewBeanstalkdConfig()),
-		Categories: []string{
-			"Services",
-		},
-	})
+	err := service.RegisterInput(
+			"beanstalkd", beanstalkdInputConfig(),
+			func(conf *service.ParsedConfig, mgr *service.Resources) (service.Input, error) {
+				return newBeanstalkdReaderFromConfig(conf, mgr.Logger())
+			})
+
 	if err != nil {
 		panic(err)
 	}
 }
 
-func newBeanstalkdInput(conf input.Config, mgr bundle.NewManagement) (input.Streamed, error) {
-	var bs input.Async
-	var err error
-	if bs, err = newBeanstalkdReader(conf.Beanstalkd, mgr.Logger()); err != nil {
-		return nil, err
-	}
-	return input.NewAsyncReader("beanstalkd", true, bs, mgr)
-}
-
 type beanstalkdReader struct {
-	connection *beanstalk.Conn
-	bMut       sync.Mutex
+	connection	*beanstalk.Conn
+	connMut		sync.Mutex
 
-	conf input.BeanstalkdConfig
-	log  log.Modular
+	address	string
+	log		*service.Logger
 }
 
-func newBeanstalkdReader(conf input.BeanstalkdConfig, log log.Modular) (*beanstalkdReader, error) {
+func newBeanstalkdReaderFromConfig(conf *service.ParsedConfig, log *service.Logger) (*beanstalkdReader, error) {
 	bs := beanstalkdReader{
-		conf: conf,
 		log:  log,
 	}
+
+	tcpAddr, err := conf.FieldString("tcp_address")
+	if err != nil {
+		return nil, err
+	}
+	bs.address = tcpAddr
 
 	return &bs, nil
 }
 
-func (bs *beanstalkdReader) Connect(ctx context.Context) (err error) {
-	bs.bMut.Lock()
-	defer bs.bMut.Unlock()
+func (bs *beanstalkdReader) Connect(ctx context.Context) error {
+	bs.connMut.Lock()
+	defer bs.connMut.Unlock()
 
-	conn, err := beanstalk.Dial("tcp", bs.conf.Address)
+	conn, err := beanstalk.Dial("tcp", bs.address)
 	if err != nil {
-		return
+		return err
 	}
 
 	bs.connection = conn
-	bs.log.Infof("Receiving Beanstalkd messages from address: %s\n", bs.conf.Address)
-	return
+	bs.log.Infof("Receiving Beanstalkd messages from address: %s\n", bs.address)
+	return nil
 }
 
 func (bs *beanstalkdReader) disconnect() error {
-	bs.bMut.Lock()
-	defer bs.bMut.Unlock()
+	bs.connMut.Lock()
+	defer bs.connMut.Unlock()
 
 	if bs.connection != nil {
 		if err := bs.connection.Close(); err != nil {
@@ -85,36 +81,27 @@ func (bs *beanstalkdReader) disconnect() error {
 	return nil
 }
 
-func (bs *beanstalkdReader) ReadBatch(ctx context.Context) (message.Batch, input.AsyncAckFn, error) {
-	var mpStats map[string]string
-	res := make([][]byte, 0)
-
-	mpStats, err := bs.connection.Stats()
-	if err != nil {
-		return nil, nil, err
+func (bs *beanstalkdReader) Read(ctx context.Context) (*service.Message, service.AckFunc, error) {
+	if bs.connection == nil {
+		return nil, nil, service.ErrNotConnected
 	}
 
-	jobs, err := strconv.Atoi(mpStats["current-jobs-ready"])
-	if err != nil {
-		return nil, nil, err
-	}
-
-	for i := 0; i < jobs; i++ {
+	for {
 		id, body, err := bs.connection.Reserve(time.Millisecond * 200)
 		if err != nil {
 			return nil, nil, err
 		}
 
-		res = append(res, body)
 		err = bs.connection.Delete(id)
 		if err != nil {
 			return nil, nil, err
 		}
-	}
 
-	return message.QuickBatch(res), func(ctx context.Context, res error) error {
-		return nil
-	}, nil
+		msg := service.NewMessage(body)
+		return msg, func(ctx context.Context, res error) error {
+			return nil
+		}, nil
+	}
 }
 
 func (bs *beanstalkdReader) Close(ctx context.Context) (err error) {
