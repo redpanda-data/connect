1077bfe90141fbe8d00d47c4749d8156259b829d
mkvolkov <mk.volkov34@gmail.com>
2022-09-12 13:58:34 +0300

    four tests

--- internal/impl/beanstalkd/integration_test.go
+++ internal/impl/beanstalkd/integration_test.go
@@ -11,9 +11,45 @@ import (
 	"github.com/benthosdev/benthos/v4/internal/integration"
 )
 
-func TestIntegrationBeanstalkd(t *testing.T) {
+func TestIntegrationBeanstalkdOpenClose(t *testing.T) {
 	integration.CheckSkip(t)
-	t.Parallel()
+
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	pool.MaxWait = time.Second * 30
+	resource, err := pool.Run("websmurf/beanstalkd", "1.12-alpine-3.14", nil)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, pool.Purge(resource))
+	})
+
+	_ = resource.Expire(900)
+	require.NoError(t, pool.Retry(func() error {
+		return nil
+	}))
+
+	template := `
+output:
+  beanstalkd:
+    tcp_address: localhost:$PORT
+    max_in_flight: $MAX_IN_FLIGHT
+
+input:
+  beanstalkd:
+    tcp_address: localhost:$PORT
+`
+	suite := integration.StreamTests(
+		integration.StreamTestOpenClose(),
+	)
+	suite.Run(
+		t, template,
+		integration.StreamTestOptPort(resource.GetPort("11300/tcp")),
+	)
+}
+
+func TestIntegrationBeanstalkdSendBatch(t *testing.T) {
+	integration.CheckSkip(t)
 
 	pool, err := dockertest.NewPool("")
 	require.NoError(t, err)
@@ -42,6 +78,80 @@ input:
 `
 	suite := integration.StreamTests(
 		integration.StreamTestSendBatch(10),
+	)
+	suite.Run(
+		t, template,
+		integration.StreamTestOptPort(resource.GetPort("11300/tcp")),
+	)
+}
+
+func TestIntegrationBeanstalkdStreamSequential(t *testing.T) {
+	integration.CheckSkip(t)
+
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	pool.MaxWait = time.Second * 30
+	resource, err := pool.Run("websmurf/beanstalkd", "1.12-alpine-3.14", nil)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, pool.Purge(resource))
+	})
+
+	_ = resource.Expire(900)
+	require.NoError(t, pool.Retry(func() error {
+		return nil
+	}))
+
+	template := `
+output:
+  beanstalkd:
+    tcp_address: localhost:$PORT
+    max_in_flight: $MAX_IN_FLIGHT
+
+input:
+  beanstalkd:
+    tcp_address: localhost:$PORT
+`
+	suite := integration.StreamTests(
+		integration.StreamTestStreamSequential(100),
+	)
+	suite.Run(
+		t, template,
+		integration.StreamTestOptPort(resource.GetPort("11300/tcp")),
+	)
+}
+
+func TestIntegrationBeanstalkdStreamParallel(t *testing.T) {
+	integration.CheckSkip(t)
+
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	pool.MaxWait = time.Second * 30
+	resource, err := pool.Run("websmurf/beanstalkd", "1.12-alpine-3.14", nil)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, pool.Purge(resource))
+	})
+
+	_ = resource.Expire(900)
+	require.NoError(t, pool.Retry(func() error {
+		return nil
+	}))
+
+	template := `
+output:
+  beanstalkd:
+    tcp_address: localhost:$PORT
+    max_in_flight: $MAX_IN_FLIGHT
+
+input:
+  beanstalkd:
+    tcp_address: localhost:$PORT
+`
+	suite := integration.StreamTests(
+		integration.StreamTestStreamParallel(100),
 	)
 	suite.Run(
 		t, template,
