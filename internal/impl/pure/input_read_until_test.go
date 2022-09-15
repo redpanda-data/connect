d22234bce0aeb71de61bcf9d2c446694ec27b730
Ashley Jeffs <ash@jeffail.uk>
2022-09-15 19:44:46 +0100

    Fix read_until connect check on terminated input

--- internal/impl/pure/input_read_until_test.go
+++ internal/impl/pure/input_read_until_test.go
@@ -54,6 +54,9 @@ baz`)
 	t.Run("ReadUntilBasic", func(te *testing.T) {
 		testReadUntilBasic(inconf, te)
 	})
+	t.Run("ReadUntilRestart", func(te *testing.T) {
+		testReadUntilRestart(inconf, te)
+	})
 	t.Run("ReadUntilRetry", func(te *testing.T) {
 		testReadUntilRetry(inconf, te)
 	})
@@ -116,6 +119,46 @@ func testReadUntilBasic(inConf input.Config, t *testing.T) {
 	if err = in.WaitForClose(ctx); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func testReadUntilRestart(inConf input.Config, t *testing.T) {
+	ctx, done := context.WithTimeout(context.Background(), time.Second*5)
+	defer done()
+
+	rConf := input.NewConfig()
+	rConf.Type = "read_until"
+	rConf.ReadUntil.Input = &inConf
+	rConf.ReadUntil.Check = `false`
+	rConf.ReadUntil.Restart = true
+
+	in, err := bmock.NewManager().NewInput(rConf)
+	require.NoError(t, err)
+
+	expMsgs := []string{
+		"foo",
+		"bar",
+		"baz",
+	}
+
+	for i := 0; i < 3; i++ {
+		for _, expMsg := range expMsgs {
+			var tran message.Transaction
+			var open bool
+			select {
+			case tran, open = <-in.TransactionChan():
+				require.True(t, open)
+			case <-time.After(time.Second):
+				t.Fatal("timed out")
+			}
+
+			require.Len(t, tran.Payload, 1)
+			assert.Equal(t, expMsg, string(tran.Payload[0].AsBytes()))
+			require.NoError(t, tran.Ack(ctx, nil))
+		}
+	}
+
+	in.TriggerStopConsuming()
+	require.NoError(t, in.WaitForClose(ctx))
 }
 
 func testReadUntilRetry(inConf input.Config, t *testing.T) {
