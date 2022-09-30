082eb3db5b94c743697e8f0fa1a3f160ed254fb0
Tiago Peczenyj <tpeczenyj@weborama.com>
2022-09-30 14:52:35 +0200

    add integration tests

--- internal/impl/cassandra/integration_test.go
+++ internal/impl/cassandra/integration_test.go
@@ -16,6 +16,8 @@ import (
 	"github.com/benthosdev/benthos/v4/internal/integration"
 )
 
+const maxWait = 120 * time.Second
+
 func TestIntegrationCassandra(t *testing.T) {
 	integration.CheckSkip(t)
 	if runtime.GOOS == "darwin" {
@@ -27,7 +29,8 @@ func TestIntegrationCassandra(t *testing.T) {
 	pool, err := dockertest.NewPool("")
 	require.NoError(t, err)
 
-	pool.MaxWait = time.Second * 60
+	pool.MaxWait = maxWait
+
 	resource, err := pool.Run("cassandra", "latest", nil)
 	require.NoError(t, err)
 	t.Cleanup(func() {
@@ -41,14 +44,22 @@ func TestIntegrationCassandra(t *testing.T) {
 		}
 	})
 
+	t.Logf("may wait %s to connect on cassandra docker id %s",
+		pool.MaxWait, resource.Container.ID)
+
 	_ = resource.Expire(900)
 	require.NoError(t, pool.Retry(func() error {
 		if session == nil {
-			conn := gocql.NewCluster(fmt.Sprintf("localhost:%v", resource.GetPort("9042/tcp")))
+			host := fmt.Sprintf("localhost:%v", resource.GetPort("9042/tcp"))
+
+			conn := gocql.NewCluster(host)
 			conn.Consistency = gocql.All
+
+			t.Logf("will try to connect on cassandra %s", host)
+
 			var rerr error
 			if session, rerr = conn.CreateSession(); rerr != nil {
-				return rerr
+				return fmt.Errorf("unable to create session on host %s: %w", host, rerr)
 			}
 		}
 		_ = session.Query(
@@ -146,30 +157,75 @@ output:
 		)
 	})
 
-	t.Run("with old style values", func(t *testing.T) {
+	t.Run("with values and batching (unlogged batch)", func(t *testing.T) {
 		template := `
 output:
   cassandra:
     addresses:
       - localhost:$PORT
-    query: 'INSERT INTO testspace.table$ID (id, content, created_at) VALUES (?, ?, ?)'
-    args:
-      - ${! json("id") }
-      - ${! json("content") }
-      - ${! timestamp("2006-01-02T15:04:05.999Z07:00") }
+    query: 'INSERT INTO testspace.table$ID (id, content, created_at, meows) VALUES (?, ?, ?, ?)'
+    args_mapping: |
+      root = [ this.id, this.content, now(), [ "first meow", "second meow" ] ]
+    batching:
+      period: 50ms
+      count: 2
 `
 		queryGetFn := func(ctx context.Context, testID, messageID string) (string, []string, error) {
 			var resID int
 			var resContent string
 			var createdAt time.Time
+			var meows []string
 			if err := session.Query(
-				fmt.Sprintf("select id, content, created_at from testspace.table%v where id = ?;", testID), messageID,
-			).Scan(&resID, &resContent, &createdAt); err != nil {
+				fmt.Sprintf("select id, content, created_at, meows from testspace.table%v where id = ?;", testID), messageID,
+			).Scan(&resID, &resContent, &createdAt, &meows); err != nil {
 				return "", nil, err
 			}
 			if time.Since(createdAt) > time.Hour || time.Since(createdAt) < 0 {
 				return "", nil, fmt.Errorf("received bad created_at: %v", createdAt)
 			}
+			assert.Equal(t, []string{"first meow", "second meow"}, meows)
+			return fmt.Sprintf(`{"content":"%v","id":%v}`, resContent, resID), nil, err
+		}
+		suite := integration.StreamTests(
+			integration.StreamTestOutputOnlySendSequential(10, queryGetFn),
+			integration.StreamTestOutputOnlySendBatch(10, queryGetFn),
+		)
+		suite.Run(
+			t, template,
+			integration.StreamTestOptSleepAfterInput(500*time.Millisecond),
+			integration.StreamTestOptSleepAfterOutput(500*time.Millisecond),
+			integration.StreamTestOptPort(resource.GetPort("9042/tcp")),
+		)
+	})
+
+	t.Run("with values and batching (async batch)", func(t *testing.T) {
+		template := `
+output:
+  cassandra:
+    addresses:
+      - localhost:$PORT
+    query: 'INSERT INTO testspace.table$ID (id, content, created_at, meows) VALUES (?, ?, ?, ?)'
+    args_mapping: |
+      root = [ this.id, this.content, now(), [ "first meow", "second meow" ] ]
+    async_batch: true
+    batching:
+      period: 50ms
+      count: 2
+`
+		queryGetFn := func(ctx context.Context, testID, messageID string) (string, []string, error) {
+			var resID int
+			var resContent string
+			var createdAt time.Time
+			var meows []string
+			if err := session.Query(
+				fmt.Sprintf("select id, content, created_at, meows from testspace.table%v where id = ?;", testID), messageID,
+			).Scan(&resID, &resContent, &createdAt, &meows); err != nil {
+				return "", nil, err
+			}
+			if time.Since(createdAt) > time.Hour || time.Since(createdAt) < 0 {
+				return "", nil, fmt.Errorf("received bad created_at: %v", createdAt)
+			}
+			assert.Equal(t, []string{"first meow", "second meow"}, meows)
 			return fmt.Sprintf(`{"content":"%v","id":%v}`, resContent, resID), nil, err
 		}
 		suite := integration.StreamTests(
@@ -185,7 +241,7 @@ output:
 				vars.ID = strings.ReplaceAll(testID, "-", "")
 				require.NoError(t, session.Query(
 					fmt.Sprintf(
-						"CREATE TABLE testspace.table%v (id int primary key, content text, created_at timestamp);",
+						"CREATE TABLE testspace.table%v (id int primary key, content text, created_at timestamp, meows list<text>);",
 						vars.ID,
 					),
 				).Exec())
