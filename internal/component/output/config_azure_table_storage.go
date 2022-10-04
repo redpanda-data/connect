ce70cdd18cdcc9dbb86cb2c862fd75424b1d5290
Marco Amador <amador.marco@gmail.com>
2022-10-04 10:58:02 +0100

    Deprecate `insert_type` field and make it interpolated as it was supposed to be and add new `transaction_type` field with 3 more supported operations

--- internal/component/output/config_azure_table_storage.go
+++ internal/component/output/config_azure_table_storage.go
@@ -12,6 +12,7 @@ type AzureTableStorageConfig struct {
 	RowKey                  string             `json:"row_key" yaml:"row_key"`
 	Properties              map[string]string  `json:"properties" yaml:"properties"`
 	InsertType              string             `json:"insert_type" yaml:"insert_type"`
+	TransactionType         string             `json:"transaction_type" yaml:"transaction_type"`
 	Timeout                 string             `json:"timeout" yaml:"timeout"`
 	MaxInFlight             int                `json:"max_in_flight" yaml:"max_in_flight"`
 	Batching                batchconfig.Config `json:"batching" yaml:"batching"`
@@ -28,6 +29,7 @@ func NewAzureTableStorageConfig() AzureTableStorageConfig {
 		RowKey:                  "",
 		Properties:              map[string]string{},
 		InsertType:              "INSERT",
+		TransactionType:         "",
 		Timeout:                 "5s",
 		MaxInFlight:             64,
 		Batching:                batchconfig.NewConfig(),
