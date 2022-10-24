3dd60b3bb54717b2223f096283598c304f77911c
Hagelstein, Kai <kai.hagelstein@spiegel.de>
2022-10-24 14:17:22 +0200

    Add comments for exported struct and methods

--- internal/impl/javascript/logger.go
+++ internal/impl/javascript/logger.go
@@ -2,18 +2,22 @@ package javascript
 
 import "github.com/benthosdev/benthos/v4/public/service"
 
+// Logger wraps the service.Logger so that we can define the below methods.
 type Logger struct {
 	l *service.Logger
 }
 
+// Log will be used for "console.log()" in JS
 func (l *Logger) Log(message string) {
 	l.l.Info(message)
 }
 
+// Warn will be used for "console.warn()" in JS
 func (l *Logger) Warn(message string) {
 	l.l.Warn(message)
 }
 
+// Error will be used for "console.error()" in JS
 func (l *Logger) Error(message string) {
 	l.l.Error(message)
 }
