c291fbfe182ef63e1cb76e608bb5ab4477ec9c6c
Jonas Kint <kint.jonas@gmail.com>
2022-10-27 13:45:58 +0200

    Enabling pulsar input

--- cmd/benthos/main.go
+++ cmd/benthos/main.go
@@ -5,6 +5,7 @@ import (
 
 	// Import all plugins defined within the repo.
 	_ "github.com/benthosdev/benthos/v4/public/components/all"
+	_ "github.com/benthosdev/benthos/v4/public/components/pulsar"
 )
 
 func main() {
