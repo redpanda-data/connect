49461e177aae9d474587e633647354d6a94d97a4
xiaolei.wang <xiaolei.wang@appshahe.com>
2023-01-06 15:32:17 +0800

    feat: svcdiscover config

--- internal/svcdiscover/config.go
+++ internal/svcdiscover/config.go
@@ -1,10 +1,5 @@
 package svcdiscover
 
-import (
-	"github.com/benthosdev/benthos/v4/internal/docs"
-	yaml "gopkg.in/yaml.v3"
-)
-
 type Config struct {
 	Label   string      `json:"label" yaml:"label"`
 	Type    string      `json:"type" yaml:"type"`
@@ -21,34 +16,4 @@ func NewConfig() Config {
 		Plugin:  nil,
 		Nacos:   NewNacosConfig(),
 	}
-}
-
-// UnmarshalYAML ensures that when parsing configs that are in a slice the
-// default values are still applied.
-func (conf *Config) UnmarshalYAML(value *yaml.Node) error {
-	type confAlias Config
-	aliased := confAlias(NewConfig())
-
-	err := value.Decode(&aliased)
-	if err != nil {
-		return docs.NewLintError(value.Line, docs.LintFailedRead, err.Error())
-	}
-
-	var spec docs.ComponentSpec
-	if aliased.Type, spec, err = docs.GetInferenceCandidateFromYAML(docs.DeprecatedProvider, docs.TypeProcessor, value); err != nil {
-		return docs.NewLintError(value.Line, docs.LintComponentMissing, err.Error())
-	}
-
-	if spec.Plugin {
-		pluginNode, err := docs.GetPluginConfigYAML(aliased.Type, value)
-		if err != nil {
-			return docs.NewLintError(value.Line, docs.LintFailedRead, err.Error())
-		}
-		aliased.Plugin = &pluginNode
-	} else {
-		aliased.Plugin = nil
-	}
-
-	*conf = Config(aliased)
-	return nil
 }
