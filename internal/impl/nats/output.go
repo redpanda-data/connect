02de728fce4022ebc0aef50d6d7fc46f5e8060c9
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-01-13 12:45:04 -0800

    Rewrote nats output to use new service api

--- internal/impl/nats/output.go
+++ internal/impl/nats/output.go
@@ -10,100 +10,105 @@ import (
 
 	"github.com/nats-io/nats.go"
 
-	"github.com/benthosdev/benthos/v4/internal/bloblang/field"
-	"github.com/benthosdev/benthos/v4/internal/bundle"
-	"github.com/benthosdev/benthos/v4/internal/component"
-	"github.com/benthosdev/benthos/v4/internal/component/output"
-	"github.com/benthosdev/benthos/v4/internal/component/output/processors"
-	"github.com/benthosdev/benthos/v4/internal/docs"
 	"github.com/benthosdev/benthos/v4/internal/impl/nats/auth"
-	"github.com/benthosdev/benthos/v4/internal/log"
-	"github.com/benthosdev/benthos/v4/internal/message"
-	btls "github.com/benthosdev/benthos/v4/internal/tls"
 	"github.com/benthosdev/benthos/v4/public/service"
 )
 
-func init() {
-	err := bundle.AllOutputs.Add(processors.WrapConstructor(newNATSOutput), docs.ComponentSpec{
-		Name:    "nats",
-		Summary: `Publish to an NATS subject.`,
-		Description: output.Description(true, false, `
-This output will interpolate functions within the subject field, you can find a list of functions [here](/docs/configuration/interpolation#bloblang-queries).
+func natsOutputConfig() *service.ConfigSpec {
+	return service.NewConfigSpec().
+		Categories("Services").
+		Summary("Publish to an NATS subject.").
+		Description(`This output will interpolate functions within the subject field, you can find a list of functions [here](/docs/configuration/interpolation#bloblang-queries
 
-`+auth.Description()),
-		Config: docs.FieldComponent().WithChildren(
-			docs.FieldString(
-				"urls",
-				"A list of URLs to connect to. If an item of the list contains commas it will be expanded into multiple URLs.",
-				[]string{"nats://127.0.0.1:4222"},
-				[]string{"nats://username:password@127.0.0.1:4222"},
-			).Array(),
-			docs.FieldString("subject", "The subject to publish to.").IsInterpolated(),
-			docs.FieldString("headers", "Explicit message headers to add to messages.",
-				map[string]string{
-					"Content-Type": "application/json",
-					"Timestamp":    `${!meta("Timestamp")}`,
-				},
-			).IsInterpolated().Map(),
-			docs.FieldInt("max_in_flight", "The maximum number of messages to have in flight at a given time. Increase this to improve throughput."),
-			btls.FieldSpec(),
-			auth.FieldSpec(),
-		).ChildDefaultAndTypesFromStruct(output.NewNATSConfig()),
-		Categories: []string{
-			"Services",
+` + auth.Description()).
+		Field(service.NewStringListField("urls").
+			Description("A list of URLs to connect to. If an item of the list contains commas it will be expanded into multiple URLs.").
+			Example([]string{"nats://127.0.0.1:4222"}).
+			Example([]string{"nats://username:password@127.0.0.1:4222"})).
+		Field(service.NewStringField("subject").
+			Description("The subject to publish to.").
+			Example("foo.bar.baz")).
+		Field(service.NewInterpolatedStringMapField("headers").
+			Description("Explicit message headers to add to messages.").
+			Default(map[string]any{}).
+			Example(map[string]any{
+				"Content-Type": "application/json",
+				"Timestamp":    `${!meta("Timestamp")}`,
+			})).
+		Field(service.NewIntField("max_in_flight").
+			Description("The maximum number of messages to have in flight at a given time. Increase this to improve throughput.").
+			Default(64)). //TODO: Do we want to bump this up at some point?
+		Field(service.NewTLSToggledField("tls")).
+		Field(service.NewInternalField(auth.FieldSpec()))
+}
+
+func init() {
+	err := service.RegisterOutput(
+		"nats", natsOutputConfig(),
+		func(conf *service.ParsedConfig, mgr *service.Resources) (service.Output, int, error) {
+			maxInFlight, err := conf.FieldInt("max_in_flight")
+			if err != nil {
+				return nil, 0, err
+			}
+			w, err := newNATSWriter(conf, mgr)
+			return w, maxInFlight, err
 		},
-	})
+	)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func newNATSOutput(conf output.Config, mgr bundle.NewManagement) (output.Streamed, error) {
-	w, err := newNATSWriter(conf.NATS, mgr, mgr.Logger())
-	if err != nil {
-		return nil, err
-	}
-	return output.NewAsyncWriter("nats", conf.NATS.MaxInFlight, w, mgr)
-}
-
 type natsWriter struct {
-	log log.Modular
+	urls          string
+	headers       map[string]*service.InterpolatedString
+	subjectStr    *service.InterpolatedString
+	subjectStrRaw string
+	authConf      auth.Config
+	tlsConf       *tls.Config
+
+	log *service.Logger
 	fs  *service.FS
 
 	natsConn *nats.Conn
 	connMut  sync.RWMutex
-
-	urls       string
-	conf       output.NATSConfig
-	headers    map[string]*field.Expression
-	subjectStr *field.Expression
-	tlsConf    *tls.Config
 }
 
-func newNATSWriter(conf output.NATSConfig, mgr bundle.NewManagement, log log.Modular) (*natsWriter, error) {
+func newNATSWriter(conf *service.ParsedConfig, mgr *service.Resources) (*natsWriter, error) {
 	n := natsWriter{
-		log:     log,
-		fs:      service.NewFS(mgr.FS()),
-		conf:    conf,
-		headers: make(map[string]*field.Expression),
+		log:     mgr.Logger(),
+		fs:      mgr.FS(),
+		headers: make(map[string]*service.InterpolatedString),
 	}
-	var err error
-	if n.subjectStr, err = mgr.BloblEnvironment().NewField(conf.Subject); err != nil {
-		return nil, fmt.Errorf("failed to parse subject expression: %v", err)
+	urlList, err := conf.FieldStringList("urls")
+	if err != nil {
+		return nil, err
 	}
-	for k, v := range conf.Headers {
-		if n.headers[k], err = mgr.BloblEnvironment().NewField(v); err != nil {
-			return nil, fmt.Errorf("failed to parse header '%s' expresion: %v", k, err)
-		}
-	}
-	n.urls = strings.Join(conf.URLs, ",")
+	n.urls = strings.Join(urlList, ",")
 
-	if conf.TLS.Enabled {
-		if n.tlsConf, err = conf.TLS.Get(mgr.FS()); err != nil {
-			return nil, err
-		}
+	if n.subjectStrRaw, err = conf.FieldString("subject"); err != nil {
+		return nil, err
 	}
 
+	if n.subjectStr, err = conf.FieldInterpolatedString("subject"); err != nil {
+		return nil, err
+	}
+
+	if n.headers, err = conf.FieldInterpolatedStringMap("headers"); err != nil {
+		return nil, err
+	}
+
+	tlsConf, tlsEnabled, err := conf.FieldTLSToggled("tls")
+	if err != nil {
+		return nil, err
+	}
+	if tlsEnabled {
+		n.tlsConf = tlsConf
+	}
+
+	if n.authConf, err = AuthFromParsedConfig(conf.Namespace("auth")); err != nil {
+		return nil, err
+	}
 	return &n, nil
 }
 
@@ -122,63 +127,60 @@ func (n *natsWriter) Connect(ctx context.Context) error {
 		opts = append(opts, nats.Secure(n.tlsConf))
 	}
 
-	opts = append(opts, authConfToOptions(n.conf.Auth, n.fs)...)
+	opts = append(opts, authConfToOptions(n.authConf, n.fs)...)
 
 	if n.natsConn, err = nats.Connect(n.urls, opts...); err != nil {
 		return err
 	}
 
 	if err == nil {
-		n.log.Infof("Sending NATS messages to subject: %v\n", n.conf.Subject)
+		n.log.Infof("Sending NATS messages to subject: %v\n", n.subjectStrRaw)
 	}
 	return err
 }
 
-// WriteBatch attempts to write a message.
-func (n *natsWriter) WriteBatch(ctx context.Context, msg message.Batch) error {
-	return n.Write(msg)
-}
-
 // Write attempts to write a message.
-func (n *natsWriter) Write(msg message.Batch) error {
+func (n *natsWriter) Write(context context.Context, msg *service.Message) error {
 	n.connMut.RLock()
 	conn := n.natsConn
 	n.connMut.RUnlock()
 
 	if conn == nil {
-		return component.ErrNotConnected
+		return service.ErrNotConnected
 	}
 
-	return output.IterateBatchedSend(msg, func(i int, p *message.Part) error {
-		subject, err := n.subjectStr.String(i, msg)
-		if err != nil {
-			return fmt.Errorf("subject interpolation error: %w", err)
-		}
+	subject, err := n.subjectStr.TryString(msg)
+	if err != nil {
+		return fmt.Errorf("subject interpolation error: %w", err)
+	}
 
-		n.log.Debugf("Writing NATS message to topic %s", subject)
-		// fill message data
-		nMsg := nats.NewMsg(subject)
-		nMsg.Data = p.AsBytes()
-		if conn.HeadersSupported() {
-			// fill bloblang headers
-			for k, v := range n.headers {
-				headerStr, err := v.String(i, msg)
-				if err != nil {
-					return fmt.Errorf("header %v interpolation error: %w", k, err)
-				}
-				nMsg.Header.Add(k, headerStr)
-			}
-		}
-
-		if err = conn.PublishMsg(nMsg); errors.Is(err, nats.ErrConnectionClosed) {
-			conn.Close()
-			n.connMut.Lock()
-			n.natsConn = nil
-			n.connMut.Unlock()
-			return component.ErrNotConnected
-		}
+	n.log.Debugf("Writing NATS message to subject %s", subject)
+	// fill message data
+	nMsg := nats.NewMsg(subject)
+	nMsg.Data, err = msg.AsBytes()
+	if err != nil {
 		return err
-	})
+	}
+
+	if conn.HeadersSupported() {
+		// fill bloblang headers
+		for k, v := range n.headers {
+			headerStr, err := v.TryString(msg)
+			if err != nil {
+				return fmt.Errorf("header %v interpolation error: %w", k, err)
+			}
+			nMsg.Header.Add(k, headerStr)
+		}
+	}
+
+	if err = conn.PublishMsg(nMsg); errors.Is(err, nats.ErrConnectionClosed) {
+		conn.Close()
+		n.connMut.Lock()
+		n.natsConn = nil
+		n.connMut.Unlock()
+		return service.ErrNotConnected
+	}
+	return err
 }
 
 func (n *natsWriter) Close(context.Context) (err error) {
