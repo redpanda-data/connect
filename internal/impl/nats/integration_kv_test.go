058445ea42b0f0422ede47546868b8fcaacf0b33
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-01-14 13:52:55 -0800

    Integration test for kv

---
+++ internal/impl/nats/integration_kv_test.go
@@ -0,0 +1,81 @@
+package nats
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/benthosdev/benthos/v4/internal/integration"
+	"github.com/nats-io/nats.go"
+	"github.com/ory/dockertest/v3"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIntegrationNatsKV(t *testing.T) {
+	integration.CheckSkip(t)
+	t.Parallel()
+
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	pool.MaxWait = time.Second * 30
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "nats",
+		Tag:        "latest",
+		Cmd:        []string{"--js", "--trace"},
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, pool.Purge(resource))
+	})
+
+	var natsConn *nats.Conn
+	_ = resource.Expire(900)
+	require.NoError(t, pool.Retry(func() error {
+		natsConn, err = nats.Connect(fmt.Sprintf("tcp://localhost:%v", resource.GetPort("4222/tcp")))
+		return err
+	}))
+	t.Cleanup(func() {
+		natsConn.Close()
+	})
+
+	template := `
+output:
+  nats_kv:
+    urls: [ tcp://localhost:$PORT ]
+    bucket: bucket-$ID
+    key: foo
+
+input:
+  nats_kv:
+    urls: [ tcp://localhost:$PORT ]
+    bucket: bucket-$ID
+`
+	suite := integration.StreamTests(
+		integration.StreamTestOpenClose(),
+		// integration.StreamTestMetadata(), TODO
+		// integration.StreamTestSendBatch(10),
+	// integration.StreamTestStreamParallel(500),
+	// integration.StreamTestStreamParallelLossy(500),
+	)
+	suite.Run(
+		t, template,
+		integration.StreamTestOptLogging("DEBUG"),
+		integration.StreamTestOptPreTest(func(t testing.TB, _ context.Context, testID string, _ *integration.StreamTestConfigVars) {
+			js, err := natsConn.JetStream()
+			require.NoError(t, err)
+
+			bucketName := "bucket-" + testID
+
+			_, err = js.CreateKeyValue(&nats.KeyValueConfig{
+				Bucket: bucketName,
+			})
+			require.NoError(t, err)
+		}),
+		integration.StreamTestOptSleepAfterInput(100*time.Millisecond),
+		integration.StreamTestOptSleepAfterOutput(100*time.Millisecond),
+		integration.StreamTestOptPort(resource.GetPort("4222/tcp")),
+	)
+}
