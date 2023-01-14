384d605eb5da031efc8e5511d85b0f751ea80a4e
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-01-13 16:47:43 -0800

    Added config parsing test

---
+++ internal/impl/nats/input_kv_test.go
@@ -0,0 +1,41 @@
+package nats
+
+import (
+	"testing"
+
+	"github.com/benthosdev/benthos/v4/public/service"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTestInputKVParse(t *testing.T) {
+	spec := natsKVInputConfig()
+	env := service.NewEnvironment()
+
+	inputConfig := `
+urls: [ url1, url2 ]
+bucket: testbucket
+key: testkey
+ignore_deletes: true
+include_history: true
+meta_only: true
+auth:
+  nkey_file: test auth n key file
+  user_credentials_file: test auth user creds file
+`
+
+	conf, err := spec.ParseYAML(inputConfig, env)
+	require.NoError(t, err)
+
+	e, err := newKVReader(conf, service.MockResources())
+	require.NoError(t, err)
+
+	assert.Equal(t, "url1,url2", e.urls)
+	assert.Equal(t, "testbucket", e.bucket)
+	assert.Equal(t, "testkey", e.key)
+	assert.Equal(t, true, e.ignoreDeletes)
+	assert.Equal(t, true, e.includeHistory)
+	assert.Equal(t, true, e.metaOnly)
+	assert.Equal(t, "test auth n key file", e.authConf.NKeyFile)
+	assert.Equal(t, "test auth user creds file", e.authConf.UserCredentialsFile)
+}
