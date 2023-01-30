ab0a2bc34486e97b5c913774f02d897c4a4cc60e
Dan Goodman <danthegoodman@icloud.com>
2023-01-30 11:12:38 +0000

    Add CRDB changefeed input

---
+++ internal/impl/crdb/config_test.go
@@ -0,0 +1,32 @@
+package crdb
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/benthosdev/benthos/v4/public/service"
+)
+
+func TestCRDBConfigParse(t *testing.T) {
+	conf := `
+crdb_changefeed:
+dsn: postgresql://dan:xxxx@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/defaultdb?sslmode=require&options=--cluster%3Dportly-impala-2852
+tables:
+    - strm_2
+options:
+    - UPDATED
+    - CURSOR='1637953249519902405.0000000000'
+`
+
+	spec := crdbChangefeedInputConfig()
+	env := service.NewEnvironment()
+
+	selectConfig, err := spec.ParseYAML(conf, env)
+	require.NoError(t, err)
+
+	selectInput, err := newCRDBChangefeedInputFromConfig(selectConfig, nil)
+	require.NoError(t, err)
+	require.NoError(t, selectInput.Close(context.Background()))
+}
