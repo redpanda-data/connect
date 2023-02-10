86114e0d540e18eca2ca4b996a11d74b7fc1044c
Dan McGee <dan.mcgee@sodahealth.com>
2023-02-10 10:52:49 -0600

    Add ability to skip a UTF BOM in files

--- internal/codec/reader.go
+++ internal/codec/reader.go
@@ -15,6 +15,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/dimchansky/utfbom"
 	"github.com/klauspost/compress/gzip"
 
 	goavro "github.com/linkedin/goavro/v2"
@@ -39,6 +40,7 @@ var ReaderDocs = docs.FieldString(
 	"lines", "Consume the file in segments divided by linebreaks.",
 	"multipart", "Consumes the output of another codec and batches messages together. A batch ends when an empty message is consumed. For example, the codec `lines/multipart` could be used to consume multipart messages where an empty line indicates the end of each batch.",
 	"regex:(?m)^\\d\\d:\\d\\d:\\d\\d", "Consume the file in segments divided by regular expression.",
+	"skipbom", "Skip a byte order mark",
 	"tar", "Parse the file as a tar archive, and consume each file of the archive as a message.",
 )
 
@@ -192,7 +194,14 @@ func ioReader(codec string, conf ReaderConfig) (ioReaderConstructor, bool) {
 				r.Close()
 				return nil, err
 			}
-			return g, nil
+			unzipped := ioReadCloserWrapper{Reader: g, underlying: r}
+			return &unzipped, nil
+		}, true
+	}
+	if codec == "skipbom" {
+		return func(_ string, r io.ReadCloser) (io.ReadCloser, error) {
+			skipBom := ioReadCloserWrapper{Reader: utfbom.SkipOnly(r), underlying: r}
+			return &skipBom, nil
 		}, true
 	}
 	return nil, false
@@ -335,6 +344,21 @@ func autoCodec(conf ReaderConfig) ReaderConstructor {
 		}
 		return ctor(path, r, fn)
 	}
+}
+
+// ioReadCloserWrapper is a helper that closes both the upper and underlying reader
+// when you are creating some sort of wrapped reader where you want to ensure both
+// are closed.
+type ioReadCloserWrapper struct {
+	io.Reader
+	underlying io.ReadCloser
+}
+
+func (w ioReadCloserWrapper) Close() error {
+	if rc, ok := w.Reader.(io.Closer); ok {
+		rc.Close()
+	}
+	return w.underlying.Close()
 }
 
 //------------------------------------------------------------------------------
