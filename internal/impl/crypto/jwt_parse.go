ba9aa520ad4ebb6abfa48e14c5216882755def3b
Georges Haidar <ghaidar0@gmail.com>
2023-02-11 12:48:09 +0000

    DRY it up

--- internal/impl/crypto/jwt_parse.go
+++ internal/impl/crypto/jwt_parse.go
@@ -44,53 +44,39 @@ func jwtHSParser(alg *jwt.SigningMethodHMAC) bloblang.MethodConstructorV2 {
 	}
 }
 
-func newParseJwtHSSpec(method *jwt.SigningMethodHMAC) *bloblang.PluginSpec {
-	return bloblang.NewPluginSpec().
+func registerParseJwtHSMethod(name string, signingMethod *jwt.SigningMethodHMAC, docsFixture [2]string) error {
+	spec := bloblang.NewPluginSpec().
 		Category(query.MethodCategoryStrings).
-		Description(fmt.Sprintf("Parses a claims object from a JWT string encoded with %s. This method does not validate JWT claims.", method.Alg())).
-		Param(bloblang.NewStringParam("signing_secret").Description("The HMAC secret that was used for signing the token."))
+		Description(fmt.Sprintf("Parses a claims object from a JWT string encoded with %s. This method does not validate JWT claims.", signingMethod.Alg())).
+		Param(bloblang.NewStringParam("signing_secret").Description("The HMAC secret that was used for signing the token.")).
+		Example(
+			"",
+			fmt.Sprintf(`root.claims = this.signed.%s("dont-tell-anyone")`, name),
+			docsFixture,
+		)
+
+	return bloblang.RegisterMethodV2(name, spec, jwtHSParser(signingMethod))
 }
 
 func registerParseJwtHSMethods() error {
-	spec256 := newParseJwtHSSpec(jwt.SigningMethodHS256).
-		Example(
-			"",
-			`root.claims = this.signed.parse_jwt_hs256("dont-tell-anyone")`,
-			[2]string{
-				`{"signed":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMTIzIn0.hUl-nngPMY_3h9vveWJUPsCcO5PeL6k9hWLnMYeFbFQ"}`,
-				`{"claims":{"sub":"user123"}}`,
-			},
-		)
-	err := bloblang.RegisterMethodV2("parse_jwt_hs256", spec256, jwtHSParser(jwt.SigningMethodHS256))
-	if err != nil {
+	if err := registerParseJwtHSMethod("parse_jwt_hs256", jwt.SigningMethodHS256, [2]string{
+		`{"signed":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMTIzIn0.hUl-nngPMY_3h9vveWJUPsCcO5PeL6k9hWLnMYeFbFQ"}`,
+		`{"claims":{"sub":"user123"}}`,
+	}); err != nil {
 		return err
 	}
 
-	spec384 := newParseJwtHSSpec(jwt.SigningMethodHS384).
-		Example(
-			"",
-			`root.claims = this.signed.parse_jwt_hs384("dont-tell-anyone")`,
-			[2]string{
-				`{"signed":"eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMTIzIn0.zGYLr83aToon1efUNq-hw7XgT20lPvZb8sYei8x6S6mpHwb433SJdXJXx0Oio8AZ"}`,
-				`{"claims":{"sub":"user123"}}`,
-			},
-		)
-	err = bloblang.RegisterMethodV2("parse_jwt_hs384", spec384, jwtHSParser(jwt.SigningMethodHS384))
-	if err != nil {
+	if err := registerParseJwtHSMethod("parse_jwt_hs384", jwt.SigningMethodHS384, [2]string{
+		`{"signed":"eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMTIzIn0.zGYLr83aToon1efUNq-hw7XgT20lPvZb8sYei8x6S6mpHwb433SJdXJXx0Oio8AZ"}`,
+		`{"claims":{"sub":"user123"}}`,
+	}); err != nil {
 		return err
 	}
 
-	spec512 := newParseJwtHSSpec(jwt.SigningMethodHS512).
-		Example(
-			"",
-			`root.claims = this.signed.parse_jwt_hs512("dont-tell-anyone")`,
-			[2]string{
-				`{"signed":"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMTIzIn0.zBNR9o_6EDwXXKkpKLNJhG26j8Dc-mV-YahBwmEdCrmiWt5les8I9rgmNlWIowpq6Yxs4kLNAdFhqoRz3NXT3w"}`,
-				`{"claims":{"sub":"user123"}}`,
-			},
-		)
-	err = bloblang.RegisterMethodV2("parse_jwt_hs512", spec512, jwtHSParser(jwt.SigningMethodHS512))
-	if err != nil {
+	if err := registerParseJwtHSMethod("parse_jwt_hs512", jwt.SigningMethodHS512, [2]string{
+		`{"signed":"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMTIzIn0.zBNR9o_6EDwXXKkpKLNJhG26j8Dc-mV-YahBwmEdCrmiWt5les8I9rgmNlWIowpq6Yxs4kLNAdFhqoRz3NXT3w"}`,
+		`{"claims":{"sub":"user123"}}`,
+	}); err != nil {
 		return err
 	}
 
