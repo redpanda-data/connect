70686caf61bcdcf2eeedd2f3bc2bb8dad5f2896e
Ashley Jeffs <ash@jeffail.uk>
2023-05-14 09:40:54 +0100

    Move aws cloudwatch metrics to new APIs

--- internal/component/metrics/config.go
+++ internal/component/metrics/config.go
@@ -9,31 +9,29 @@ import (
 // Config is the all encompassing configuration struct for all metric output
 // types.
 type Config struct {
-	Type          string           `json:"type" yaml:"type"`
-	Mapping       string           `json:"mapping" yaml:"mapping"`
-	AWSCloudWatch CloudWatchConfig `json:"aws_cloudwatch" yaml:"aws_cloudwatch"`
-	JSONAPI       JSONAPIConfig    `json:"json_api" yaml:"json_api"`
-	InfluxDB      InfluxDBConfig   `json:"influxdb" yaml:"influxdb"`
-	None          struct{}         `json:"none" yaml:"none"`
-	Prometheus    PrometheusConfig `json:"prometheus" yaml:"prometheus"`
-	Statsd        StatsdConfig     `json:"statsd" yaml:"statsd"`
-	Logger        LoggerConfig     `json:"logger" yaml:"logger"`
-	Plugin        any              `json:"plugin,omitempty" yaml:"plugin,omitempty"`
+	Type       string           `json:"type" yaml:"type"`
+	Mapping    string           `json:"mapping" yaml:"mapping"`
+	JSONAPI    JSONAPIConfig    `json:"json_api" yaml:"json_api"`
+	InfluxDB   InfluxDBConfig   `json:"influxdb" yaml:"influxdb"`
+	None       struct{}         `json:"none" yaml:"none"`
+	Prometheus PrometheusConfig `json:"prometheus" yaml:"prometheus"`
+	Statsd     StatsdConfig     `json:"statsd" yaml:"statsd"`
+	Logger     LoggerConfig     `json:"logger" yaml:"logger"`
+	Plugin     any              `json:"plugin,omitempty" yaml:"plugin,omitempty"`
 }
 
 // NewConfig returns a configuration struct fully populated with default values.
 func NewConfig() Config {
 	return Config{
-		Type:          docs.DefaultTypeOf(docs.TypeMetrics),
-		Mapping:       "",
-		AWSCloudWatch: NewCloudWatchConfig(),
-		JSONAPI:       NewJSONAPIConfig(),
-		InfluxDB:      NewInfluxDBConfig(),
-		None:          struct{}{},
-		Prometheus:    NewPrometheusConfig(),
-		Statsd:        NewStatsdConfig(),
-		Logger:        NewLoggerConfig(),
-		Plugin:        nil,
+		Type:       docs.DefaultTypeOf(docs.TypeMetrics),
+		Mapping:    "",
+		JSONAPI:    NewJSONAPIConfig(),
+		InfluxDB:   NewInfluxDBConfig(),
+		None:       struct{}{},
+		Prometheus: NewPrometheusConfig(),
+		Statsd:     NewStatsdConfig(),
+		Logger:     NewLoggerConfig(),
+		Plugin:     nil,
 	}
 }
 
