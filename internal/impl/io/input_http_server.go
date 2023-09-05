993674dcaa2e206f9d43da12cd4e2b035b13ddb2
Ashley Jeffs <ash@jeffail.uk>
2023-09-05 11:37:44 +0100

    Narrow gap between http_server request rejections and shutdown

--- internal/impl/io/input_http_server.go
+++ internal/impl/io/input_http_server.go
@@ -780,16 +780,29 @@ func (h *httpServerInput) loop() {
 				h.log.Errorf("Failed to gracefully terminate http_server: %v\n", err)
 			}
 		} else {
-			if len(h.conf.Path) > 0 {
-				h.mgr.RegisterEndpoint(h.conf.Path, "Endpoint disabled.", func(w http.ResponseWriter, r *http.Request) {
-					http.Error(w, "Service unavailable", http.StatusServiceUnavailable)
-				})
-			}
-			if len(h.conf.WSPath) > 0 {
-				h.mgr.RegisterEndpoint(h.conf.WSPath, "Endpoint disabled.", func(w http.ResponseWriter, r *http.Request) {
-					http.Error(w, "Service unavailable", http.StatusServiceUnavailable)
-				})
-			}
+			// We are using the service-wide HTTP server. In order to prevent
+			// situations where a slow shutdown results in serving an abundance
+			// of 503 responses we wait until either the current requests are
+			// handled and shutdown can commence, or we've been instructed to
+			// close immediately, which prevents these requests from
+			// indefinitely blocking shutdown.
+			go func() {
+				select {
+				case <-h.shutSig.HasClosedChan():
+				case <-h.shutSig.CloseNowChan():
+				}
+
+				if len(h.conf.Path) > 0 {
+					h.mgr.RegisterEndpoint(h.conf.Path, "Endpoint disabled.", func(w http.ResponseWriter, r *http.Request) {
+						http.Error(w, "Service unavailable", http.StatusServiceUnavailable)
+					})
+				}
+				if len(h.conf.WSPath) > 0 {
+					h.mgr.RegisterEndpoint(h.conf.WSPath, "Endpoint disabled.", func(w http.ResponseWriter, r *http.Request) {
+						http.Error(w, "Service unavailable", http.StatusServiceUnavailable)
+					})
+				}
+			}()
 		}
 
 		h.handlerWG.Wait()
