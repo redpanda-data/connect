9db1c6e7c6ec51b46866675d4d89df0231370a4e
Ashley Jeffs <ash@jeffail.uk>
2023-09-08 09:03:33 +0100

    Avoid incrementing http_server waitgroup on shutdown

--- internal/impl/io/input_http_server.go
+++ internal/impl/io/input_http_server.go
@@ -470,6 +470,11 @@ func (h *httpServerInput) extractMessageFromRequest(r *http.Request) (message.Ba
 }
 
 func (h *httpServerInput) postHandler(w http.ResponseWriter, r *http.Request) {
+	if h.shutSig.ShouldCloseAtLeisure() {
+		http.Error(w, "Server closing", http.StatusServiceUnavailable)
+		return
+	}
+
 	h.handlerWG.Add(1)
 	defer h.handlerWG.Done()
 	defer r.Body.Close()
@@ -653,6 +658,11 @@ func (h *httpServerInput) postHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *httpServerInput) wsHandler(w http.ResponseWriter, r *http.Request) {
+	if h.shutSig.ShouldCloseAtLeisure() {
+		http.Error(w, "Server closing", http.StatusServiceUnavailable)
+		return
+	}
+
 	h.handlerWG.Add(1)
 	defer h.handlerWG.Done()
 
