6eacbb563e8473df83cd43dcc9fc344c800e6bb9
Ashley Jeffs <ash@jeffail.uk>
2024-03-04 22:12:18 +0000

    Stuff what we done on the stream

--- resources/docker/enrichments/init/settings.sql
+++ resources/docker/enrichments/init/settings.sql
@@ -1,1 +1,23 @@
 SET CLUSTER SETTING kv.rangefeed.enabled = true;
+
+CREATE TABLE IF NOT EXISTS users (
+	id varchar primary key,
+	name varchar,
+	email varchar,
+	updated_at timestamp
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+	id varchar primary key,
+	content varchar,
+	author varchar,
+	updated_at timestamp
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+	id varchar primary key,
+	content varchar,
+	post varchar,
+	author varchar,
+	updated_at timestamp
+);
