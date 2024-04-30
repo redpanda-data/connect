5b77698b908a8ce8e9ad43ee3b18f8da176e87f5
Daan Gerits <daan.gerits@gmail.com>
2024-04-30 16:46:41 +0200

    require the pool key to explicitly be defined
    Signed-off-by: Daan Gerits <daan.gerits@gmail.com>

--- internal/impl/nats/connection.go
+++ internal/impl/nats/connection.go
@@ -1,79 +1,83 @@
 package nats
 
 import (
-	"context"
-	"crypto/tls"
-	"strings"
+  "crypto/tls"
+  "strings"
 
-	"github.com/nats-io/nats.go"
+  "github.com/nats-io/nats.go"
 
-	"github.com/benthosdev/benthos/v4/public/service"
+  "github.com/benthosdev/benthos/v4/public/service"
 )
 
 // I've split the connection fields into two, which allows us to put tls and
 // auth further down the fields stack. This is literally just polish for the
 // docs.
 func connectionHeadFields() []*service.ConfigField {
-	return []*service.ConfigField{
-		service.NewStringListField("urls").
-			Description("A list of URLs to connect to. If an item of the list contains commas it will be expanded into multiple URLs.").
-			Example([]string{"nats://127.0.0.1:4222"}).
-			Example([]string{"nats://username:password@127.0.0.1:4222"}),
-		service.NewStringField("name").
-			Description("An optional name to assign to the connection. If not set, will default to the label").
-			Default(""),
-	}
+  return []*service.ConfigField{
+    service.NewStringListField("urls").
+      Description("A list of URLs to connect to. If an item of the list contains commas it will be expanded into multiple URLs.").
+      Example([]string{"nats://127.0.0.1:4222"}).
+      Example([]string{"nats://username:password@127.0.0.1:4222"}),
+    service.NewStringField("name").
+      Description("An optional name to assign to the connection. If not set, will default to the label").
+      Default(""),
+  }
 }
 
 func connectionTailFields() []*service.ConfigField {
-	return []*service.ConfigField{
-		service.NewTLSToggledField("tls"),
-		authFieldSpec(),
-	}
+  return []*service.ConfigField{
+    service.NewTLSToggledField("tls"),
+    authFieldSpec(),
+    service.NewStringField("pool_key").
+      Description("The connection pool key to use. Components using the same poolKey will share their connection").
+      Default("default").
+      Advanced(),
+  }
 }
 
 type connectionDetails struct {
-	Urls     string
-	Opts     []nats.Option
-	authConf authConfig
+  poolKey  string
+  urls     string
+  opts     []nats.Option
+  authConf authConfig
 }
 
 func connectionDetailsFromParsed(conf *service.ParsedConfig, mgr *service.Resources, extraOpts ...nats.Option) (c connectionDetails, err error) {
-	var urlList []string
-	if urlList, err = conf.FieldStringList("urls"); err != nil {
-		return
-	}
-	c.Urls = strings.Join(urlList, ",")
+  var urlList []string
+  if urlList, err = conf.FieldStringList("urls"); err != nil {
+    return
+  }
+  c.urls = strings.Join(urlList, ",")
 
-	var name string
-	if name, err = conf.FieldString("name"); err != nil {
-		return
-	}
-	if name == "" {
-		name = mgr.Label()
-	}
-	c.Opts = append(c.Opts, nats.Name(name))
+  if c.poolKey, err = conf.FieldString("pool_key"); err != nil {
+    return
+  }
 
-	var tlsEnabled bool
-	var tlsConf *tls.Config
-	if tlsConf, tlsEnabled, err = conf.FieldTLSToggled("tls"); err != nil {
-		return
-	}
-	if tlsEnabled && tlsConf != nil {
-		c.Opts = append(c.Opts, nats.Secure(tlsConf))
-	}
+  var name string
+  if name, err = conf.FieldString("name"); err != nil {
+    return
+  }
+  if name == "" {
+    name = mgr.Label()
+  }
+  c.opts = append(c.opts, nats.Name(name))
 
-	if c.authConf, err = AuthFromParsedConfig(conf.Namespace("auth")); err != nil {
-		return
-	}
-	c.Opts = append(c.Opts, authConfToOptions(c.authConf, mgr.FS())...)
+  var tlsEnabled bool
+  var tlsConf *tls.Config
+  if tlsConf, tlsEnabled, err = conf.FieldTLSToggled("tls"); err != nil {
+    return
+  }
+  if tlsEnabled && tlsConf != nil {
+    c.opts = append(c.opts, nats.Secure(tlsConf))
+  }
 
-	c.Opts = append(c.Opts, errorHandlerOption(mgr.Logger()))
-	c.Opts = append(c.Opts, extraOpts...)
+  if c.authConf, err = AuthFromParsedConfig(conf.Namespace("auth")); err != nil {
+    return
+  }
+  c.opts = append(c.opts, authConfToOptions(c.authConf, mgr.FS())...)
 
-	return
-}
+  c.opts = append(c.opts, errorHandlerOption(mgr.Logger()))
+  c.opts = append(c.opts, extraOpts...)
 
-func (c *connectionDetails) get(_ context.Context) (*nats.Conn, error) {
-	return nats.Connect(c.Urls, c.Opts...)
+  return
 }
--- internal/impl/nats/pool_test.go
+++ internal/impl/nats/pool_test.go
@@ -1,92 +1,86 @@
 package nats
 
 import (
-	"context"
-	"testing"
+  "context"
+  "testing"
 
-	"github.com/mitchellh/hashstructure"
-	"github.com/nats-io/nats.go"
-	"github.com/stretchr/testify/assert"
+  "github.com/nats-io/nats.go"
+  "github.com/stretchr/testify/assert"
 )
 
 func TestGet(t *testing.T) {
-	t.Run("shouldCreateIfNotExists", shouldCreateIfNotExists)
-	t.Run("shouldReuseIfExists", shouldReuseIfExists)
-	t.Run("shouldReuseIfAskedMultipleTimes", shouldReuseIfAskedMultipleTimes)
+  t.Run("shouldCreateIfNotExists", shouldCreateIfNotExists)
+  t.Run("shouldReuseIfExists", shouldReuseIfExists)
+  t.Run("shouldReuseIfAskedMultipleTimes", shouldReuseIfAskedMultipleTimes)
 }
 
 func shouldCreateIfNotExists(t *testing.T) {
-	pl := &connectionPool{
-		cache: map[uint64]*connRef{},
-		connectFn: func(ctx context.Context, s string, details connectionDetails) (*nats.Conn, error) {
-			return &nats.Conn{Opts: nats.Options{Name: s}}, nil
-		},
-	}
+  pl := &connectionPool{
+    cache: map[string]*connRef{},
+    connectFn: func(ctx context.Context, s string, details connectionDetails) (*nats.Conn, error) {
+      return &nats.Conn{Opts: nats.Options{Name: s}}, nil
+    },
+  }
 
-	caller := "caller_id"
-	cd := connectionDetails{Urls: "url1, url2"}
-	hash, err := hashstructure.Hash(cd, nil)
-	assert.NoError(t, err)
+  pk := "default"
+  caller := "caller_id"
+  cd := connectionDetails{poolKey: pk, urls: "url1, url2"}
 
-	assert.Nil(t, pl.cache[hash])
-
-	res, err := pl.Get(context.Background(), caller, cd)
-	assert.NoError(t, err)
-	assert.NotNil(t, res)
-	assert.NotNil(t, pl.cache[hash])
-	assert.Equal(t, caller, pl.cache[hash].Nc.Opts.Name)
-	assert.Equal(t, caller, pl.cache[hash].References[0])
+  res, err := pl.Get(context.Background(), caller, cd)
+  assert.NoError(t, err)
+  assert.NotNil(t, res)
+  assert.NotNil(t, pl.cache[pk])
+  assert.Equal(t, caller, pl.cache[pk].Nc.Opts.Name)
+  assert.Equal(t, caller, pl.cache[pk].References[0])
 }
 
 func shouldReuseIfExists(t *testing.T) {
-	pl := &connectionPool{
-		cache: map[uint64]*connRef{},
-		connectFn: func(ctx context.Context, s string, details connectionDetails) (*nats.Conn, error) {
-			return &nats.Conn{Opts: nats.Options{Name: s}}, nil
-		},
-	}
+  pl := &connectionPool{
+    cache: map[string]*connRef{},
+    connectFn: func(ctx context.Context, s string, details connectionDetails) (*nats.Conn, error) {
+      return &nats.Conn{Opts: nats.Options{Name: s}}, nil
+    },
+  }
 
-	c1 := "caller_id_1"
-	c2 := "caller_id_2"
+  c1 := "caller_id_1"
+  c2 := "caller_id_2"
 
-	cd := connectionDetails{Urls: "url1, url2"}
-	hash, _ := hashstructure.Hash(cd, nil)
-	res1, err := pl.Get(context.Background(), c1, cd)
-	assert.NoError(t, err)
-	assert.NotNil(t, res1)
+  cd := connectionDetails{poolKey: "default", urls: "url1, url2"}
+  res1, err := pl.Get(context.Background(), c1, cd)
+  assert.NoError(t, err)
+  assert.NotNil(t, res1)
 
-	res2, err := pl.Get(context.Background(), c2, cd)
-	assert.NoError(t, err)
-	assert.NotNil(t, res2)
+  res2, err := pl.Get(context.Background(), c2, cd)
+  assert.NoError(t, err)
+  assert.NotNil(t, res2)
 
-	assert.Equal(t, []string{c1, c2}, pl.cache[hash].References)
-	assert.Len(t, pl.cache, 1)
+  assert.Equal(t, []string{c1, c2}, pl.cache[cd.poolKey].References)
+  assert.Len(t, pl.cache, 1)
 
-	assert.Equal(t, res1, res2)
+  assert.Equal(t, res1, res2)
 }
 
 func shouldReuseIfAskedMultipleTimes(t *testing.T) {
-	pl := &connectionPool{
-		cache: map[uint64]*connRef{},
-		connectFn: func(ctx context.Context, s string, details connectionDetails) (*nats.Conn, error) {
-			return &nats.Conn{Opts: nats.Options{Name: s}}, nil
-		},
-	}
+  pl := &connectionPool{
+    cache: map[string]*connRef{},
+    connectFn: func(ctx context.Context, s string, details connectionDetails) (*nats.Conn, error) {
+      return &nats.Conn{Opts: nats.Options{Name: s}}, nil
+    },
+  }
 
-	c1 := "caller_id_1"
+  c1 := "caller_id_1"
 
-	cd := connectionDetails{Urls: "url1, url2"}
-	hash, _ := hashstructure.Hash(cd, nil)
-	res1, err := pl.Get(context.Background(), c1, cd)
-	assert.NoError(t, err)
-	assert.NotNil(t, res1)
+  cd := connectionDetails{poolKey: "default", urls: "url1, url2"}
+  res1, err := pl.Get(context.Background(), c1, cd)
+  assert.NoError(t, err)
+  assert.NotNil(t, res1)
 
-	res2, err := pl.Get(context.Background(), c1, cd)
-	assert.NoError(t, err)
-	assert.NotNil(t, res2)
+  res2, err := pl.Get(context.Background(), c1, cd)
+  assert.NoError(t, err)
+  assert.NotNil(t, res2)
 
-	assert.Equal(t, []string{c1}, pl.cache[hash].References)
-	assert.Len(t, pl.cache, 1)
+  assert.Equal(t, []string{c1}, pl.cache[cd.poolKey].References)
+  assert.Len(t, pl.cache, 1)
 
-	assert.Equal(t, res1, res2)
+  assert.Equal(t, res1, res2)
 }
