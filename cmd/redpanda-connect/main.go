339bfa8242d02bf97bf8196693534f3a2c133ea0
Ashley Jeffs <ash@jeffail.uk>
2024-05-23 17:17:16 +0100

    Add main binary and kafka logger

---
+++ cmd/redpanda-connect/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/benthosdev/benthos/v4/public/service"
+
+	"github.com/redpanda-data/connect/v4/internal/impl/kafka"
+
+	_ "github.com/redpanda-data/connect/v4/public/components/all"
+)
+
+var (
+	Version   string
+	DateBuilt string
+)
+
+func redpandaTopLevelConfigField() *service.ConfigField {
+	return service.NewObjectField("redpanda", kafka.TopicLoggerFields()...)
+}
+
+func main() {
+	rpLogger := kafka.NewTopicLogger()
+
+	service.RunCLI(
+		context.Background(),
+		service.CLIOptSetVersion(Version, DateBuilt),
+		service.CLIOptSetMainSchemaFrom(func() *service.ConfigSchema {
+			return service.NewEnvironment().FullConfigSchema(Version, DateBuilt).
+				Field(redpandaTopLevelConfigField())
+		}),
+		service.CLIOptOnLoggerInit(func(l *service.Logger) {
+			rpLogger.SetFallbackLogger(l)
+		}),
+		service.CLIOptAddTeeLogger(slog.New(rpLogger)),
+		service.CLIOptOnConfigParse(func(fn *service.ParsedConfig) error {
+			return rpLogger.InitOutputFromParsed(fn.Namespace("redpanda"))
+		}),
+	)
+}
