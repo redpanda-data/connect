b4a340d8d01a50ce35284173925d85b7f46fb0d4
Ashley Jeffs <ash@jeffail.uk>
2024-05-31 09:22:18 +0100

    Fix links in release notes script

--- resources/scripts/release_notes.sh
+++ resources/scripts/release_notes.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-echo "For installation instructions check out the [getting started guide](https://www.docs.redpanda.com/redpanda-connect/guides/getting_started)."
+echo "For installation instructions check out the [getting started guide](https://docs.redpanda.com/redpanda-connect/guides/getting_started)."
 cat CHANGELOG.md | awk '
   /^## [0-9]/ {
       release++;
   }
   /TBD$/ {
       print "";
-      print "NOTE: This is a release candidate, you can download a binary from this page or pull a docker image from https://github.com/redpanda-data/connect/pkgs/container/connect with the specific tag of the release candidate.";
+      print "NOTE: This is a release candidate, you can download a binary from this page.";
   }
   !/^## [0-9]/ {
       if ( release == 1 ) print;
