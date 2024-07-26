abefa4a778fdcce4c9ad5d04943131bfd9f1b51b
Davide Baldo <davide@baldo.me>
2024-07-26 19:44:55 +0200

    Better parameter organization for ockam kafka

--- internal/impl/kafka/output_kafka_franz.go
+++ internal/impl/kafka/output_kafka_franz.go
@@ -30,7 +30,7 @@ import (
 	"github.com/redpanda-data/benthos/v4/public/service"
 )
 
-func FranzKafkaOutputConfig() *service.ConfigSpec {
+func franzKafkaOutputConfig() *service.ConfigSpec {
 	return service.NewConfigSpec().
 		Beta().
 		Categories("Services").
@@ -41,80 +41,86 @@ Writes a batch of messages to Kafka brokers and waits for acknowledgement before
 
 This output often out-performs the traditional ` + "`kafka`" + ` output as well as providing more useful logs and error messages.
 `).
-		Field(service.NewStringListField("seed_brokers").
+		Fields(FranzKafkaOutputConfigFields()...).
+		LintRule(`
+root = if this.partitioner == "manual" {
+if this.partition.or("") == "" {
+"a partition must be specified when the partitioner is set to manual"
+}
+} else if this.partition.or("") != "" {
+"a partition cannot be specified unless the partitioner is set to manual"
+}`)
+}
+
+func FranzKafkaOutputConfigFields() []*service.ConfigField {
+	return []*service.ConfigField{
+		service.NewStringListField("seed_brokers").
 			Description("A list of broker addresses to connect to in order to establish connections. If an item of the list contains commas it will be expanded into multiple addresses.").
 			Example([]string{"localhost:9092"}).
 			Example([]string{"foo:9092", "bar:9092"}).
-			Example([]string{"foo:9092,bar:9092"})).
-		Field(service.NewInterpolatedStringField("topic").
-			Description("A topic to write messages to.")).
-		Field(service.NewInterpolatedStringField("key").
-			Description("An optional key to populate for each message.").Optional()).
-		Field(service.NewStringAnnotatedEnumField("partitioner", map[string]string{
+			Example([]string{"foo:9092,bar:9092"}),
+		service.NewInterpolatedStringField("topic").
+			Description("A topic to write messages to."),
+		service.NewInterpolatedStringField("key").
+			Description("An optional key to populate for each message.").Optional(),
+		service.NewStringAnnotatedEnumField("partitioner", map[string]string{
 			"murmur2_hash": "Kafka's default hash algorithm that uses a 32-bit murmur2 hash of the key to compute which partition the record will be on.",
 			"round_robin":  "Round-robin's messages through all available partitions. This algorithm has lower throughput and causes higher CPU load on brokers, but can be useful if you want to ensure an even distribution of records to partitions.",
 			"least_backup": "Chooses the least backed up partition (the partition with the fewest amount of buffered records). Partitions are selected per batch.",
 			"manual":       "Manually select a partition for each message, requires the field `partition` to be specified.",
 		}).
 			Description("Override the default murmur2 hashing partitioner.").
-			Advanced().Optional()).
-		Field(service.NewInterpolatedStringField("partition").
+			Advanced().Optional(),
+		service.NewInterpolatedStringField("partition").
 			Description("An optional explicit partition to set for each message. This field is only relevant when the `partitioner` is set to `manual`. The provided interpolation string must be a valid integer.").
 			Example(`${! meta("partition") }`).
-			Optional()).
-		Field(service.NewStringField("client_id").
+			Optional(),
+		service.NewStringField("client_id").
 			Description("An identifier for the client connection.").
 			Default("benthos").
-			Advanced()).
-		Field(service.NewStringField("rack_id").
+			Advanced(),
+		service.NewStringField("rack_id").
 			Description("A rack identifier for this client.").
 			Default("").
-			Advanced()).
-		Field(service.NewBoolField("idempotent_write").
+			Advanced(),
+		service.NewBoolField("idempotent_write").
 			Description("Enable the idempotent write producer option. This requires the `IDEMPOTENT_WRITE` permission on `CLUSTER` and can be disabled if this permission is not available.").
 			Default(true).
-			Advanced()).
-		Field(service.NewMetadataFilterField("metadata").
+			Advanced(),
+		service.NewMetadataFilterField("metadata").
 			Description("Determine which (if any) metadata values should be added to messages as headers.").
-			Optional()).
-		Field(service.NewIntField("max_in_flight").
+			Optional(),
+		service.NewIntField("max_in_flight").
 			Description("The maximum number of batches to be sending in parallel at any given time.").
-			Default(10)).
-		Field(service.NewDurationField("timeout").
+			Default(10),
+		service.NewDurationField("timeout").
 			Description("The maximum period of time to wait for message sends before abandoning the request and retrying").
 			Default("10s").
-			Advanced()).
-		Field(service.NewBatchPolicyField("batching")).
-		Field(service.NewStringField("max_message_bytes").
+			Advanced(),
+		service.NewBatchPolicyField("batching"),
+		service.NewStringField("max_message_bytes").
 			Description("The maximum space in bytes than an individual message may take, messages larger than this value will be rejected. This field corresponds to Kafka's `max.message.bytes`.").
 			Advanced().
 			Default("1MB").
 			Example("100MB").
-			Example("50mib")).
-		Field(service.NewStringEnumField("compression", "lz4", "snappy", "gzip", "none", "zstd").
+			Example("50mib"),
+		service.NewStringEnumField("compression", "lz4", "snappy", "gzip", "none", "zstd").
 			Description("Optionally set an explicit compression type. The default preference is to use snappy when the broker supports it, and fall back to none if not.").
 			Optional().
-			Advanced()).
-		Field(service.NewTLSToggledField("tls")).
-		Field(SASLFields()).
-		Field(service.NewInterpolatedStringField("timestamp").
+			Advanced(),
+		service.NewTLSToggledField("tls"),
+		SASLFields(),
+		service.NewInterpolatedStringField("timestamp").
 			Description("An optional timestamp to set for each message. When left empty, the current timestamp is used.").
 			Example(`${! timestamp_unix() }`).
 			Example(`${! metadata("kafka_timestamp_unix") }`).
 			Optional().
-			Advanced()).
-		LintRule(`
-root = if this.partitioner == "manual" {
-  if this.partition.or("") == "" {
-    "a partition must be specified when the partitioner is set to manual"
-  }
-} else if this.partition.or("") != "" {
-  "a partition cannot be specified unless the partitioner is set to manual"
-}`)
+			Advanced(),
+	}
 }
 
 func init() {
-	err := service.RegisterBatchOutput("kafka_franz", FranzKafkaOutputConfig(),
+	err := service.RegisterBatchOutput("kafka_franz", franzKafkaOutputConfig(),
 		func(conf *service.ParsedConfig, mgr *service.Resources) (
 			output service.BatchOutput,
 			batchPolicy service.BatchPolicy,
