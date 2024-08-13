494f5f4588383f4c47d28897db55c377cf21dc55
Ashley Jeffs <ash@jeffail.uk>
2024-08-13 18:25:54 +0100

    Disable pulsar on arm32

--- public/components/pulsar/arm_32.go
+++ public/components/pulsar/arm_32.go
@@ -12,11 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !arm
+//go:build arm
 
 package pulsar
-
-import (
-	// Bring in the internal plugin definitions.
-	_ "github.com/redpanda-data/connect/v4/internal/impl/pulsar"
-)
