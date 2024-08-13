3e88785d88ce40232c87eb009b908e8b630c782c
Ashley Jeffs <ash@jeffail.uk>
2024-08-13 16:39:23 +0100

    Disable ockam for windows builds

--- public/components/ockam/windows.go
+++ public/components/ockam/windows.go
@@ -12,21 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+//go:build windows
 
-import (
-	"bytes"
-
-	"github.com/redpanda-data/redpanda/src/transform-sdk/go/transform"
-)
-
-func main() {
-	transform.OnRecordWritten(makeUppercase)
-}
-
-func makeUppercase(e transform.WriteEvent, w transform.RecordWriter) error {
-	return w.Write(transform.Record{
-		Key:   e.Record().Key,
-		Value: bytes.ToUpper(e.Record().Value),
-	})
-}
+package ockam
