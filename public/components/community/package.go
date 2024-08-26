d04c5c3206ddc04265ed6b65ff272a7e76b3b638
Andreas Bergmeier <andreas.bergmeier@otto.de>
2024-08-26 14:16:17 +0200

    Add new processor vault_key
    Retrieves Keys from Vault.
    Supports Role auth currently.

--- public/components/community/package.go
+++ public/components/community/package.go
@@ -66,6 +66,7 @@ import (
 	_ "github.com/redpanda-data/connect/v4/public/components/sql"
 	_ "github.com/redpanda-data/connect/v4/public/components/statsd"
 	_ "github.com/redpanda-data/connect/v4/public/components/twitter"
+	_ "github.com/redpanda-data/connect/v4/public/components/vault"
 	_ "github.com/redpanda-data/connect/v4/public/components/wasm"
 	_ "github.com/redpanda-data/connect/v4/public/components/zeromq"
 )
