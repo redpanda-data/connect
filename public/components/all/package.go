fcce4192383b8eb3d5b662d67b39e0f27844c265
Ashley Jeffs <ash@jeffail.uk>
2024-08-29 15:02:18 +0100

    Add migration components to all package and allow list

--- public/components/all/package.go
+++ public/components/all/package.go
@@ -25,6 +25,7 @@ import (
 	// Import all enterprise components.
 	_ "github.com/redpanda-data/connect/v4/public/components/aws/enterprise"
 	_ "github.com/redpanda-data/connect/v4/public/components/gcp/enterprise"
+	_ "github.com/redpanda-data/connect/v4/public/components/kafka/enterprise"
 	_ "github.com/redpanda-data/connect/v4/public/components/ollama"
 	_ "github.com/redpanda-data/connect/v4/public/components/openai"
 	_ "github.com/redpanda-data/connect/v4/public/components/snowflake"
