6ba6cbe9dcd66ee7b37e3726653a9ae13be96f13
Ashley Jeffs <ash@jeffail.uk>
2024-08-29 15:33:03 +0100

    Add kafka enterprise to cloud

--- public/components/cloud/package.go
+++ public/components/cloud/package.go
@@ -27,6 +27,7 @@ import (
 	_ "github.com/redpanda-data/connect/v4/public/components/gcp/enterprise"
 	_ "github.com/redpanda-data/connect/v4/public/components/io"
 	_ "github.com/redpanda-data/connect/v4/public/components/kafka"
+	_ "github.com/redpanda-data/connect/v4/public/components/kafka/enterprise"
 	_ "github.com/redpanda-data/connect/v4/public/components/maxmind"
 	_ "github.com/redpanda-data/connect/v4/public/components/memcached"
 	_ "github.com/redpanda-data/connect/v4/public/components/msgpack"
