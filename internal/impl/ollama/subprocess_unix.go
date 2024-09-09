2b59438a32a319fb8f970d9d2666b45fdbab5a52
Tyler Rockwood <rockwood@redpanda.com>
2024-09-09 15:26:38 +0000

    ollama: bump automatically downloaded version

--- internal/impl/ollama/subprocess_unix.go
+++ internal/impl/ollama/subprocess_unix.go
@@ -53,7 +53,7 @@ func (c *runOllamaConfig) lookPath(file string) (string, error) {
 func (c *runOllamaConfig) downloadOllama(ctx context.Context, path string) error {
 	var url string
 	if c.downloadURL == "" {
-		const baseURL string = "https://github.com/ollama/ollama/releases/download/v0.3.6/ollama"
+		const baseURL string = "https://github.com/ollama/ollama/releases/download/v0.3.9/ollama"
 		switch runtime.GOOS {
 		case "darwin":
 			// They ship an universal executable for darwin
