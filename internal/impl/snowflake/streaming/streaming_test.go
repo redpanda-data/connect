cd311e48b460eb1b44a85adbaaff0d026431bfb7
Tyler Rockwood <rockwood@redpanda.com>
2024-10-21 14:26:38 +0000

    snowpipe: add test to ensure I don't forget to disable debug mode

---
+++ internal/impl/snowflake/streaming/streaming_test.go
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2024 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+
+package streaming
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDebugModeDisabled(t *testing.T) {
+	// So I can't forget to disable this!
+	require.Equal(t, false, debug)
+}
