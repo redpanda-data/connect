76ef36600dc9aa53e1ecfb8f5f95af8f049b03f3
Vladyslav Len <heavenly.beta@gmail.com>
2024-11-06 23:30:35 +0100

    feat(mysql_cdc): working on mysql_cdc plugin

--- go.mod
+++ go.mod
@@ -170,6 +170,7 @@ require (
 	github.com/containerd/platforms v0.2.1 // indirect
 	github.com/envoyproxy/go-control-plane v0.13.0 // indirect
 	github.com/envoyproxy/protoc-gen-validate v1.1.0 // indirect
+	github.com/go-mysql-org/go-mysql v1.9.1 // indirect
 	github.com/hamba/avro/v2 v2.22.2-0.20240625062549-66aad10411d9 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/jzelinskie/stringz v0.0.3 // indirect
---
+++ internal/impl/mysql/integration_test.go
@@ -0,0 +1,18 @@
+// Copyright 2024 Redpanda Data, Inc.
+//
+// Licensed as a Redpanda Enterprise file under the Redpanda Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// https://github.com/redpanda-data/connect/v4/blob/main/licenses/rcl.md
+
+package mysql
+
+import (
+	"github.com/redpanda-data/benthos/v4/public/service/integration"
+	"testing"
+)
+
+func TestIntegrationMySQLCDC(t *testing.T) {
+	integration.CheckSkip(t)
+}
