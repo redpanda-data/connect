a24c600d40e7fde883a44e98a27ab1b77b5ec588
Tomasz Sadura <tomasz@redpanda.com>
2024-11-06 09:52:55 +0100

    Switch AWS secrets API to v2

--- go.mod
+++ go.mod
@@ -31,7 +31,7 @@ require (
 	github.com/authzed/grpcutil v0.0.0-20240123194739-2ea1e3d2d98b
 	github.com/aws/aws-lambda-go v1.47.0
 	github.com/aws/aws-sdk-go v1.50.36
-	github.com/aws/aws-sdk-go-v2 v1.30.4
+	github.com/aws/aws-sdk-go-v2 v1.32.3
 	github.com/aws/aws-sdk-go-v2/config v1.27.27
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.27
 	github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression v1.7.32
@@ -155,6 +155,7 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/keyvault/azsecrets v0.12.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/keyvault/internal v0.7.1 // indirect
 	github.com/certifi/gocertifi v0.0.0-20210507211836-431795d63e8d // indirect
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.34.3 // indirect
 	github.com/containerd/platforms v0.2.1 // indirect
 	github.com/envoyproxy/protoc-gen-validate v1.1.0 // indirect
 	github.com/hamba/avro/v2 v2.22.2-0.20240625062549-66aad10411d9 // indirect
@@ -205,8 +206,8 @@ require (
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.4 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.14.10 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.11 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.16 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.16 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.22 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.22 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.15 // indirect
 	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.22.3 // indirect
@@ -217,7 +218,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.17.15 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.22.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.4 // indirect
-	github.com/aws/smithy-go v1.20.4
+	github.com/aws/smithy-go v1.22.0
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bits-and-blooms/bitset v1.4.0 // indirect
--- internal/secrets/aws.go
+++ internal/secrets/aws.go
@@ -8,38 +8,35 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager/types"
 )
 
 type awsSecretsManager struct {
-	client *secretsmanager.SecretsManager
+	client *secretsmanager.Client
 	logger *slog.Logger
 }
 
-func newAWSSecretsManager(_ context.Context, logger *slog.Logger, url *url.URL) (secretAPI, error) {
-	s, err := session.NewSession(&aws.Config{
-		Region: getRegion(url.Host),
-	})
+func newAWSSecretsManager(ctx context.Context, logger *slog.Logger, url *url.URL) (secretAPI, error) {
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(getRegion(url.Host)))
 	if err != nil {
-		return nil, fmt.Errorf("failed to create secret manager session: %w", err)
+		return nil, fmt.Errorf("failed to load AWS config: %w", err)
 	}
 
 	return &awsSecretsManager{
-		client: secretsmanager.New(s, aws.NewConfig()),
+		client: secretsmanager.NewFromConfig(cfg),
 		logger: logger,
 	}, nil
 }
 
 func (a *awsSecretsManager) getSecretValue(ctx context.Context, key string) (string, bool) {
-	value, err := a.client.GetSecretValueWithContext(ctx, &secretsmanager.GetSecretValueInput{
+	value, err := a.client.GetSecretValue(ctx, &secretsmanager.GetSecretValueInput{
 		SecretId: &key,
 	})
 	if err != nil {
-		var notFound *secretsmanager.ResourceNotFoundException
-		if !errors.As(err, &notFound) {
-			// An error that isn't due to key-not-found gets logged
+		var nf *types.ResourceNotFoundException
+		if !errors.As(err, &nf) {
 			a.logger.With("error", err, "key", key).Error("Failed to look up secret")
 		}
 		return "", false
@@ -48,9 +45,9 @@ func (a *awsSecretsManager) getSecretValue(ctx context.Context, key string) (str
 	return *value.SecretString, true
 }
 
-func getRegion(host string) *string {
+func getRegion(host string) string {
 	endpoint := strings.TrimPrefix(host, "secretsmanager.")
 	region := strings.TrimSuffix(endpoint, ".amazonaws.com")
 
-	return &region
+	return region
 }
