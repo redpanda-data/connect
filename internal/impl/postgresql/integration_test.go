61ea84b3fd5c74bce7e2d5c9dbd19a2e50f3f74d
Vladyslav Len <heavenly.beta@gmail.com>
2024-11-11 22:07:37 +0100

    chore(): updated docs && fixed lint

--- internal/impl/postgresql/integration_test.go
+++ internal/impl/postgresql/integration_test.go
@@ -12,7 +12,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"github.com/redpanda-data/benthos/v4/public/service/integration"
 	"strings"
 	"sync"
 	"testing"
@@ -23,6 +22,7 @@ import (
 	_ "github.com/redpanda-data/benthos/v4/public/components/io"
 	_ "github.com/redpanda-data/benthos/v4/public/components/pure"
 	"github.com/redpanda-data/benthos/v4/public/service"
+	"github.com/redpanda-data/benthos/v4/public/service/integration"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
