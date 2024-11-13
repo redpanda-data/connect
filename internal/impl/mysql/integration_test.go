1e6dc8c59cf7f73e5fb18de0ae4d73961af19aa7
Vladyslav Len <heavenly.beta@gmail.com>
2024-11-13 21:50:16 +0100

    chore(): added mysql tests

--- internal/impl/mysql/integration_test.go
+++ internal/impl/mysql/integration_test.go
@@ -10,11 +10,15 @@ package mysql
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 	"sync"
 	"testing"
 	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/redpanda-data/benthos/v4/public/components/io"
 	_ "github.com/redpanda-data/benthos/v4/public/components/pure"
 	"github.com/redpanda-data/benthos/v4/public/service"
@@ -23,7 +27,72 @@ import (
 )
 
 func TestIntegrationMySQLCDC(t *testing.T) {
-	mysqlDsn := "vultradmin:AVNS_fgGkgy43bJw3NzNwDCV@tcp(public-vultr-prod-a70dc516-1330-488a-bf57-712a3d91be58-vultr-pr.vultrdb.com:16751)/defaultdb"
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	pool.MaxWait = time.Minute
+
+	// MySQL specific environment variables
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "mysql",
+		Tag:        "9.1",
+		Env: []string{
+			"MYSQL_ROOT_PASSWORD=password",
+			"MYSQL_DATABASE=testdb",
+		},
+		Cmd: []string{
+			"--server-id=1",
+			"--log-bin=mysql-bin",
+			"--binlog-format=ROW",
+			"--binlog-row-image=FULL",
+			"--log-slave-updates=ON",
+		},
+		ExposedPorts: []string{"3306/tcp"},
+	}, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{
+			Name: "no",
+		}
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, pool.Purge(resource))
+	})
+
+	port := resource.GetPort("3306/tcp")
+	dsn := fmt.Sprintf(
+		"root:password@tcp(localhost:%s)/testdb?parseTime=true&timeout=30s&readTimeout=30s&writeTimeout=30s&multiStatements=true",
+		port,
+	)
+
+	var db *sql.DB
+	err = pool.Retry(func() error {
+		var err error
+		db, err = sql.Open("mysql", dsn)
+		if err != nil {
+			return err
+		}
+
+		db.SetMaxOpenConns(10)
+		db.SetMaxIdleConns(5)
+		db.SetConnMaxLifetime(time.Minute * 5)
+
+		if err != nil {
+			return err
+		}
+
+		return db.Ping()
+	})
+	require.NoError(t, err)
+
+	// Create table
+	_, err = db.Exec(`
+    CREATE TABLE IF NOT EXISTS foo (
+        a INT PRIMARY KEY
+    )
+`)
+	require.NoError(t, err)
 	tmpDir := t.TempDir()
 
 	template := fmt.Sprintf(`
@@ -32,9 +101,9 @@ mysql_stream:
   stream_snapshot: false
   checkpoint_key: foocache
   tables:
-    - users
+    - foo
   flavor: mysql
-`, mysqlDsn)
+`, dsn)
 
 	cacheConf := fmt.Sprintf(`
 label: foocache
@@ -52,7 +121,6 @@ file:
 		msgBytes, err := mb[0].AsBytes()
 		require.NoError(t, err)
 		outBatchMut.Lock()
-		fmt.Println(string(msgBytes))
 		outBatches = append(outBatches, string(msgBytes))
 		outBatchMut.Unlock()
 		return nil
@@ -66,13 +134,56 @@ file:
 		require.NoError(t, err)
 	}()
 
+	time.Sleep(time.Second * 5)
+	for i := 0; i < 1000; i++ {
+		// Insert 10000 rows
+		if _, err = db.Exec("INSERT INTO foo VALUES (?)", i); err != nil {
+			require.NoError(t, err)
+		}
+	}
+
 	assert.Eventually(t, func() bool {
 		outBatchMut.Lock()
 		defer outBatchMut.Unlock()
-		return len(outBatches) == 10000
+		return len(outBatches) == 1000
+	}, time.Minute*5, time.Millisecond*100)
+
+	require.NoError(t, streamOut.StopWithin(time.Second*10))
+
+	streamOutBuilder = service.NewStreamBuilder()
+	require.NoError(t, streamOutBuilder.SetLoggerYAML(`level: INFO`))
+	require.NoError(t, streamOutBuilder.AddCacheYAML(cacheConf))
+	require.NoError(t, streamOutBuilder.AddInputYAML(template))
+
+	outBatches = nil
+	require.NoError(t, streamOutBuilder.AddBatchConsumerFunc(func(c context.Context, mb service.MessageBatch) error {
+		msgBytes, err := mb[0].AsBytes()
+		require.NoError(t, err)
+		outBatchMut.Lock()
+		outBatches = append(outBatches, string(msgBytes))
+		outBatchMut.Unlock()
+		return nil
+	}))
+
+	streamOut, err = streamOutBuilder.Build()
+	require.NoError(t, err)
+
+	time.Sleep(time.Second)
+	for i := 1001; i < 2001; i++ {
+		_, err = db.Exec("INSERT INTO foo VALUES (?)", i)
+		require.NoError(t, err)
+	}
+
+	go func() {
+		err = streamOut.Run(context.Background())
+		require.NoError(t, err)
+	}()
+
+	assert.Eventually(t, func() bool {
+		outBatchMut.Lock()
+		defer outBatchMut.Unlock()
+		return len(outBatches) == 1000
 	}, time.Minute*5, time.Millisecond*100)
 
 	require.NoError(t, streamOut.StopWithin(time.Second*10))
 }
-
-// mysql://vultradmin:AVNS_fgGkgy43bJw3NzNwDCV@tcp(vultr-prod-a70dc516-1330-488a-bf57-712a3d91be58-vultr-prod-6745.vultrdb.com:16751)/defaultdb
