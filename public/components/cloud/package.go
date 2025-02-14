a93ce3db33ededba2573a89599e72c011623a13a
Tyler Rockwood <rockwotj@gmail.com>
2025-02-14 13:02:16 -0600

    mongo/cdc: add as enterprise plug in
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- public/components/cloud/package.go
+++ public/components/cloud/package.go
@@ -31,6 +31,7 @@ import (
 	_ "github.com/redpanda-data/connect/v4/public/components/maxmind"
 	_ "github.com/redpanda-data/connect/v4/public/components/memcached"
 	_ "github.com/redpanda-data/connect/v4/public/components/mongodb"
+	_ "github.com/redpanda-data/connect/v4/public/components/mongodb/enterprise"
 	_ "github.com/redpanda-data/connect/v4/public/components/mqtt"
 	_ "github.com/redpanda-data/connect/v4/public/components/msgpack"
 	_ "github.com/redpanda-data/connect/v4/public/components/mysql"
