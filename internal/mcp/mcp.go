53a27e861c841521448daf0b255516be01711f71
Tyler Rockwood <rockwood@redpanda.com>
2025-03-28 10:36:52 +0000

    mcp: add starlark support

--- internal/mcp/mcp.go
+++ internal/mcp/mcp.go
@@ -1,14 +1,28 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+
 package mcp
 
 import (
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"net/url"
 	"os"
+	"path/filepath"
 
 	"github.com/mark3labs/mcp-go/server"
 
+	"github.com/redpanda-data/benthos/v4/public/service"
 	"github.com/redpanda-data/connect/v4/internal/mcp/repository"
+	"github.com/redpanda-data/connect/v4/internal/mcp/starlark"
 	"github.com/redpanda-data/connect/v4/internal/mcp/tools"
 
 	_ "github.com/redpanda-data/connect/v4/public/components/all"
@@ -27,6 +41,36 @@ func Run(logger *slog.Logger, repositoryDir, baseURLStr string) error {
 
 	repoScanner := repository.NewScanner(os.DirFS(repositoryDir))
 	repoScanner.OnResourceFile(func(resourceType string, filename string, contents []byte) error {
+		if filepath.Ext(filename) == ".star" {
+			result, err := starlark.Eval(
+				service.GlobalEnvironment(),
+				nil,
+				filename,
+				contents,
+			)
+			if err != nil {
+				return err
+			}
+			for k, v := range result.Processors {
+				cfg := map[string]any{
+					"label": k,
+					v.Name:  v.SerializedConfig,
+					"meta": map[string]any{
+						"mcp": map[string]any{
+							"enabled": true,
+						},
+					},
+				}
+				b, err := json.Marshal(&cfg)
+				if err != nil {
+					return err
+				}
+				if err := resWrapper.AddProcessor(b); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
 		switch resourceType {
 		case "cache":
 			if err := resWrapper.AddCache(contents); err != nil {
