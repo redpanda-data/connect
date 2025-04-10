a2896e35a805eea9e07c8352c7dd30f687ff2a17
Tyler Rockwood <rockwood@redpanda.com>
2025-04-10 02:17:23 +0000

    cohere: fmt

--- internal/impl/cohere/chat_processor_test.go
+++ internal/impl/cohere/chat_processor_test.go
@@ -22,8 +22,9 @@ import (
 	_ "github.com/redpanda-data/benthos/v4/public/components/pure"
 	"github.com/redpanda-data/benthos/v4/public/service"
 	"github.com/redpanda-data/benthos/v4/public/service/integration"
-	"github.com/redpanda-data/connect/v4/internal/license"
 	"github.com/stretchr/testify/require"
+
+	"github.com/redpanda-data/connect/v4/internal/license"
 )
 
 type TestMessageCollector struct {
