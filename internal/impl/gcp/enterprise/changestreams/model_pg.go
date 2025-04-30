463a450d7236881f630419861b6afc9999376a78
Michał Matczuk <michal.matczuk@redpanda.com>
2025-04-30 16:09:47 +0200

    spanner_cdc(changestreams): improve model
    The model is very pointer heavy, and it cannot be improved.
    This is due to the spanner APIs. Spanner expects a slice of struct pointers
    ex. []*ChangeRecord when decoding ARRAY[STRUCT] data.
    
    - Extract model to sparate files
    - Add relevant sections from Google Spanner docs explaining meaning of relevant fields
    - Implement fmt.Stringer
    
    Fixes CON-66

---
+++ internal/impl/gcp/enterprise/changestreams/model_pg.go
@@ -0,0 +1,49 @@
+// Copyright 2025 Redpanda Data, Inc.
+//
+// Licensed as a Redpanda Enterprise file under the Redpanda Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// https://github.com/redpanda-data/connect/blob/main/licenses/rcl.md
+
+package changestreams
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"cloud.google.com/go/spanner"
+)
+
+func decodePostgresRow(row *spanner.Row) (*ChangeRecord, error) {
+	var col spanner.NullJSON
+	if err := row.Column(0, &col); err != nil {
+		return nil, fmt.Errorf("extract column from row: %w", err)
+	}
+
+	b, err := col.MarshalJSON()
+	if err != nil {
+		return nil, fmt.Errorf("marshal JSON column: %w", err)
+	}
+
+	var pgcr struct {
+		DataChangeRecord      *DataChangeRecord      `json:"data_change_record"`
+		HeartbeatRecord       *HeartbeatRecord       `json:"heartbeat_record"`
+		ChildPartitionsRecord *ChildPartitionsRecord `json:"child_partitions_record"`
+	}
+	if err := json.Unmarshal(b, &pgcr); err != nil {
+		return nil, fmt.Errorf("unmarshal JSON data: %w", err)
+	}
+
+	var cr ChangeRecord
+	if pgcr.DataChangeRecord != nil {
+		cr.DataChangeRecords = []*DataChangeRecord{pgcr.DataChangeRecord}
+	}
+	if pgcr.HeartbeatRecord != nil {
+		cr.HeartbeatRecords = []*HeartbeatRecord{pgcr.HeartbeatRecord}
+	}
+	if pgcr.ChildPartitionsRecord != nil {
+		cr.ChildPartitionsRecords = []*ChildPartitionsRecord{pgcr.ChildPartitionsRecord}
+	}
+	return &cr, nil
+}
