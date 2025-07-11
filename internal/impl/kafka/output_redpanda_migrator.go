7f300db09ca81ac8fa65289542a346b332cf6f92
Michał Matczuk <michal.matczuk@redpanda.com>
2025-07-11 13:52:31 +0200

    kafka: output_redpanda_migrator refactoring
    Improve readability and remove duplicated code. Refactor FranzWriter by reducing hooks.
    Fix a bug where topic created after the output has started has not been resolved but just prefixed.

--- internal/impl/kafka/output_redpanda_migrator.go
+++ internal/impl/kafka/output_redpanda_migrator.go
@@ -48,7 +48,7 @@ const (
 	rmoResourceDefaultLabel = "redpanda_migrator_output"
 )
 
-func redpandaMigratorOutputConfig() *service.ConfigSpec {
+func redpandaMigratorOutputSpec() *service.ConfigSpec {
 	return service.NewConfigSpec().
 		Beta().
 		Categories("Services").
@@ -124,247 +124,278 @@ func redpandaMigratorOutputConfigFields() []*service.ConfigField {
 }
 
 func init() {
-	service.MustRegisterBatchOutput("redpanda_migrator", redpandaMigratorOutputConfig(),
+	service.MustRegisterBatchOutput("redpanda_migrator", redpandaMigratorOutputSpec(),
 		func(conf *service.ParsedConfig, mgr *service.Resources) (
 			output service.BatchOutput,
 			batchPolicy service.BatchPolicy,
 			maxInFlight int,
 			err error,
 		) {
-			topicPrefix, err := conf.FieldString(rmoFieldTopicPrefix)
-			if err != nil {
+			if output, err = redpandaMigratorOutputFromParsed(conf, mgr); err != nil {
 				return
 			}
-			var destTopicResolver *service.InterpolatedString
-			if topicPrefix != "" {
-				if destTopicResolver, err = conf.FieldInterpolatedString(rmoFieldTopic); err != nil {
-					return
-				}
-			}
-
 			if maxInFlight, err = conf.FieldInt(rmoFieldMaxInFlight); err != nil {
 				return
 			}
 
-			var inputResource string
-			if inputResource, err = conf.FieldString(rmoFieldInputResource); err != nil {
-				return
-			}
-
-			createTopicCfg := createTopicConfig{}
-
-			if createTopicCfg.replicationFactorOverride, err = conf.FieldBool(rmoFieldRepFactorOverride); err != nil {
-				return
-			}
-
-			if createTopicCfg.replicationFactor, err = conf.FieldInt(rmoFieldRepFactor); err != nil {
-				return
-			}
-
-			var translateSchemaIDs bool
-			if translateSchemaIDs, err = conf.FieldBool(rmoFieldTranslateSchemaIDs); err != nil {
-				return
-			}
-
-			if createTopicCfg.isServerlessBroker, err = conf.FieldBool(rmoFieldIsServerless); err != nil {
-				return
-			}
-
-			var schemaRegistryOutputResource srResourceKey
-			if translateSchemaIDs {
-				var res string
-				if res, err = conf.FieldString(rmoFieldSchemaRegistryOutputResource); err != nil {
-					return
-				}
-				schemaRegistryOutputResource = srResourceKey(res)
-			}
-
-			var tmpOpts, clientOpts []kgo.Opt
-
-			var connDetails *FranzConnectionDetails
-			if connDetails, err = FranzConnectionDetailsFromConfig(conf, mgr.Logger()); err != nil {
-				return
-			}
-			clientOpts = append(clientOpts, connDetails.FranzOpts()...)
-
-			if tmpOpts, err = FranzProducerOptsFromConfig(conf); err != nil {
-				return
-			}
-			clientOpts = append(clientOpts, tmpOpts...)
-
-			var client *kgo.Client
-			var clientMut sync.Mutex
-			// Stores the source to destination SchemaID mapping.
-			var schemaIDCache sync.Map
-			var topicCache sync.Map
-			var runOnce sync.Once
-			output, err = NewFranzWriterFromConfig(
-				conf,
-				NewFranzWriterHooks(
-					func(ctx context.Context, fn FranzSharedClientUseFn) error {
-						clientMut.Lock()
-						defer clientMut.Unlock()
-
-						if client == nil {
-							var err error
-							if client, err = NewFranzClient(ctx, clientOpts...); err != nil {
-								return err
-							}
-						}
-
-						return fn(&FranzSharedClientInfo{Client: client, ConnDetails: connDetails})
-					}).WithYieldClientFn(
-					func(context.Context) error {
-						clientMut.Lock()
-						defer clientMut.Unlock()
-
-						if client == nil {
-							return nil
-						}
-
-						client.Close()
-						client = nil
-						return nil
-					}).WithWriteHookFn(
-					func(ctx context.Context, client *kgo.Client, records []*kgo.Record) error {
-						// Try to create all topics which the input `redpanda_migrator` resource is configured to read
-						// from when we receive the first message.
-						runOnce.Do(func() {
-							err := FranzSharedClientUse(inputResource, mgr, func(details *FranzSharedClientInfo) error {
-								inputClient := details.Client
-								outputClient := client
-								topics := inputClient.GetConsumeTopics()
-
-								for _, topic := range topics {
-									destTopic := topic
-									// Check if the user specified a topic alias and resolve it.
-									if topicPrefix != "" {
-										// Hack: The current message corresponds to a specific topic, but we want to
-										// create all topics, so we assume users will only use the `kafka_topic`
-										// metadata when specifying the `topic`.
-										tmpMsg := service.NewMessage(nil)
-										tmpMsg.MetaSetMut("kafka_topic", topic)
-										if destTopic, err = destTopicResolver.TryString(tmpMsg); err != nil {
-											return fmt.Errorf("failed to parse destination topic: %s", err)
-										} else if destTopic == "" {
-											return errors.New("failed to parse destination topic: empty string")
-										}
-
-										destTopic = topicPrefix + destTopic
-									}
-
-									cfg := createTopicCfg
-									cfg.srcTopic = topic
-									cfg.destTopic = destTopic
-									if err := createTopic(ctx, mgr.Logger(), inputClient, outputClient, cfg); err != nil {
-										if errors.Is(err, kerr.TopicAlreadyExists) {
-											mgr.Logger().Debugf("Topic %q already exists", topic)
-										} else {
-											// This may be a topic which doesn't have any messages in it, so if we
-											// failed to create it now, we log an error and continue. If it does contain
-											// messages, we'll attempt to create it again anyway when receiving a
-											// message from it.
-											mgr.Logger().Errorf("Failed to create topic %q and ACLs: %s", topic, err)
-											continue
-										}
-									} else {
-										mgr.Logger().Infof("Created topic %q", destTopic)
-									}
-
-									if err := createACLs(ctx, topic, destTopic, inputClient, outputClient); err != nil {
-										mgr.Logger().Errorf("Failed to create ACLs for topic %q: %s", topic, err)
-									}
-
-									topicCache.Store(topic, struct{}{})
-								}
-
-								return nil
-							})
-							if err != nil {
-								mgr.Logger().Errorf("Failed to fetch topics from input %q: %s", inputResource, err)
-							}
-						})
-
-						var srOutput *schemaRegistryOutput
-						if translateSchemaIDs {
-							if res, ok := mgr.GetGeneric(schemaRegistryOutputResource); ok {
-								srOutput = res.(*schemaRegistryOutput)
-							} else {
-								mgr.Logger().Warnf("schema_registry output resource %q not found; skipping schema ID translation", schemaRegistryOutputResource)
-							}
-						}
-						translateSchemaID := func(record *kgo.Record) error {
-							if srOutput == nil {
-								return nil
-							}
-
-							if record.Value == nil {
-								// Do not attept to translate schema IDs for tombstone records.
-								return nil
-							}
-
-							var ch franz_sr.ConfluentHeader
-							schemaID, _, err := ch.DecodeID(record.Value)
-							if err != nil {
-								return fmt.Errorf("failed to extract schema ID: %s", err)
-							}
-
-							var destSchemaID int
-							if cachedID, ok := schemaIDCache.Load(schemaID); !ok {
-								if destSchemaID, err = srOutput.GetDestinationSchemaID(ctx, schemaID); err != nil {
-									return fmt.Errorf("failed to fetch destination schema ID: %s", err)
-								}
-								schemaIDCache.Store(schemaID, destSchemaID)
-							} else {
-								destSchemaID = cachedID.(int)
-							}
-
-							if err = sr.UpdateID(record.Value, destSchemaID); err != nil {
-								return fmt.Errorf("failed to update schema ID: %s", err)
-							}
-
-							return nil
-						}
-
-						// The current record may be coming from a topic which was created later during runtime, so we
-						// need to try and create it if we haven't done so already.
-						if err := FranzSharedClientUse(inputResource, mgr, func(details *FranzSharedClientInfo) error {
-							for i, record := range records {
-								if err := translateSchemaID(record); err != nil {
-									mgr.Logger().Warnf("Failed to update schema ID in record index %d on topic %s: %q", i, record.Topic, err)
-								}
-
-								srcTopic := record.Topic
-								record.Topic = topicPrefix + record.Topic
-
-								if _, ok := topicCache.Load(srcTopic); !ok {
-									cfg := createTopicCfg
-									cfg.srcTopic = srcTopic
-									cfg.destTopic = record.Topic
-									if err := createTopic(ctx, mgr.Logger(), details.Client, client, cfg); err != nil {
-										if errors.Is(err, kerr.TopicAlreadyExists) {
-											mgr.Logger().Debugf("Topic %q already exists", record.Topic)
-										} else {
-											return fmt.Errorf("failed to create topic %q and ACLs: %s", record.Topic, err)
-										}
-									} else {
-										mgr.Logger().Infof("Created topic %q", record.Topic)
-									}
-
-									if err := createACLs(ctx, srcTopic, record.Topic, details.Client, client); err != nil {
-										mgr.Logger().Errorf("Failed to create ACLs for topic %q: %s", record.Topic, err)
-									}
-
-									topicCache.Store(srcTopic, struct{}{})
-								}
-							}
-							return nil
-						}); err != nil {
-							mgr.Logger().With("error", err, "resource", inputResource).Warn("Failed to access shared client for given resource identifier")
-						}
-
-						return nil
-					}))
 			return
 		})
+}
+
+type redpandaMigratorOutput struct {
+	*FranzWriter
+	topicPrefix                  string
+	destTopicResolver            *service.InterpolatedString
+	inputResource                string
+	createTopicCfg               createTopicConfig
+	translateSchemaIDs           bool
+	schemaRegistryOutputResource srResourceKey
+
+	// Shared client resources
+	client      *kgo.Client
+	clientMu    sync.Mutex // TODO(mmt): is this needed?
+	clientOpts  []kgo.Opt
+	connDetails *FranzConnectionDetails
+
+	// Caches
+	schemaIDCache sync.Map
+	topicCache    sync.Map
+	once          sync.Once
+
+	// Resources
+	logger *service.Logger
+	mgr    *service.Resources
+}
+
+func redpandaMigratorOutputFromParsed(conf *service.ParsedConfig, mgr *service.Resources) (*redpandaMigratorOutput, error) {
+	o := &redpandaMigratorOutput{
+		logger: mgr.Logger(),
+		mgr:    mgr,
+	}
+
+	var err error
+	o.FranzWriter, err = NewFranzWriterFromConfig(conf, NewFranzWriterHooks(
+		func(ctx context.Context, fn FranzSharedClientUseFn) error {
+			o.clientMu.Lock()
+			defer o.clientMu.Unlock()
+
+			if o.client == nil {
+				var err error
+				if o.client, err = NewFranzClient(ctx, o.clientOpts...); err != nil {
+					return err
+				}
+			}
+
+			return fn(&FranzSharedClientInfo{Client: o.client, ConnDetails: o.connDetails})
+		}).WithYieldClientFn(
+		func(context.Context) error {
+			o.clientMu.Lock()
+			defer o.clientMu.Unlock()
+
+			if o.client == nil {
+				return nil
+			}
+
+			o.client.Close()
+			o.client = nil
+			return nil
+		}))
+	if err != nil {
+		return nil, err
+	}
+	o.OnWrite = o.onWrite
+
+	if o.topicPrefix, err = conf.FieldString(rmoFieldTopicPrefix); err != nil {
+		return nil, err
+	}
+
+	if o.topicPrefix != "" {
+		if o.destTopicResolver, err = conf.FieldInterpolatedString(rmoFieldTopic); err != nil {
+			return nil, err
+		}
+	}
+
+	if o.inputResource, err = conf.FieldString(rmoFieldInputResource); err != nil {
+		return nil, err
+	}
+
+	if o.createTopicCfg.replicationFactorOverride, err = conf.FieldBool(rmoFieldRepFactorOverride); err != nil {
+		return nil, err
+	}
+
+	if o.createTopicCfg.replicationFactor, err = conf.FieldInt(rmoFieldRepFactor); err != nil {
+		return nil, err
+	}
+
+	if o.translateSchemaIDs, err = conf.FieldBool(rmoFieldTranslateSchemaIDs); err != nil {
+		return nil, err
+	}
+
+	if o.createTopicCfg.isServerlessBroker, err = conf.FieldBool(rmoFieldIsServerless); err != nil {
+		return nil, err
+	}
+
+	if o.translateSchemaIDs {
+		var res string
+		if res, err = conf.FieldString(rmoFieldSchemaRegistryOutputResource); err != nil {
+			return nil, err
+		}
+		o.schemaRegistryOutputResource = srResourceKey(res)
+	}
+
+	if o.connDetails, err = FranzConnectionDetailsFromConfig(conf, mgr.Logger()); err != nil {
+		return nil, err
+	}
+	o.clientOpts = append(o.clientOpts, o.connDetails.FranzOpts()...)
+	var opts []kgo.Opt
+	if opts, err = FranzProducerOptsFromConfig(conf); err != nil {
+		return nil, err
+	}
+	o.clientOpts = append(o.clientOpts, opts...)
+
+	return o, nil
+}
+
+func (o *redpandaMigratorOutput) onWrite(ctx context.Context, _ *kgo.Client, records []*kgo.Record) error {
+	return FranzSharedClientUse(o.inputResource, o.mgr, func(details *FranzSharedClientInfo) error {
+		o.once.Do(func() {
+			o.logger.Infof("Creating topics for %s", o.inputResource)
+			count := o.tryCreateAllTopics(ctx, details)
+			o.logger.Infof("Created %d topics for %s", count, o.inputResource)
+		})
+
+		if err := o.updateTopicsInRecords(ctx, details.Client, records); err != nil {
+			return err
+		}
+		if o.translateSchemaIDs {
+			if err := o.updateSchemaIDsInRecords(ctx, records); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
+
+func (o *redpandaMigratorOutput) tryCreateAllTopics(ctx context.Context, details *FranzSharedClientInfo) int {
+	inputClient := details.Client
+
+	count := 0
+	for _, topic := range inputClient.GetConsumeTopics() {
+		if _, err := o.createTopicIfNeeded(ctx, inputClient, topic); err != nil {
+			// Continue on error, we will attempt to create the topic again when
+			// we receive a message.
+			o.logger.Errorf("Failed to create topic %q: %s", topic, err)
+		} else {
+			count++
+		}
+	}
+
+	return count
+}
+
+func (o *redpandaMigratorOutput) updateTopicsInRecords(ctx context.Context, inputClient *kgo.Client, records []*kgo.Record) error {
+	for _, record := range records {
+		destTopic, err := o.createTopicIfNeeded(ctx, inputClient, record.Topic)
+		if err != nil {
+			return err
+		}
+		record.Topic = destTopic
+	}
+	return nil
+}
+
+func (o *redpandaMigratorOutput) createTopicIfNeeded(ctx context.Context, inputClient *kgo.Client, topic string) (string, error) {
+	if cachedTopic, ok := o.topicCache.Load(topic); ok {
+		return cachedTopic.(string), nil
+	}
+
+	destTopic, err := o.resolveTopic(topic)
+	if err != nil {
+		return "", fmt.Errorf("resolve topic %q: %w", topic, err)
+	}
+
+	cfg := o.createTopicCfg // copy
+	cfg.srcTopic = topic
+	cfg.destTopic = destTopic
+	if err := createTopic(ctx, o.logger, inputClient, o.client, cfg); err != nil {
+		if !errors.Is(err, kerr.TopicAlreadyExists) {
+			return "", fmt.Errorf("create topic %q: %w", topic, err)
+		}
+	}
+	if err := createACLs(ctx, topic, destTopic, inputClient, o.client); err != nil {
+		return "", fmt.Errorf("create ACLs for topic %q: %w", topic, err)
+	}
+	o.logger.Infof("Created topic and ACLs %s -> %s", topic, destTopic)
+
+	o.topicCache.Store(topic, destTopic)
+	return destTopic, nil
+}
+
+func (o *redpandaMigratorOutput) resolveTopic(topic string) (string, error) {
+	if o.topicPrefix == "" {
+		return topic, nil
+	}
+
+	// Hack: The current message corresponds to a specific topic, but we want to
+	// create all topics, so we assume users will only use the `kafka_topic`
+	// metadata when specifying the `topic`.
+	msg := service.NewMessage(nil)
+	msg.MetaSetMut("kafka_topic", topic)
+	destTopic, err := o.destTopicResolver.TryString(msg)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse destination topic: %s", err)
+	}
+	if destTopic == "" {
+		return "", errors.New("failed to parse destination topic: empty string")
+	}
+	destTopic = o.topicPrefix + destTopic
+
+	return destTopic, nil
+}
+
+func (o *redpandaMigratorOutput) updateSchemaIDsInRecords(ctx context.Context, records []*kgo.Record) error {
+	res, ok := o.mgr.GetGeneric(o.schemaRegistryOutputResource)
+	if !ok {
+		return fmt.Errorf("schema_registry output resource %q not found", o.schemaRegistryOutputResource)
+	}
+	srOutput := res.(*schemaRegistryOutput)
+
+	for _, record := range records {
+		if err := o.updateRecordSchemaID(ctx, srOutput, record); err != nil {
+			return fmt.Errorf("update schema ID in record offset %d on topic %s: %w", record.Offset, record.Topic, err)
+		}
+	}
+
+	return nil
+}
+
+func (o *redpandaMigratorOutput) updateRecordSchemaID(ctx context.Context, srOutput *schemaRegistryOutput, record *kgo.Record) error {
+	// Do not attempt to translate schema IDs for a tombstone
+	if record.Value == nil {
+		return nil
+	}
+
+	var ch franz_sr.ConfluentHeader
+	schemaID, _, err := ch.DecodeID(record.Value)
+	if err != nil {
+		return fmt.Errorf("decode schema ID: %w", err)
+	}
+
+	var destSchemaID int
+	if cachedID, ok := o.schemaIDCache.Load(schemaID); !ok {
+		var err error
+		destSchemaID, err = srOutput.GetDestinationSchemaID(ctx, schemaID)
+		if err != nil {
+			return fmt.Errorf("translate schema ID: %w", err)
+		}
+		o.schemaIDCache.Store(schemaID, destSchemaID)
+	} else {
+		destSchemaID = cachedID.(int)
+	}
+
+	if err = sr.UpdateID(record.Value, destSchemaID); err != nil {
+		return err
+	}
+
+	return nil
 }
