ad99edb07ad335b0c2947bad242c1c6055d20811
Joseph Woodward <joseph.woodward@redpanda.com>
2025-09-09 12:24:17 +0100

    feat: updates based on comments

--- internal/impl/mssqlserver/stream.go
+++ internal/impl/mssqlserver/stream.go
@@ -4,7 +4,7 @@
 // License (the "License"); you may not use this file except in compliance with
 // the License. You may obtain a copy of the License at
 //
-// https://github.com/redpanda-data/connect/v4/blob/main/licenses/rcl.md
+// https://github.com/redpanda-data/connect/blob/main/licenses/rcl.md
 
 package mssqlserver
 
