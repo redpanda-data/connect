6996a7cda755dc41aff7bcd73ac414320cfa4c76
batiati <rbatiati@gmail.com>
2025-09-19 08:57:32 -0300

    Conditional CGO build

--- public/components/tigerbeetle/cgo.go
+++ public/components/tigerbeetle/cgo.go
@@ -12,4 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build cgo
+
 package tigerbeetle
+
+import (
+	// Bring in the internal plugin definitions.
+	_ "github.com/redpanda-data/connect/v4/internal/impl/tigerbeetle"
+)
