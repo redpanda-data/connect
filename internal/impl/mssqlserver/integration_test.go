77645d750ef82f42ce61f5189092c3f0a3a83923
Joseph Woodward <joseph.woodward@redpanda.com>
2025-09-30 16:08:33 +0100

    feat: add additional manual test data

--- internal/impl/mssqlserver/integration_test.go
+++ internal/impl/mssqlserver/integration_test.go
@@ -566,8 +566,11 @@ func Test_ManualTesting_AddTestDataWithUniqueLSN(t *testing.T) {
 		CREATE TABLE test.users (
 			id INT IDENTITY(1,1) PRIMARY KEY,
 			name NVARCHAR(100) NOT NULL,
+			surname NVARCHAR(100) NOT NULL,
+			about NVARCHAR(255) NOT NULL,
 			email NVARCHAR(255) NOT NULL,
 			date_of_birth DATE NULL,
+			join_date DATE NULL,
 			created_at DATETIME2 NOT NULL DEFAULT SYSUTCDATETIME(),
 			is_active BIT NOT NULL DEFAULT 1,
 			login_count INT NOT NULL DEFAULT 0,
@@ -583,6 +586,20 @@ func Test_ManualTesting_AddTestDataWithUniqueLSN(t *testing.T) {
 		created_at DATETIME2 NOT NULL DEFAULT SYSUTCDATETIME(),
 		balance DECIMAL(10,2) NOT NULL DEFAULT 0.00
 	);`)
+	require.NoError(t, err)
+
+	t.Log("Creating test tables 'dbo.cart'...")
+	err = testDB.createTableWithCDCEnabledIfNotExists(t.Context(), "dbo.cart", `
+		CREATE TABLE dbo.cart (
+			id INT IDENTITY(1,1) PRIMARY KEY,
+			name NVARCHAR(100) NOT NULL,
+			email NVARCHAR(255) NOT NULL,
+			date_of_birth DATE NULL,
+			created_at DATETIME2 NOT NULL DEFAULT SYSUTCDATETIME(),
+			is_active BIT NOT NULL DEFAULT 1,
+			login_count INT NOT NULL DEFAULT 0,
+			balance DECIMAL(10,2) NOT NULL DEFAULT 0.00
+		);`)
 	require.NoError(t, err)
 
 	// --- insert test data
@@ -615,11 +632,14 @@ func Test_ManualTesting_AddTestDataWithUniqueLSN(t *testing.T) {
 		FROM sys.all_objects a
 		CROSS JOIN sys.all_objects b
 	)
-	INSERT INTO test.users (name, email, date_of_birth, created_at, is_active, login_count, balance)
+	INSERT INTO test.users (name, surname, about, email, date_of_birth, join_date, created_at, is_active, login_count, balance)
 	SELECT
 		CONCAT('user-', n),                                -- name
+		CONCAT('surname-', n),                             -- surname
+		CONCAT('about-', n),							   -- about
 		CONCAT('user', n, '@example.com'),                 -- email
 		DATEADD(DAY, -n % 10000, GETDATE()),               -- date_of_birth, spread over ~27 years
+		SYSUTCDATETIME(),                                  -- join_date
 		SYSUTCDATETIME(),                                  -- created_at
 		CASE WHEN n % 2 = 0 THEN 1 ELSE 0 END,             -- is_active alternating 1/0
 		n % 100,                                           -- login_count between 0-99
@@ -638,6 +658,25 @@ func Test_ManualTesting_AddTestDataWithUniqueLSN(t *testing.T) {
 		SELECT
 		CONCAT('product-', n),                             -- name
 		SYSUTCDATETIME(),                                  -- created_at
+		CAST((n % 1000) + RAND(CHECKSUM(NEWID())) * 100 AS DECIMAL(10,2)) -- balance
+	FROM Numbers;
+	`)
+	require.NoError(t, err)
+
+	_, err = db.Exec(`
+	WITH Numbers AS (
+		SELECT TOP (5000000) ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS n
+		FROM sys.all_objects a
+		CROSS JOIN sys.all_objects b
+	)
+	INSERT INTO dbo.cart (name, email, date_of_birth, created_at, is_active, login_count, balance)
+	SELECT
+		CONCAT('cart-', n),                                -- name
+		CONCAT('cart', n, '@example.com'),                 -- email
+		DATEADD(DAY, -n % 10000, GETDATE()),               -- date_of_birth, spread over ~27 years
+		SYSUTCDATETIME(),                                  -- created_at
+		CASE WHEN n % 2 = 0 THEN 1 ELSE 0 END,             -- is_active alternating 1/0
+		n % 100,                                           -- login_count between 0-99
 		CAST((n % 1000) + RAND(CHECKSUM(NEWID())) * 100 AS DECIMAL(10,2)) -- balance
 	FROM Numbers;
 	`)
