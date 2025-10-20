fa7e614a02763a720c7c10f7d4160b5e230efdcb
Tyler Rockwood <rockwood@redpanda.com>
2025-10-20 07:26:09 +0000

    protobuf: split parsing utilities
    I'm introducing a new parser for protobuf, which requires a different
    form for protos. This means it's helpful to break down the methods to
    parse and load protobuf into it's primative forms.

--- internal/impl/protobuf/common.go
+++ internal/impl/protobuf/common.go
@@ -17,7 +17,10 @@ package protobuf
 import (
 	"fmt"
 
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/reflect/protoregistry"
+	"google.golang.org/protobuf/types/descriptorpb"
 	"google.golang.org/protobuf/types/dynamicpb"
 
 	//nolint:staticcheck // Ignore SA1019 "github.com/jhump/protoreflect/desc/protoparse" is deprecated warning
@@ -29,6 +32,16 @@ import (
 // protobuf types. These registries can then be used as a mechanism for
 // dynamically (un)marshalling the definitions within.
 func RegistriesFromMap(filesMap map[string]string) (*protoregistry.Files, *protoregistry.Types, error) {
+	fds, err := ParseProtos(filesMap)
+	if err != nil {
+		return nil, nil, err
+	}
+	return BuildRegistries(fds)
+}
+
+// ParseProtos dynamically parses protobuf files from a map of import path to proto file contents,
+// and loads them as a FileDescriptorSet, which can be used to dynamically (un)marshal protos.
+func ParseProtos(filesMap map[string]string) (*descriptorpb.FileDescriptorSet, error) {
 	var parser protoparse.Parser
 	parser.Accessor = protoparse.FileContentsFromMap(filesMap)
 
@@ -39,23 +52,39 @@ func RegistriesFromMap(filesMap map[string]string) (*protoregistry.Files, *proto
 
 	fds, err := parser.ParseFiles(names...)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-
-	files, types := &protoregistry.Files{}, &protoregistry.Types{}
+	var files []*descriptorpb.FileDescriptorProto
 	for _, v := range fds {
-		if err := files.RegisterFile(v.UnwrapFile()); err != nil {
-			return nil, nil, fmt.Errorf("failed to register file '%v': %w", v.GetName(), err)
+		files = append(files, v.AsFileDescriptorProto())
+	}
+	return &descriptorpb.FileDescriptorSet{File: files}, nil
+}
+
+// BuildRegistries converts a FileDescriptorSet into a registry that is able to
+// resolve types and lookup protos by name.
+func BuildRegistries(descriptors *descriptorpb.FileDescriptorSet) (*protoregistry.Files, *protoregistry.Types, error) {
+	files, err := protodesc.NewFiles(descriptors)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to register proto files: %w", err)
+	}
+	types := &protoregistry.Types{}
+	var register func(mds protoreflect.MessageDescriptors) error
+	register = func(mds protoreflect.MessageDescriptors) error {
+		for i := range mds.Len() {
+			msg := mds.Get(i)
+			if err := types.RegisterMessage(dynamicpb.NewMessageType(msg)); err != nil {
+				return fmt.Errorf("failed to register type %q: %w", msg.FullName(), err)
+			}
+			if err := register(msg.Messages()); err != nil {
+				return err
+			}
 		}
-		for _, t := range v.GetMessageTypes() {
-			if err := types.RegisterMessage(dynamicpb.NewMessageType(t.UnwrapMessage())); err != nil {
-				return nil, nil, fmt.Errorf("failed to register type '%v': %w", t.GetName(), err)
-			}
-			for _, nt := range t.GetNestedMessageTypes() {
-				if err := types.RegisterMessage(dynamicpb.NewMessageType(nt.UnwrapMessage())); err != nil {
-					return nil, nil, fmt.Errorf("failed to register type '%v': %w", nt.GetFullyQualifiedName(), err)
-				}
-			}
+		return nil
+	}
+	for file := range files.RangeFiles {
+		if err := register(file.Messages()); err != nil {
+			return nil, nil, err
 		}
 	}
 	return files, types, nil
