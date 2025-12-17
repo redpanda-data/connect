b2f1f2033725a7c4936848da87de1aa3b95972cf
Ashley Jeffs <ash@jeffail.uk>
2025-12-17 13:38:58 +0000

    Add test for suspended consumption

--- internal/impl/kafka/integration_connectivity_test.go
+++ internal/impl/kafka/integration_connectivity_test.go
@@ -132,7 +132,7 @@ redpanda:
     enabled: true
 `, kafkaPortStr)))
 
-	resources, _, err := resBuilder.Build()
+	resources, _, err := resBuilder.BuildSuspended()
 	require.NoError(t, err)
 
 	require.NoError(t, resources.AccessInput(t.Context(), "ainput", func(i *service.ResourceInput) {
@@ -299,7 +299,7 @@ redpanda:
   topic: testtopic
 `, kafkaPortStr)))
 
-	resources, _, err := resBuilder.Build()
+	resources, _, err := resBuilder.BuildSuspended()
 	require.NoError(t, err)
 
 	require.NoError(t, resources.AccessInput(t.Context(), "ainput", func(i *service.ResourceInput) {
@@ -337,4 +337,130 @@ redpanda:
 		require.Len(t, connResults, 1)
 		require.Error(t, connResults[0].Err)
 	}))
+}
+
+func TestRedpandaConnectionTestPrematureConnectIntegration(t *testing.T) {
+	integration.CheckSkip(t)
+
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	kafkaPort, err := integration.GetFreePort()
+	require.NoError(t, err)
+
+	kafkaPortStr := strconv.Itoa(kafkaPort)
+
+	options := &dockertest.RunOptions{
+		Repository:   "docker.redpanda.com/redpandadata/redpanda",
+		Tag:          "latest",
+		Hostname:     "redpanda",
+		ExposedPorts: []string{"9092/tcp"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"9092/tcp": {{HostIP: "", HostPort: kafkaPortStr + "/tcp"}},
+		},
+		Cmd: []string{
+			"redpanda",
+			"start",
+			"--node-id 0",
+			"--mode dev-container",
+			"--set rpk.additional_start_flags=[--reactor-backend=epoll]",
+			"--kafka-addr 0.0.0.0:9092",
+			fmt.Sprintf("--advertise-kafka-addr localhost:%v", kafkaPort),
+		},
+	}
+
+	pool.MaxWait = time.Minute
+	resource, err := pool.RunWithOptions(options)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, pool.Purge(resource))
+	})
+
+	_ = resource.Expire(900)
+	require.NoError(t, pool.Retry(func() error {
+		return createKafkaTopic(t.Context(), "localhost:"+kafkaPortStr, "testtopic", 1)
+	}))
+
+	resBuilder := service.NewResourceBuilder()
+
+	require.NoError(t, resBuilder.AddOutputYAML(fmt.Sprintf(`
+label: aoutput
+redpanda:
+  seed_brokers: [ localhost:%v ]
+  topic: testtopic
+`, kafkaPortStr)))
+
+	resources, closeFn, err := resBuilder.Build()
+	require.NoError(t, err)
+
+	require.NoError(t, resources.AccessOutput(t.Context(), "aoutput", func(o *service.ResourceOutput) {
+		require.NoError(t, o.WriteBatch(t.Context(), service.MessageBatch{
+			service.NewMessage([]byte("1")),
+			service.NewMessage([]byte("2")),
+			service.NewMessage([]byte("3")),
+			service.NewMessage([]byte("4")),
+			service.NewMessage([]byte("5")),
+		}))
+	}))
+
+	require.NoError(t, closeFn(t.Context()))
+
+	resBuilder = service.NewResourceBuilder()
+
+	require.NoError(t, resBuilder.AddInputYAML(fmt.Sprintf(`
+label: ainput
+redpanda:
+  seed_brokers: [ localhost:%v ]
+  topics: [ testtopic ]
+  consumer_group: testingstuff
+`, kafkaPortStr)))
+
+	require.NoError(t, resBuilder.AddOutputYAML(fmt.Sprintf(`
+label: aoutput
+redpanda:
+  seed_brokers: [ localhost:%v ]
+  topic: testtopic
+`, kafkaPortStr)))
+
+	resources, _, err = resBuilder.BuildSuspended()
+	require.NoError(t, err)
+
+	require.NoError(t, resources.AccessInput(t.Context(), "ainput", func(i *service.ResourceInput) {
+		connResults := i.ConnectionTest(t.Context())
+		require.Len(t, connResults, 1)
+		require.NoError(t, connResults[0].Err)
+	}))
+
+	require.NoError(t, resources.AccessOutput(t.Context(), "aoutput", func(o *service.ResourceOutput) {
+		connResults := o.ConnectionTest(t.Context())
+		require.Len(t, connResults, 1)
+		require.NoError(t, connResults[0].Err)
+	}))
+
+	resBuilder = service.NewResourceBuilder()
+
+	require.NoError(t, resBuilder.AddInputYAML(fmt.Sprintf(`
+label: ainput
+redpanda:
+  seed_brokers: [ localhost:%v ]
+  topics: [ testtopic ]
+  consumer_group: testingstuff
+`, kafkaPortStr)))
+
+	resources, closeFn, err = resBuilder.Build()
+	require.NoError(t, err)
+
+	require.NoError(t, resources.AccessInput(t.Context(), "ainput", func(i *service.ResourceInput) {
+		b, aFn, err := i.ReadBatch(t.Context())
+		require.NoError(t, err)
+		require.GreaterOrEqual(t, len(b), 1)
+
+		mBytes, err := b[0].AsBytes()
+		require.NoError(t, err)
+		assert.Equal(t, "1", string(mBytes))
+
+		require.NoError(t, aFn(t.Context(), nil))
+	}))
+
+	require.NoError(t, closeFn(t.Context()))
 }
