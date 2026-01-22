blip-ai/redpanda-data-connect

Add dynamoDB streams support for cdc

internal/impl/aws/input_dynamodb_cdc_checkpoint.go
@@ -1,0 +1,143 @@
+// Copyright 2026 Redpanda Data, Inc.
+//
+// Licensed as a Redpanda Enterprise file under the Redpanda Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// https://github.com/redpanda-data/connect/blob/main/licenses/rcl.md
+
+package aws
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+
+	"github.com/redpanda-data/benthos/v4/public/service"
+)
+
+// dynamoDBCDCCheckpointer manages checkpoints for DynamoDB CDC shards.
+type dynamoDBCDCCheckpointer struct {
+	tableName       string
+	streamArn       string
+	checkpointLimit int
+	svc             *dynamodb.Client
+	log             *service.Logger
+}
+
+// newDynamoDBCDCCheckpointer creates a new checkpointer for DynamoDB CDC.
+func newDynamoDBCDCCheckpointer(
+	ctx context.Context,
+	svc *dynamodb.Client,
+	tableName,
+	streamArn string,
+	checkpointLimit int,
+	log *service.Logger,
+) (*dynamoDBCDCCheckpointer, error) {
+	c := &dynamoDBCDCCheckpointer{
+		tableName:       tableName,
+		streamArn:       streamArn,
+		checkpointLimit: checkpointLimit,
+		svc:             svc,
+		log:             log,
+	}
+
+	if err := c.ensureTableExists(ctx); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *dynamoDBCDCCheckpointer) ensureTableExists(ctx context.Context) error {
+	_, err := c.svc.DescribeTable(ctx, &dynamodb.DescribeTableInput{
+		TableName: aws.String(c.tableName),
+	})
+
+	var aerr *types.ResourceNotFoundException
+	if err == nil || !errors.As(err, &aerr) {
+		return err
+	}
+
+	// Table doesn't exist, create it
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []types.AttributeDefinition{
+			{AttributeName: aws.String("StreamArn"), AttributeType: types.ScalarAttributeTypeS},
+			{AttributeName: aws.String("ShardID"), AttributeType: types.ScalarAttributeTypeS},
+		},
+		BillingMode: types.BillingModePayPerRequest,
+		KeySchema: []types.KeySchemaElement{
+			{AttributeName: aws.String("StreamArn"), KeyType: types.KeyTypeHash},
+			{AttributeName: aws.String("ShardID"), KeyType: types.KeyTypeRange},
+		},
+		TableName: aws.String(c.tableName),
+	}
+
+	if _, err = c.svc.CreateTable(ctx, input); err != nil {
+		return fmt.Errorf("failed to create checkpoint table: %w", err)
+	}
+
+	c.log.Infof("Created checkpoint table: %s", c.tableName)
+	return nil
+}
+
+// Get retrieves the checkpoint for a shard.
+func (c *dynamoDBCDCCheckpointer) Get(ctx context.Context, shardID string) (string, error) {
+	result, err := c.svc.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: aws.String(c.tableName),
+		Key: map[string]types.AttributeValue{
+			"StreamArn": &types.AttributeValueMemberS{Value: c.streamArn},
+			"ShardID":   &types.AttributeValueMemberS{Value: shardID},
+		},
+	})
+	if err != nil {
+		var aerr *types.ResourceNotFoundException
+		if errors.As(err, &aerr) {
+			return "", nil
+		}
+		return "", err
+	}
+
+	if result.Item == nil {
+		return "", nil
+	}
+
+	if s, ok := result.Item["SequenceNumber"].(*types.AttributeValueMemberS); ok {
+		return s.Value, nil
+	}
+
+	return "", nil
+}
+
+// Set stores a checkpoint for a shard.
+func (c *dynamoDBCDCCheckpointer) Set(ctx context.Context, shardID, sequenceNumber string) error {
+	_, err := c.svc.PutItem(ctx, &dynamodb.PutItemInput{
+		TableName: aws.String(c.tableName),
+		Item: map[string]types.AttributeValue{
+			"StreamArn":      &types.AttributeValueMemberS{Value: c.streamArn},
+			"ShardID":        &types.AttributeValueMemberS{Value: shardID},
+			"SequenceNumber": &types.AttributeValueMemberS{Value: sequenceNumber},
+		},
+	})
+	return err
+}
+
+// FlushCheckpoints writes all pending checkpoints to DynamoDB.
+func (c *dynamoDBCDCCheckpointer) FlushCheckpoints(ctx context.Context, checkpoints map[string]string) error {
+	// Flush all pending checkpoints
+	for shardID, seq := range checkpoints {
+		if seq == "" {
+			continue
+		}
+		if err := c.Set(ctx, shardID, seq); err != nil {
+			c.log.Errorf("Failed to flush checkpoint for shard %s: %w", shardID, err)
+			return err
+		}
+		c.log.Infof("Flushed checkpoint for shard %s at sequence %s", shardID, seq)
+	}
+	return nil
+}

internal/impl/aws/input_dynamodb_cdc_test.go
@@ -1,0 +1,220 @@
+// Copyright 2026 Redpanda Data, Inc.
+//
+// Licensed as a Redpanda Enterprise file under the Redpanda Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// https://github.com/redpanda-data/connect/blob/main/licenses/rcl.md
+package aws
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	streamstypes "github.com/aws/aws-sdk-go-v2/service/dynamodbstreams/types"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/redpanda-data/benthos/v4/public/service"
+)
+
+func TestConvertAttributeValue(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    streamstypes.AttributeValue
+		expected any
+	}{
+		{
+			name:     "string value",
+			input:    &streamstypes.AttributeValueMemberS{Value: "test"},
+			expected: "test",
+		},
+		{
+			name:     "number value",
+			input:    &streamstypes.AttributeValueMemberN{Value: "123"},
+			expected: "123",
+		},
+		{
+			name:     "boolean true",
+			input:    &streamstypes.AttributeValueMemberBOOL{Value: true},
+			expected: true,
+		},
+		{
+			name:     "boolean false",
+			input:    &streamstypes.AttributeValueMemberBOOL{Value: false},
+			expected: false,
+		},
+		{
+			name:     "null value",
+			input:    &streamstypes.AttributeValueMemberNULL{Value: true},
+			expected: nil,
+		},
+		{
+			name:     "string set",
+			input:    &streamstypes.AttributeValueMemberSS{Value: []string{"a", "b", "c"}},
+			expected: []string{"a", "b", "c"},
+		},
+		{
+			name:     "number set",
+			input:    &streamstypes.AttributeValueMemberNS{Value: []string{"1", "2", "3"}},
+			expected: []string{"1", "2", "3"},
+		},
+		{
+			name: "map value",
+			input: &streamstypes.AttributeValueMemberM{Value: map[string]streamstypes.AttributeValue{
+				"key1": &streamstypes.AttributeValueMemberS{Value: "value1"},
+				"key2": &streamstypes.AttributeValueMemberN{Value: "42"},
+			}},
+			expected: map[string]any{
+				"key1": "value1",
+				"key2": "42",
+			},
+		},
+		{
+			name: "list value",
+			input: &streamstypes.AttributeValueMemberL{Value: []streamstypes.AttributeValue{
+				&streamstypes.AttributeValueMemberS{Value: "item1"},
+				&streamstypes.AttributeValueMemberN{Value: "100"},
+			}},
+			expected: []any{"item1", "100"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := convertAttributeValue(tt.input)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func TestConvertAttributeMap(t *testing.T) {
+	input := map[string]streamstypes.AttributeValue{
+		"id":     &streamstypes.AttributeValueMemberS{Value: "123"},
+		"count":  &streamstypes.AttributeValueMemberN{Value: "42"},
+		"active": &streamstypes.AttributeValueMemberBOOL{Value: true},
+		"metadata": &streamstypes.AttributeValueMemberM{Value: map[string]streamstypes.AttributeValue{
+			"created": &streamstypes.AttributeValueMemberS{Value: "2024-01-01"},
+		}},
+	}
+
+	result := convertAttributeMap(input)
+
+	assert.Equal(t, "123", result["id"])
+	assert.Equal(t, "42", result["count"])
+	assert.Equal(t, true, result["active"])
+	assert.IsType(t, map[string]any{}, result["metadata"])
+	metadata := result["metadata"].(map[string]any)
+	assert.Equal(t, "2024-01-01", metadata["created"])
+}
+
+func TestMinFunction(t *testing.T) {
+	tests := []struct {
+		a        int
+		b        int
+		expected int
+	}{
+		{1, 2, 1},
+		{5, 3, 3},
+		{10, 10, 10},
+		{-1, 5, -1},
+		{0, 0, 0},
+	}
+
+	for _, tt := range tests {
+		result := min(tt.a, tt.b)
+		assert.Equal(t, tt.expected, result)
+	}
+}
+
+// Test shard reader structure
+func TestShardReaderStructure(t *testing.T) {
+	// Create input with multiple shard readers
+	input := &dynamoDBCDCInput{
+		shardReaders: map[string]*dynamoDBShardReader{
+			"shard-001": {
+				shardID:   "shard-001",
+				iterator:  aws.String("iter-001"),
+				exhausted: false,
+			},
+			"shard-002": {
+				shardID:   "shard-002",
+				iterator:  aws.String("iter-002"),
+				exhausted: false,
+			},
+			"shard-003": {
+				shardID:   "shard-003",
+				iterator:  aws.String("iter-003"),
+				exhausted: false,
+			},
+		},
+	}
+
+	// Test that shard readers are tracked
+	assert.Contains(t, input.shardReaders, "shard-001")
+	assert.Contains(t, input.shardReaders, "shard-002")
+	assert.Contains(t, input.shardReaders, "shard-003")
+	assert.Len(t, input.shardReaders, 3)
+}
+
+// Regression test: Verify RWMutex allows concurrent reads
+func TestConcurrentShardReaderAccess(t *testing.T) {
+	logger := service.MockResources().Logger()
+
+	input := &dynamoDBCDCInput{
+		shardReaders: map[string]*dynamoDBShardReader{
+			"shard-001": {shardID: "shard-001", iterator: aws.String("iter-001"), exhausted: false},
+			"shard-002": {shardID: "shard-002", iterator: aws.String("iter-002"), exhausted: false},
+		},
+		log: logger,
+	}
+
+	// Multiple goroutines should be able to read concurrently
+	done := make(chan bool, 3)
+
+	for i := 0; i < 3; i++ {
+		go func() {
+			input.mu.Lock()
+			count := len(input.shardReaders)
+			input.mu.Unlock()
+			assert.Equal(t, 2, count)
+			done <- true
+		}()
+	}
+
+	for i := 0; i < 3; i++ {
+		<-done
+	}
+}
+
+// Test that exhausted shards are properly handled
+func TestExhaustedShardHandling(t *testing.T) {
+	logger := service.MockResources().Logger()
+
+	input := &dynamoDBCDCInput{
+		shardReaders: map[string]*dynamoDBShardReader{
+			"shard-001": {
+				shardID:   "shard-001",
+				iterator:  nil, // Exhausted - no iterator
+				exhausted: true,
+			},
+			"shard-002": {
+				shardID:   "shard-002",
+				iterator:  aws.String("iter-002"),
+				exhausted: false,
+			},
+		},
+	}
+	_ = logger
+
+	// Count active readers
+	input.mu.Lock()
+	activeCount := 0
+	for _, reader := range input.shardReaders {
+		if !reader.exhausted && reader.iterator != nil {
+			activeCount++
+		}
+	}
+	input.mu.Unlock()
+
+	assert.Equal(t, 1, activeCount, "Only one shard should be active")
+}

redpanda-data/connect

internal/impl/aws/input_dynamodb_cdc_checkpoint.go
@@ -1,0 +1,148 @@
+// Copyright 2026 Redpanda Data, Inc.
+//
+// Licensed as a Redpanda Enterprise file under the Redpanda Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// https://github.com/redpanda-data/connect/blob/main/licenses/rcl.md
+
+package aws
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+
+	"github.com/redpanda-data/benthos/v4/public/service"
+)
+
+// dynamoDBCDCCheckpointer manages checkpoints for DynamoDB CDC shards.
+type dynamoDBCDCCheckpointer struct {
+	tableName       string
+	streamArn       string
+	checkpointLimit int
+	svc             *dynamodb.Client
+	log             *service.Logger
+}
+
+// newDynamoDBCDCCheckpointer creates a new checkpointer for DynamoDB CDC.
+func newDynamoDBCDCCheckpointer(
+	ctx context.Context,
+	svc *dynamodb.Client,
+	tableName,
+	streamArn string,
+	checkpointLimit int,
+	log *service.Logger,
+) (*dynamoDBCDCCheckpointer, error) {
+	c := &dynamoDBCDCCheckpointer{
+		tableName:       tableName,
+		streamArn:       streamArn,
+		checkpointLimit: checkpointLimit,
+		svc:             svc,
+		log:             log,
+	}
+
+	if err := c.ensureTableExists(ctx); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *dynamoDBCDCCheckpointer) ensureTableExists(ctx context.Context) error {
+	_, err := c.svc.DescribeTable(ctx, &dynamodb.DescribeTableInput{
+		TableName: aws.String(c.tableName),
+	})
+
+	var aerr *types.ResourceNotFoundException
+	if err == nil || !errors.As(err, &aerr) {
+		return err
+	}
+
+	// Table doesn't exist, create it
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []types.AttributeDefinition{
+			{AttributeName: aws.String("StreamArn"), AttributeType: types.ScalarAttributeTypeS},
+			{AttributeName: aws.String("ShardID"), AttributeType: types.ScalarAttributeTypeS},
+		},
+		BillingMode: types.BillingModePayPerRequest,
+		KeySchema: []types.KeySchemaElement{
+			{AttributeName: aws.String("StreamArn"), KeyType: types.KeyTypeHash},
+			{AttributeName: aws.String("ShardID"), KeyType: types.KeyTypeRange},
+		},
+		TableName: aws.String(c.tableName),
+	}
+
+	if _, err = c.svc.CreateTable(ctx, input); err != nil {
+		return fmt.Errorf("failed to create checkpoint table: %w", err)
+	}
+
+	c.log.Infof("Created checkpoint table: %s", c.tableName)
+	return nil
+}
+
+// Get retrieves the checkpoint for a shard.
+func (c *dynamoDBCDCCheckpointer) Get(ctx context.Context, shardID string) (string, error) {
+	result, err := c.svc.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: aws.String(c.tableName),
+		Key: map[string]types.AttributeValue{
+			"StreamArn": &types.AttributeValueMemberS{Value: c.streamArn},
+			"ShardID":   &types.AttributeValueMemberS{Value: shardID},
+		},
+	})
+	if err != nil {
+		var aerr *types.ResourceNotFoundException
+		if errors.As(err, &aerr) {
+			return "", nil
+		}
+		return "", fmt.Errorf("failed to get checkpoint for table=%s stream=%s shard=%s: %w",
+			c.tableName, c.streamArn, shardID, err)
+	}
+
+	if result.Item == nil {
+		return "", nil
+	}
+
+	if s, ok := result.Item["SequenceNumber"].(*types.AttributeValueMemberS); ok {
+		return s.Value, nil
+	}
+
+	return "", nil
+}
+
+// Set stores a checkpoint for a shard.
+func (c *dynamoDBCDCCheckpointer) Set(ctx context.Context, shardID, sequenceNumber string) error {
+	_, err := c.svc.PutItem(ctx, &dynamodb.PutItemInput{
+		TableName: aws.String(c.tableName),
+		Item: map[string]types.AttributeValue{
+			"StreamArn":      &types.AttributeValueMemberS{Value: c.streamArn},
+			"ShardID":        &types.AttributeValueMemberS{Value: shardID},
+			"SequenceNumber": &types.AttributeValueMemberS{Value: sequenceNumber},
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to set checkpoint for table=%s stream=%s shard=%s seq=%s: %w",
+			c.tableName, c.streamArn, shardID, sequenceNumber, err)
+	}
+	return nil
+}
+
+// FlushCheckpoints writes all pending checkpoints to DynamoDB.
+func (c *dynamoDBCDCCheckpointer) FlushCheckpoints(ctx context.Context, checkpoints map[string]string) error {
+	// Flush all pending checkpoints
+	for shardID, seq := range checkpoints {
+		if seq == "" {
+			continue
+		}
+		if err := c.Set(ctx, shardID, seq); err != nil {
+			c.log.Errorf("Failed to flush checkpoint for shard %s: %v", shardID, err)
+			return err
+		}
+		c.log.Infof("Flushed checkpoint for shard %s at sequence %s", shardID, seq)
+	}
+	return nil
+}

internal/impl/aws/input_dynamodb_cdc_test.go
@@ -1,0 +1,188 @@
+// Copyright 2026 Redpanda Data, Inc.
+//
+// Licensed as a Redpanda Enterprise file under the Redpanda Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// https://github.com/redpanda-data/connect/blob/main/licenses/rcl.md
+
+package aws
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	streamstypes "github.com/aws/aws-sdk-go-v2/service/dynamodbstreams/types"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/redpanda-data/benthos/v4/public/service"
+)
+
+func TestConvertAttributeValue(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    streamstypes.AttributeValue
+		expected any
+	}{
+		{
+			name:     "string value",
+			input:    &streamstypes.AttributeValueMemberS{Value: "test"},
+			expected: "test",
+		},
+		{
+			name:     "number value",
+			input:    &streamstypes.AttributeValueMemberN{Value: "123"},
+			expected: "123",
+		},
+		{
+			name:     "boolean true",
+			input:    &streamstypes.AttributeValueMemberBOOL{Value: true},
+			expected: true,
+		},
+		{
+			name:     "boolean false",
+			input:    &streamstypes.AttributeValueMemberBOOL{Value: false},
+			expected: false,
+		},
+		{
+			name:     "null value",
+			input:    &streamstypes.AttributeValueMemberNULL{Value: true},
+			expected: nil,
+		},
+		{
+			name:     "string set",
+			input:    &streamstypes.AttributeValueMemberSS{Value: []string{"a", "b", "c"}},
+			expected: []string{"a", "b", "c"},
+		},
+		{
+			name:     "number set",
+			input:    &streamstypes.AttributeValueMemberNS{Value: []string{"1", "2", "3"}},
+			expected: []string{"1", "2", "3"},
+		},
+		{
+			name: "map value",
+			input: &streamstypes.AttributeValueMemberM{Value: map[string]streamstypes.AttributeValue{
+				"key1": &streamstypes.AttributeValueMemberS{Value: "value1"},
+				"key2": &streamstypes.AttributeValueMemberN{Value: "42"},
+			}},
+			expected: map[string]any{
+				"key1": "value1",
+				"key2": "42",
+			},
+		},
+		{
+			name: "list value",
+			input: &streamstypes.AttributeValueMemberL{Value: []streamstypes.AttributeValue{
+				&streamstypes.AttributeValueMemberS{Value: "item1"},
+				&streamstypes.AttributeValueMemberN{Value: "100"},
+			}},
+			expected: []any{"item1", "100"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := convertAttributeValue(tt.input)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func TestConvertAttributeMap(t *testing.T) {
+	input := map[string]streamstypes.AttributeValue{
+		"id":     &streamstypes.AttributeValueMemberS{Value: "123"},
+		"count":  &streamstypes.AttributeValueMemberN{Value: "42"},
+		"active": &streamstypes.AttributeValueMemberBOOL{Value: true},
+		"metadata": &streamstypes.AttributeValueMemberM{Value: map[string]streamstypes.AttributeValue{
+			"created": &streamstypes.AttributeValueMemberS{Value: "2024-01-01"},
+		}},
+	}
+
+	result := convertAttributeMap(input)
+
+	assert.Equal(t, "123", result["id"])
+	assert.Equal(t, "42", result["count"])
+	assert.Equal(t, true, result["active"])
+	assert.IsType(t, map[string]any{}, result["metadata"])
+	metadata := result["metadata"].(map[string]any)
+	assert.Equal(t, "2024-01-01", metadata["created"])
+}
+
+func TestMinFunction(t *testing.T) {
+	tests := []struct {
+		a        int
+		b        int
+		expected int
+	}{
+		{1, 2, 1},
+		{5, 3, 3},
+		{10, 10, 10},
+		{-1, 5, -1},
+		{0, 0, 0},
+	}
+
+	for _, tt := range tests {
+		result := min(tt.a, tt.b)
+		assert.Equal(t, tt.expected, result)
+	}
+}
+
+// Regression test: Verify RWMutex allows concurrent reads
+func TestConcurrentShardReaderAccess(t *testing.T) {
+	logger := service.MockResources().Logger()
+
+	input := &dynamoDBCDCInput{
+		shardReaders: map[string]*dynamoDBShardReader{
+			"shard-001": {shardID: "shard-001", iterator: aws.String("iter-001"), exhausted: false},
+			"shard-002": {shardID: "shard-002", iterator: aws.String("iter-002"), exhausted: false},
+		},
+		log: logger,
+	}
+
+	// Multiple goroutines should be able to read concurrently
+	done := make(chan bool, 3)
+
+	for range 3 {
+		go func() {
+			input.mu.RLock()
+			count := len(input.shardReaders)
+			input.mu.RUnlock()
+			assert.Equal(t, 2, count)
+			done <- true
+		}()
+	}
+
+	for range 3 {
+		<-done
+	}
+}
+
+// Test that exhausted shards are properly handled
+func TestExhaustedShardHandling(t *testing.T) {
+	input := &dynamoDBCDCInput{
+		shardReaders: map[string]*dynamoDBShardReader{
+			"shard-001": {
+				shardID:   "shard-001",
+				iterator:  nil, // Exhausted - no iterator
+				exhausted: true,
+			},
+			"shard-002": {
+				shardID:   "shard-002",
+				iterator:  aws.String("iter-002"),
+				exhausted: false,
+			},
+		},
+	}
+
+	// Count active readers
+	input.mu.RLock()
+	activeCount := 0
+	for _, reader := range input.shardReaders {
+		if !reader.exhausted && reader.iterator != nil {
+			activeCount++
+		}
+	}
+	input.mu.RUnlock()
+
+	assert.Equal(t, 1, activeCount, "Only one shard should be active")
+}