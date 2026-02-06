cddfc1e44cb923190580cc3115d08b6e573b6c64
ness-david-dedu <david.dedu@ness.com>
2026-02-06 16:21:02 +0200

    Update internal/impl/salesforce/salesforcehttp/http_metrics/http_metrics.go
    Co-authored-by: Joseph Woodward <joseph.woodward@xeuse.com>

--- internal/impl/salesforce/salesforcehttp/http_metrics/http_metrics.go
+++ internal/impl/salesforce/salesforcehttp/http_metrics/http_metrics.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Redpanda Data, Inc.
+// Copyright 2026 Redpanda Data, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
