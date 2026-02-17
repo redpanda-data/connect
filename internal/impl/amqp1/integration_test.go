e61a9a7088a8a170d3b2eca0b1f33073233060e1
Julien Erny <julien.erny@airnity.com>
2026-02-17 16:41:36 +0100

    amqp_1(output): add support for Anonymous Terminus in integration tests (#3990)

--- internal/impl/amqp1/integration_test.go
+++ internal/impl/amqp1/integration_test.go
@@ -111,6 +111,21 @@ input:
     source_address: "queue:/$ID"
 `
 
+	templateWithAnonymousTerminus := `
+output:
+  amqp_1:
+    url: amqp://guest:guest@localhost:$PORT/
+    target_address: ""
+    message_properties_to: "queue:/$ID"
+    max_in_flight: $MAX_IN_FLIGHT
+    metadata:
+      exclude_prefixes: [ $OUTPUT_META_EXCLUDE_PREFIX ]
+input:
+  amqp_1:
+    url: amqp://guest:guest@localhost:$PORT/
+    source_address: "queue:/$ID"
+`
+
 	testcases := []struct {
 		label    string
 		template string
@@ -126,6 +141,10 @@ input:
 		{
 			label:    "should handle content type string",
 			template: templateWithContentTypeString,
+		},
+		{
+			label:    "should handle Anonymous Terminus pattern",
+			template: templateWithAnonymousTerminus,
 		},
 	}
 
