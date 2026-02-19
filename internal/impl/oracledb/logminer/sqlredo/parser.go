fa214f72e1b6a74fbdb660a2a12448758cdacd86
Joseph Woodward <joseph.woodward@xeuse.com>
2026-02-19 12:32:09 +0000

    fix: update legal headings

--- internal/impl/oracledb/logminer/sqlredo/parser.go
+++ internal/impl/oracledb/logminer/sqlredo/parser.go
@@ -1,8 +1,10 @@
+// Copyright 2026 Redpanda Data, Inc.
 //
-// Copyright Debezium Authors.
+// Licensed as a Redpanda Enterprise file under the Redpanda Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
 //
-// Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
-//
+// https://github.com/redpanda-data/connect/blob/main/licenses/rcl.md
 
 package sqlredo
 
