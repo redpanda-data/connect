8547159001ddaf0a306e2b67ba1dbfd86fcdeada
xylaaaaa <2392805527@qq.com>
2026-04-07 19:44:41 +0800

    doris: add stream load output

--- public/components/community/package.go
+++ public/components/community/package.go
@@ -36,6 +36,7 @@ import (
 	_ "github.com/redpanda-data/connect/v4/public/components/cypher"
 	_ "github.com/redpanda-data/connect/v4/public/components/dgraph"
 	_ "github.com/redpanda-data/connect/v4/public/components/discord"
+	_ "github.com/redpanda-data/connect/v4/public/components/doris"
 	_ "github.com/redpanda-data/connect/v4/public/components/elasticsearch/v8"
 	_ "github.com/redpanda-data/connect/v4/public/components/elasticsearch/v9"
 	_ "github.com/redpanda-data/connect/v4/public/components/ffi"
