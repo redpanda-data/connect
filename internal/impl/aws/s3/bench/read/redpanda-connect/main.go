25fd2bee7115d4825e58e2110da0ef13fd4b10fb
ness-david-dedu <david.dedu@ness.com>
2026-04-30 10:32:23 +0300

    aupdate license on helper go class

--- internal/impl/aws/s3/bench/read/redpanda-connect/main.go
+++ internal/impl/aws/s3/bench/read/redpanda-connect/main.go
@@ -1,16 +1,18 @@
-// Copyright 2024 Redpanda Data, Inc.
+// Copyright 2026 Redpanda Data, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed as a Redpanda Enterprise file under the Redpanda Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+// https://github.com/redpanda-data/connect/blob/main/licenses/rcl.md
+
+// output_salesforce.go implements a Redpanda Connect output component that writes
+// messages to Salesforce using either the sObject Collections REST API (realtime mode)
+// or the Bulk API 2.0 (bulk mode).
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Messages are routed to the correct SObject configuration based on the "topic" field
+// set by the per-topic processor. Each topic_mapping entry defines the SObject, operation,
+// external ID field, and write mode for a given topic.
 
 // Package main provides a benchmark data generation tool for AWS S3.
 package main
