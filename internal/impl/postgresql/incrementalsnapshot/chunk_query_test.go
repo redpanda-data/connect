031501a5b7154a4014190eff3b718a59dcc967ea
Joseph Woodward <joseph.woodward@xeuse.com>
2026-09-06 01:13:48 +0100

    postgres_cdc: clean up

--- internal/impl/postgresql/incrementalsnapshot/chunk_query_test.go
+++ internal/impl/postgresql/incrementalsnapshot/chunk_query_test.go
@@ -1,4 +1,4 @@
-// Copyright 2025 Redpanda Data, Inc.
+// Copyright 2026 Redpanda Data, Inc.
 //
 // Licensed as a Redpanda Enterprise file under the Redpanda Community
 // License (the "License"); you may not use this file except in compliance with
