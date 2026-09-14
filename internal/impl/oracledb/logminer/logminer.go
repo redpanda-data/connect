81b1e7ba2a84362a62375875fb612cf3a19d7222
Joseph Woodward <joseph.woodward@xeuse.com>
2026-09-14 20:41:59 +0100

    address incorret log line

--- internal/impl/oracledb/logminer/logminer.go
+++ internal/impl/oracledb/logminer/logminer.go
@@ -953,7 +953,12 @@ func (lm *LogMiner) queryLogMinerContents(ctx context.Context, conn *sql.Conn, s
 	}
 
 	// Use the pre-built query from initialization
-	lm.log.Debugf("Executing LogMiner query with SCN range (scn=%d to %d with window %d)", startSCN, endSCN, lm.windowSize)
+	switch lm.cfg.WindowStrategy {
+	case WindowStrategyLogCount:
+		lm.log.Debugf("Executing LogMiner query with SCN range (scn=%d to %d, log_count budget=%d files)", startSCN, endSCN, lm.logSelector.count)
+	default:
+		lm.log.Debugf("Executing LogMiner query with SCN range (scn=%d to %d with window %d)", startSCN, endSCN, lm.windowSize)
+	}
 	queryStart := time.Now()
 	rows, err := conn.QueryContext(ctx, lm.logMinerQuery, startSCN, endSCN)
 	if err != nil {
